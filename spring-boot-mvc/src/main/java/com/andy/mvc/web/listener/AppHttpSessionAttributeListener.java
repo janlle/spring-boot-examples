90ba114043ec57883bb8ad8205221b2fade56cf6
andy <rethinkt@163.com>
2018-05-13 11:34:00 +0800

    update some file

---
+++ spring-boot-mvc/src/main/java/com/andy/mvc/web/listener/AppHttpSessionAttributeListener.java
@@ -0,0 +1,31 @@
+package com.andy.mvc.web.listener;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+/**
+ * @author: Mr.ruoLin
+ * @createBy: 2018-05-13 10:53
+ **/
+@Slf4j
+@WebListener
+public class AppHttpSessionAttributeListener implements HttpSessionAttributeListener {
+
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent event) {
+
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent event) {
+
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent event) {
+
+    }
+}
