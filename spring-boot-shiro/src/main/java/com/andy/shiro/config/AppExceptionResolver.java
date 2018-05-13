b654a463f718458bfd506fe1b06da5a46ddbfe05
leone <janlle@126.com>
2018-05-13 19:22:18 +0800

    update some file

--- spring-boot-shiro/src/main/java/com/andy/shiro/config/AppExceptionResolver.java
+++ spring-boot-shiro/src/main/java/com/andy/shiro/config/AppExceptionResolver.java
@@ -1,6 +1,7 @@
 package com.andy.shiro.config;
 
 import org.apache.shiro.authz.UnauthorizedException;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,16 +12,21 @@ import javax.servlet.http.HttpServletResponse;
  * @author: Mr.ruoLin
  * @createBy: 2018-04-21 16:51
  **/
+@Component
 public class AppExceptionResolver implements HandlerExceptionResolver {
+
+    private static ModelAndView unAuth = new ModelAndView("403");
+
+    private static ModelAndView error = new ModelAndView("500");
 
     @Override
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         //如果是shiro无权操作转发到未授权页面
-        if(ex instanceof UnauthorizedException){
-            ModelAndView mv = new ModelAndView("403");
-            return mv;
+        if (ex instanceof UnauthorizedException) {
+            return unAuth;
+        } else {
+            return error;
         }
-        return null;
     }
 
 }
