b5f164102a579b223cff9330666cce509340abbb
leone <janlle@126.com>
2018-05-13 14:36:52 +0800

    update some file

--- spring-boot-shiro/src/main/java/com/andy/shiro/config/ShiroConfiguration.java
+++ spring-boot-shiro/src/main/java/com/andy/shiro/config/ShiroConfiguration.java
@@ -1,71 +1,72 @@
-package com.andy.shiro.config;//package com.andy.shiro.config;
-//
-//import org.apache.shiro.mgt.SecurityManager;
-//import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
-//import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-//import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-//import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.LinkedHashMap;
-//
-///**
-// * @author: Mr.ruoLin
-// * @createBy: 2018-04-21 17:22
-// **/
-//@Configuration
-//public class ShiroConfiguration {
-//
-//    @Bean("shiroFilter")
-//    public ShiroFilterFactoryBean shiroFilter(@Qualifier("securityManager") SecurityManager securityManager) {
-//        ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
-//        bean.setSecurityManager(securityManager);
-//
-//        bean.setLoginUrl("/login");
-//        bean.setSuccessUrl("/index");
-//        bean.setUnauthorizedUrl("/unauthorized");
-//
-//        LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
-//        filterChainDefinitionMap.put("/index", "authc");
-//        filterChainDefinitionMap.put("/login", "anon");
-//        bean.setFilterChainDefinitionMap(filterChainDefinitionMap);
-//        return bean;
-//    }
-//
-//
-//    @Bean("securityManager")
-//    public SecurityManager securityManager(@Qualifier("authRealm") AuthRealm authRealm) {
-//        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
-//        manager.setRealm(authRealm);
-//        return manager;
-//    }
-//
-//    @Bean("authRealm")
-//    public AuthRealm authRealm(@Qualifier("credentialMatcher") CredentialMatcher matcher) {
-//        AuthRealm authRealm = new AuthRealm();
-//        authRealm.setCredentialsMatcher(matcher);
-//        return authRealm;
-//    }
-//
-//    @Bean("credentialMatcher")
-//    public CredentialMatcher credentialMatcher() {
-//        return new CredentialMatcher();
-//    }
-//
-//    @Bean
-//    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(@Qualifier("securityManager")SecurityManager securityManager) {
-//        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
-//        advisor.setSecurityManager(securityManager);
-//        return advisor;
-//    }
-//
-//    @Bean
-//    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
-//        DefaultAdvisorAutoProxyCreator creator = new DefaultAdvisorAutoProxyCreator();
-//        creator.setProxyTargetClass(true);
-//        return creator;
-//    }
-//
-//}
+package com.andy.shiro.config;
+
+
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @author: Mr.ruoLin
+ * @createBy: 2018-04-21 17:22
+ **/
+@Configuration
+public class ShiroConfiguration {
+
+    @Bean("shiroFilter")
+    public ShiroFilterFactoryBean shiroFilter(@Qualifier("securityManager") SecurityManager securityManager) {
+        ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
+        bean.setSecurityManager(securityManager);
+
+        bean.setLoginUrl("/login");
+        bean.setSuccessUrl("/index");
+        bean.setUnauthorizedUrl("/unauthorized");
+
+        LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+        filterChainDefinitionMap.put("/index", "authc");
+        filterChainDefinitionMap.put("/login", "anon");
+        bean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return bean;
+    }
+
+
+    @Bean("securityManager")
+    public SecurityManager securityManager(@Qualifier("authRealm") AuthRealm authRealm) {
+        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+        manager.setRealm(authRealm);
+        return manager;
+    }
+
+    @Bean("authRealm")
+    public AuthRealm authRealm(@Qualifier("credentialMatcher") CredentialMatcher matcher) {
+        AuthRealm authRealm = new AuthRealm();
+        authRealm.setCredentialsMatcher(matcher);
+        return authRealm;
+    }
+
+    @Bean("credentialMatcher")
+    public CredentialMatcher credentialMatcher() {
+        return new CredentialMatcher();
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(@Qualifier("securityManager")SecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
+        advisor.setSecurityManager(securityManager);
+        return advisor;
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator creator = new DefaultAdvisorAutoProxyCreator();
+        creator.setProxyTargetClass(true);
+        return creator;
+    }
+
+}
--- spring-boot-shiro/src/main/java/com/andy/shiro/repository/UserInfoDao.java
+++
@@ -1,6 +0,0 @@
-package com.andy.shiro.repository;
-
-//
-//public interface UserInfoDao extends CrudRepository<User,Long> {
-//    User findByUsername(String username);
-//}
