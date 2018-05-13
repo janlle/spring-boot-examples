f41168eddf4be2005814f2073686f03b6d254133
andy <rethinkt@163.com>
2018-05-13 14:36:52 +0800

    update some file

--- spring-boot-shiro/src/main/java/com/andy/shiro/entity/rbac/Role.java
+++ spring-boot-shiro/src/main/java/com/andy/shiro/entity/rbac/Role.java
@@ -2,6 +2,7 @@ package com.andy.shiro.entity.rbac;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,11 +12,18 @@ import java.util.Set;
  * @createBy: 2018-04-19
  **/
 @Data
+//@Entity
+//@Table(name = "sys_role")
 public class Role {
-
+//    @Id
+//    @GeneratedValue
     private Integer rid;
 
-    private String name;
+    private String role;
+
+    private String available;
+
+    private String description;
 
     //对应的权限集合
     private Set<Permission> permissions = new HashSet<>();
