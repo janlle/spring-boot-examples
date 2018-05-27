2beefdbb65648ab85c52d93084e65d4c8b591b16
leone <janlle@126.com>
2018-05-27 12:23:59 +0800

    update some file

---
+++ spring-boot-task/src/main/java/com/andy/task/TaskApplication.java
@@ -0,0 +1,17 @@
+package com.andy.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author: Mr.ruoLin
+ * @createBy: 2018-05-27
+ **/
+@EnableScheduling
+@SpringBootApplication
+public class TaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication.class, args);
+    }
+}
---
+++ spring-boot-task/src/main/java/com/andy/task/quartz/jobs/HelloJob.java
@@ -0,0 +1,28 @@
+package com.andy.task.quartz.jobs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Description:
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-05-27
+ **/
+@Slf4j
+@Configuration
+@EnableScheduling
+public class HelloJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        log.info("Welcome to Spring-Quartz World!"+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) );
+    }
+
+}
