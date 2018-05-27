9c5d33a5d197f679eda656d7529dd24f16979bec
leone <exklin@gmail.com>
2018-05-27 16:58:38 +0800

    update some file

--- spring-boot-docker/src/main/java/com/andy/docker/controller/WebController.java
+++ spring-boot-docker/src/main/java/com/andy/docker/controller/WebController.java
@@ -3,14 +3,12 @@ package com.andy.docker.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import reactor.core.publisher.Mono;
-
 @RestController()
-public class FluxController {
+public class WebController {
 	
     @GetMapping("/hello_world")
-    public Mono<String> sayHelloWorld() {
-        return Mono.just("Hello World");
+    public String sayHelloWorld() {
+        return "success";
     }
 
 }
