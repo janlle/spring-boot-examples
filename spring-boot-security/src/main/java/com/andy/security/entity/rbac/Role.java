d4f78bfa832ca1ba0de78c97bcf6f5285a8ce8e4
janlle <rethinkt@163.com>
2018-05-28 21:10:20 +0800

    update some file

---
+++ spring-boot-security/src/main/java/com/andy/security/entity/rbac/Role.java
@@ -0,0 +1,38 @@
+package com.andy.security.entity.rbac;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 角色信息
+ * @author: Mr.ruoLin
+ * @createBy: 2018-04-19
+ **/
+@Data
+@ApiModel("角色实体")
+public class Role {
+
+    @ApiModelProperty("角色id")
+    private Integer     roleId;
+
+    @ApiModelProperty("角色名称")
+    private String      role;
+
+    @ApiModelProperty("是否可用")
+    private Boolean     available;
+
+    @ApiModelProperty("角色的详情")
+    private String      description;
+
+    @ApiModelProperty("对应的权限集合")
+    private Set<Permission> permissions = new HashSet<>();
+
+    @ApiModelProperty("对应的角色集合")
+    private Set<User> users = new HashSet<>();
+
+
+}
