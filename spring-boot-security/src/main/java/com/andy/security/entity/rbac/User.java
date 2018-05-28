00ccc6a60a40814356c38a6aedd953d1350cbece
leone <exklin@gmail.com>
2018-05-28 21:10:20 +0800

    update some file

---
+++ spring-boot-security/src/main/java/com/andy/security/entity/rbac/User.java
@@ -0,0 +1,41 @@
+package com.andy.security.entity.rbac;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 用户信息
+ * @author: Mr.ruoLin
+ * @createBy: 2018-04-19
+ **/
+@Data
+@ApiModel("用户实体")
+public class User implements Serializable {
+
+    @ApiModelProperty("用户id")
+    private Integer     userId;
+
+    @ApiModelProperty("用户姓名")
+    private String      username;
+
+    @ApiModelProperty("用户密码")
+    private String      password;
+
+    @ApiModelProperty("昵称")
+    private String      nickname;
+
+    @ApiModelProperty("盐")
+    private String      salt;
+
+    @ApiModelProperty("用户状态")
+    private String      state;
+
+    @ApiModelProperty("角色集合")
+    private Set<Role>   roles = new HashSet<>();
+
+ }
