457d2308b541c59a89cb4830940cb50b0dad850d
leone <exklin@gmail.com>
2018-05-29 23:44:07 +0800

    update some file

--- spring-boot-webflux/src/main/java/com/andy/flux/repository/UserRepository.java
+++ spring-boot-webflux/src/main/java/com/andy/flux/repository/UserRepository.java
@@ -5,12 +5,12 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-public interface UserRepository extends ReactiveCrudRepository<User, String> {
+//public interface UserRepository extends ReactiveCrudRepository<User, String> {
 
-        Mono<User> findByUsername(String username);
+//        Mono<User> findByUsername(String username);
+//
+//        Mono<Long> deleteByUsername(String username);
+//
+//        Flux<User> findAll();
 
-        Mono<Long> deleteByUsername(String username);
-
-        Flux<User> findAll();
-
-}
+//}
