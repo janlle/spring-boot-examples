5d5bec47bd39e17a9a4025ba4471adafbc332a0c
leone <janlle@126.com>
2018-05-30 22:15:37 +0800

    update some file

--- spring-boot-mvc/src/main/java/com/andy/mvc/MVCApplication.java
+++ spring-boot-mvc/src/main/java/com/andy/mvc/MVCApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 //扫描servlet filter listener
+@EnableScheduling
 @ServletComponentScan
 @MapperScan("com.andy.mvc.dao.mapper")
 @SpringBootApplication
-@EnableScheduling       // 整合定时任务需要添加的注解
 public class MVCApplication {
 	public static void main(String[] args) {
         SpringApplication.run(MVCApplication.class, args);
