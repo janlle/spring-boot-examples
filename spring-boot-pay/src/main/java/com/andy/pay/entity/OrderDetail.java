adfbef9205b89de4cdee4c346fdabd4c5b27ab8c
leone <janlle@126.com>
2018-06-03 19:51:31 +0800

    add some file

--- spring-boot-pay/src/main/java/com/andy/pay/entity/OrderDetail.java
+++ spring-boot-pay/src/main/java/com/andy/pay/entity/OrderDetail.java
@@ -1,8 +1,14 @@
 package com.andy.pay.entity;
 
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
+@ApiModel("")
 public class OrderDetail {
+
     private String id;
 
     private Long detailId;
@@ -23,83 +29,5 @@ public class OrderDetail {
 
     private Date updateTime;
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id == null ? null : id.trim();
-    }
-
-    public Long getDetailId() {
-        return detailId;
-    }
-
-    public void setDetailId(Long detailId) {
-        this.detailId = detailId;
-    }
-
-    public String getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(String orderId) {
-        this.orderId = orderId == null ? null : orderId.trim();
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId == null ? null : productId.trim();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public Long getPrice() {
-        return price;
-    }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-
-    public Integer getCount() {
-        return count;
-    }
-
-    public void setCount(Integer count) {
-        this.count = count;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl == null ? null : imageUrl.trim();
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 }
