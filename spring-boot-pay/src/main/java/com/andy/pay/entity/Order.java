c9a14bfe803201303c3bdc354e42cb81860764d7
leone <exklin@gmail.com>
2018-06-03 19:51:31 +0800

    add some file

--- spring-boot-pay/src/main/java/com/andy/pay/entity/Order.java
+++ spring-boot-pay/src/main/java/com/andy/pay/entity/Order.java
@@ -1,7 +1,12 @@
 package com.andy.pay.entity;
 
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
+@ApiModel("")
 public class Order {
     private Long id;
 
@@ -37,139 +42,4 @@ public class Order {
 
     private Date payTime;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(String orderId) {
-        this.orderId = orderId == null ? null : orderId.trim();
-    }
-
-    public String getReceiveId() {
-        return receiveId;
-    }
-
-    public void setReceiveId(String receiveId) {
-        this.receiveId = receiveId == null ? null : receiveId.trim();
-    }
-
-    public String getBuyerNick() {
-        return buyerNick;
-    }
-
-    public void setBuyerNick(String buyerNick) {
-        this.buyerNick = buyerNick == null ? null : buyerNick.trim();
-    }
-
-    public Integer getPostFee() {
-        return postFee;
-    }
-
-    public void setPostFee(Integer postFee) {
-        this.postFee = postFee;
-    }
-
-    public Integer getTotalFee() {
-        return totalFee;
-    }
-
-    public void setTotalFee(Integer totalFee) {
-        this.totalFee = totalFee;
-    }
-
-    public String getBuyerRate() {
-        return buyerRate;
-    }
-
-    public void setBuyerRate(String buyerRate) {
-        this.buyerRate = buyerRate == null ? null : buyerRate.trim();
-    }
-
-    public Byte getStatus() {
-        return status;
-    }
-
-    public void setStatus(Byte status) {
-        this.status = status;
-    }
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark == null ? null : remark.trim();
-    }
-
-    public String getOutTradeNum() {
-        return outTradeNum;
-    }
-
-    public void setOutTradeNum(String outTradeNum) {
-        this.outTradeNum = outTradeNum == null ? null : outTradeNum.trim();
-    }
-
-    public String getCreateIp() {
-        return createIp;
-    }
-
-    public void setCreateIp(String createIp) {
-        this.createIp = createIp == null ? null : createIp.trim();
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description == null ? null : description.trim();
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getConsignTime() {
-        return consignTime;
-    }
-
-    public void setConsignTime(Date consignTime) {
-        this.consignTime = consignTime;
-    }
-
-    public Date getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(Date endTime) {
-        this.endTime = endTime;
-    }
-
-    public Date getCloseTime() {
-        return closeTime;
-    }
-
-    public void setCloseTime(Date closeTime) {
-        this.closeTime = closeTime;
-    }
-
-    public Date getPayTime() {
-        return payTime;
-    }
-
-    public void setPayTime(Date payTime) {
-        this.payTime = payTime;
-    }
 }
