b050960e8807a4d6d4df081d4ddad3fe854abf15
janlle <rethinkt@163.com>
2018-06-03 20:49:56 +0800

    add some file

--- spring-boot-pay/src/main/java/com/andy/pay/web/controller/UserController.java
+++ spring-boot-pay/src/main/java/com/andy/pay/web/controller/UserController.java
@@ -1,11 +1,24 @@
 package com.andy.pay.web.controller;
 
+import com.andy.pay.common.Result;
+import com.andy.pay.enums.ResultEnum;
+import com.andy.pay.exception.AppException;
+import com.andy.pay.object.entity.User;
+import com.andy.pay.service.UserService;
+import com.sun.deploy.association.utility.AppConstants;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @Slf4j
@@ -14,13 +27,66 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/order")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
 
-    @ApiOperation("")
-    @ApiImplicitParam(name = "", value = "", dataType = "", required = true, paramType = "")
-    @RequestMapping("/")
-    public String index() {
-        return "index";
+    private String emailReg = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
+    private String phoneReg = "^[1][3456789]\\d{9}$";
+
+    @ApiOperation(value = "测试", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping("/")
+    public String home() {
+        return "success";
     }
+
+    @ApiOperation(value = "用户登录接口", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "account",value ="用户账号",required = true, dataType = "String"),
+            @ApiImplicitParam(name = "password",value ="用户密码",required = true, dataType = "String")
+    })
+    @PostMapping("/login")
+    public Result<Object> login(String account, String password, HttpServletRequest request, HttpServletResponse response) {
+
+        return Result.build(ResultEnum.USERNAME_PASSWORD_FAIL, null);
+    }
+
+    @ApiOperation(value = "用户退出接口", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId",value ="用户ID",required = true, dataType = "String"),
+            @ApiImplicitParam(name = "token",value ="token",required = true, dataType = "String")
+    })
+    @GetMapping("/logout")
+    public Result<String> logout(String token, Integer userId) {
+        log.info("logout [删除token成功]->{}", token);
+        return Result.success("用户退出成功");
+    }
+
+    @ApiOperation(value = "用户注册接口", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiImplicitParam(name = "user",value ="用户信息模型",required = true, dataType = "User")
+    @PostMapping("/register")
+    public Result<String> register(@RequestBody User user) {
+
+        return Result.error("注册失败");
+    }
+
+    @ApiOperation(value = "根据用户id获取用户信息", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiImplicitParam(name = "userId",value ="用户ID",required = true, dataType = "String")
+    @GetMapping("/{userId}")
+    public Result<User> user(@PathVariable("userId") String userId) {
+        return Result.success(null);
+    }
+
+    @ApiOperation(value = "用户认证授权接口", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId",value ="用户ID",required = true, dataType = "Integer"),
+            @ApiImplicitParam(name = "token",value ="token",required = true, dataType = "String")
+    })
+    @GetMapping("/authentication")
+    public Result<String> verifyToken(String token, Integer userId, HttpServletRequest request, HttpServletResponse response) {
+
+        return Result.error("用户身份校验失败！");
+    }
+
 
 
 }
