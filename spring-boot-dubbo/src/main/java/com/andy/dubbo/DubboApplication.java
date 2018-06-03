402fde4a6bc6339a16d7edc828c2a5609a233e8a
janlle <rethinkt@163.com>
2018-06-03 12:38:28 +0800

    add dubbo model

---
+++ spring-boot-dubbo/src/main/java/com/andy/dubbo/DubboApplication.java
@@ -0,0 +1,15 @@
+package com.andy.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Mr.ruoLin
+ * @createBy: 2018-06-03 09:29
+ **/
+@SpringBootApplication
+public class DubboApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DubboApplication.class, args);
+    }
+}
