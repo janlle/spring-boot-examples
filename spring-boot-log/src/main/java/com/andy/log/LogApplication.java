d7671a59f373eb22df74d16ba05f0e87219f76b4
leone <janlle@126.com>
2018-06-12 09:15:07 +0800

    add log model

---
+++ spring-boot-log/src/main/java/com/andy/log/LogApplication.java
@@ -0,0 +1,17 @@
+package com.andy.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author: Mr.ruoLin
+ * @createBy: 2018-05-09 18:13
+ **/
+@EnableScheduling
+@SpringBootApplication
+public class LogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogApplication.class, args);
+    }
+}
