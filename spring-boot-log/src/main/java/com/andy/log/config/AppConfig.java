c16b9d961f5a6896746d983530943f4b99a29600
leone <janlle@126.com>
2018-06-12 23:19:13 +0800

    add some code

---
+++ spring-boot-log/src/main/java/com/andy/log/config/AppConfig.java
@@ -0,0 +1,36 @@
+package com.andy.log.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-06-12 23:13
+ **/
+@EnableAsync
+@Configuration
+public class AppConfig {
+
+    //线程池初始数量大小
+    private static final int CORE_POOL_SIZE = 15;
+    private static final int MAX_POOL_SIZE = 100;
+    private static final int QUEUE_CAPACITY = 10;
+
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.initialize();
+        return executor;
+    }
+
+
+
+}
--- spring-boot-log/src/main/java/com/andy/log/task/LogTask.java
+++ spring-boot-log/src/main/java/com/andy/log/task/LogTask.java
@@ -2,6 +2,7 @@ package com.andy.log.task;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import java.util.Date;
 import java.util.Random;
 
 /**
+ * 模拟用户产生日志
  * @Author: Mr.lyon
  * @CreateBy: 2018-06-12 09:12
  **/
@@ -26,18 +28,22 @@ public class LogTask {
 
     private static LocalDateTime localDateTime = LocalDateTime.now();
 
+    @Async
     @Scheduled(fixedRate = 1000 * 2)
     public void timeTask() {
-        logger.info(new Date().getTime()+names[RANDOM.nextInt(names.length)]+"买了"+names[RANDOM.nextInt(goods.length)]+"----"+localDateTime.getDayOfYear());
+        logger.info(localDateTime.toLocalDate()+names[RANDOM.nextInt(names.length)]+"-买了-"+names[RANDOM.nextInt(goods.length)]+"-订单号是-"+System.currentTimeMillis());
     }
 
+    @Async
     @Scheduled(fixedDelay = 1000 * 5)
     public void DelayTask() {
         logger.info(new Date().getTime()+names[RANDOM.nextInt(names.length)]+"购买的"+names[RANDOM.nextInt(goods.length)]+"发货了！"+RANDOM.nextInt(5000)*10000);
     }
 
 
+
     //每5秒执行一次
+    @Async
     @Scheduled(cron = "0/3 * * * * ?")
     public void doSomething() throws Exception {
         logger.info(new Date().getTime()+names[RANDOM.nextInt(names.length)]+"---买了----"+names[RANDOM.nextInt(goods.length)]+"----"+RANDOM.nextInt(5000)*10000);
@@ -45,15 +51,22 @@ public class LogTask {
 
 
     // 每分钟执行一次
+    @Async
     @Scheduled(cron = "0 0/1 * * * ?")
     public void work() throws Exception {
         logger.info(new Date().getTime()+names[RANDOM.nextInt(names.length)]+"---买了----"+names[RANDOM.nextInt(goods.length)]+"----"+RANDOM.nextInt(5000)*10000);
     }
 
     // 每一小时执行一次
+    @Async
     @Scheduled(cron = "0 0 0/1 * * ? ")
     public void goWork() throws Exception {
         logger.info(new Date().getTime()+names[RANDOM.nextInt(names.length)]+"---买了----"+names[RANDOM.nextInt(goods.length)]+"----"+RANDOM.nextInt(5000)*10000);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(localDateTime.toLocalDate());
     }
 
 }
