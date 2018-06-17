c7d8d7ca38f0cc23e7d7e0155c1723135cf8c818
janlle <rethinkt@163.com>
2018-06-17 14:01:35 +0800

    add some code

---
+++ spring-boot-pay/src/main/java/com/andy/pay/common/HttpController.java
@@ -0,0 +1,103 @@
+package com.andy.pay.common;
+
+import com.other.common.model.Product;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-06-17 13:23
+ **/
+@Slf4j
+@Api(tags ="Http测试控制器")
+@RestController
+@RequestMapping(value = "/http")
+public class HttpController {
+
+    @ApiOperation(value="get请求")
+    @RequestMapping(value="/get",method= RequestMethod.GET)
+    public String get(HttpServletRequest request, HttpServletResponse response) {
+        log.info("get请求");
+        return "success";
+    }
+
+
+    @ApiOperation(value="post请求")
+    @RequestMapping(value="/post",method= RequestMethod.POST)
+    public Object post(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Map<String, String> map = new HashMap<>();
+        SAXReader reader = new SAXReader();
+        InputStream in = request.getInputStream();
+        Document document = reader.read(in);
+        Element root = document.getRootElement();
+        List<Element> list = root.elements();
+        for (Element e : list) {
+            map.put(e.getName(), e.getText());
+        }
+        in.close();
+        log.info("post请求");
+        return map;
+    }
+
+
+    @ApiOperation(value="put请求")
+    @RequestMapping(value="/put",method= RequestMethod.PUT)
+    public String put(HttpServletRequest request, HttpServletResponse response) {
+        log.info("put请求");
+        return "success";
+    }
+
+
+    @ApiOperation(value="delete请求")
+    @RequestMapping(value="/delete",method= RequestMethod.DELETE)
+    public String delete(HttpServletRequest request, HttpServletResponse response) {
+        log.info("delete请求");
+        return "success";
+    }
+
+
+    @ApiOperation(value="head请求")
+    @RequestMapping(value="/head",method= RequestMethod.HEAD)
+    public String head(HttpServletRequest request, HttpServletResponse response) {
+        log.info("head请求");
+        return "success";
+    }
+    @ApiOperation(value="options请求")
+    @RequestMapping(value="/options",method= RequestMethod.OPTIONS)
+    public String options(HttpServletRequest request, HttpServletResponse response) {
+        log.info("options请求");
+        return "success";
+    }
+
+    @ApiOperation(value="trace请求")
+    @RequestMapping(value="/trace",method= RequestMethod.TRACE)
+    public String trace(HttpServletRequest request, HttpServletResponse response) {
+        log.info("trace请求");
+        return "success";
+    }
+
+    @ApiOperation(value="patch请求")
+    @RequestMapping(value="/patch",method= RequestMethod.PATCH)
+    public String patch(HttpServletRequest request, HttpServletResponse response) {
+        log.info("patch请求");
+        return "success";
+    }
+
+
+
+}
--- spring-boot-pay/src/main/java/com/andy/pay/modules/weixin/service/WeChatPayService.java
+++ spring-boot-pay/src/main/java/com/andy/pay/modules/weixin/service/WeChatPayService.java
@@ -2,6 +2,8 @@ package com.andy.pay.modules.weixin.service;
 
 import com.andy.pay.common.enums.OrderStatusEnum;
 import com.andy.pay.common.property.AppProperty;
+import com.andy.pay.common.utils.HttpUtils;
+import com.andy.pay.common.utils.QRCodeUtil;
 import com.andy.pay.mapper.OrderMapper;
 import com.andy.pay.mapper.UserMapper;
 import com.andy.pay.modules.weixin.util.WeChatPayUtil;
@@ -13,6 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -26,14 +31,23 @@ public class WeChatPayService {
     @Autowired
     private OrderMapper orderMapper;
 
-
     @Autowired
     private UserMapper userMapper;
 
     @Autowired
     private AppProperty appProperty;
 
+    private static final String URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+
+    /**
+     * App支付
+     * @author: Mr.lyon
+     * @createBy: 2018-06-17 12:29
+     * @params: [request, orderId]
+     * @return: void
+     **/
     public void payHandler(HttpServletRequest request, String orderId) {
+
         List<Order> orderList = orderMapper.selectByOrderId(orderId);
         Order order = orderList.get(0);
         if (order == null) {
@@ -53,7 +67,7 @@ public class WeChatPayService {
         params.put("total_fee", order.getTotalFee().toString());             //总金额(分)
         params.put("spbill_create_ip", order.getCreateIp());                 //订单生成的机器IP，指用户浏览器端IP
         params.put("notify_url", appProperty.getWeChat().getNotifyUrl());    //回调url
-        params.put("trade_type", "APP");                                     // 交易类型JSAPI--公众号支付、NATIVE--原生扫码支付、APP--app支付
+        params.put("trade_type", "APP");                                     // 交易类型:JSAPI=公众号支付、NATIVE=扫码支付、APP=app支付
         String sign = WeChatUtil.createSign("UTF-8", params, appProperty.getWeChat().getApiKey());
         params.put("sign", "sign");
         String xmlData = WeChatUtil.mapToXml(params);
@@ -96,6 +110,62 @@ public class WeChatPayService {
     }
 
 
+
+    /**
+     * 微信扫码支付传入的价格为--元
+     * @author: Mr.lyon
+     * @createBy: 2018-06-15 11:46
+     * @params: [price, userId]
+     * @return: void
+     **/
+    public boolean wxQRCodePay(String price, Integer storeId, HttpServletResponse response, HttpServletRequest request) throws Exception {
+
+        String nonceStr = System.currentTimeMillis() + new Random().nextInt(9) + "";
+
+        Integer intPrice = Integer.valueOf(price);
+        String totalFee = intPrice * 1 + "";
+        String outTradeNo = System.currentTimeMillis() + "";
+        String spbillCreateIp = "45.123.89.22";
+        String notifyUrl = "http://rllin.nat300.top/api/cms/code/weChatNotify";
+
+        SortedMap<String, String> params = new TreeMap();
+        params.put("appid", appProperty.getWeChat().getAppid());
+        params.put("mch_id", appProperty.getWeChat().getMchId());
+        params.put("nonce_str", nonceStr);
+        params.put("body", "微信扫码支付");
+        params.put("out_trade_no", outTradeNo);
+        params.put("total_fee", totalFee);
+        params.put("spbill_create_ip", spbillCreateIp);
+        params.put("notify_url", notifyUrl);
+        params.put("trade_type", "NATIVE");
+        String sign = WeChatUtil.createSign("UTF-8", params, appProperty.getWeChat().getApiKey());
+        params.put("sign", sign);
+        String requestXML = WeChatUtil.mapToXml(params);
+        String responseXml = HttpUtils.sendPostXml(URL, requestXML);
+        Map<String, String> mapResult = WeChatUtil.xmlToMap(responseXml);
+        log.info("微信相应结果为:{}", mapResult);
+        //return_code为微信返回的状态码，SUCCESS表示成功，return_msg 如非空，为错误原因 签名失败 参数格式校验错误
+        if (mapResult.get("return_code").toString().equalsIgnoreCase("SUCCESS") && mapResult.get("result_code").toString().equalsIgnoreCase("SUCCESS")) {
+            log.info("*****************发起预支付成功*********************");
+            QRCodeUtil.createQRCode(mapResult.get("code_url"), response);
+            //TODO保存订单信息
+            return true;
+        } else {
+            log.info("*****************发起预支付失败*********************");
+            return false;
+        }
+    }
+
+    public void notifyOrder(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //TODO保存支付订单信息
+        log.info("收到微信支付回调，保存支付订单信息");
+        String resXml = "<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>";
+        BufferedOutputStream out = new BufferedOutputStream(response.getOutputStream());
+        out.write(resXml.getBytes());
+        out.flush();
+        out.close();
+        log.info("向微信响应收到支付回调成功");
+    }
 
 
 }
