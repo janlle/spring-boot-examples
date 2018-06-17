b89889107623be769b7a0dc098ebb75a320bd71a
leone <exklin@gmail.com>
2018-06-17 15:53:38 +0800

    add some code

--- spring-boot-pay/src/main/java/com/andy/pay/common/utils/AppUtils.java
+++ spring-boot-pay/src/main/java/com/andy/pay/common/utils/AppUtils.java
@@ -1,11 +1,22 @@
 package com.andy.pay.common.utils;
 
 import lombok.extern.slf4j.Slf4j;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.util.*;
 
 /**
  * 工程中常用工具
@@ -31,6 +42,186 @@ public class AppUtils {
             e.printStackTrace();
         }
         return null;
+    }
+
+    /**
+     * 生成md5签名的方法
+     * @author: Mr.lyon
+     * @createBy: 2018/6/3 14:59
+     * @params: [charset, params, apiKey]
+     * @return: java.lang.String
+     **/
+    public static String createSign(String charset, Map params, String apiKey) {
+        StringBuffer sb = new StringBuffer();
+        Set es = params.entrySet();
+        Iterator it = es.iterator();
+        while (it.hasNext()) {
+            Map.Entry entry = (Map.Entry) it.next();
+            String k = (String) entry.getKey();
+            String v = (String) entry.getValue();
+            if (null != v && !"".equals(v) && !"sign".equals(k) && !"key".equals(k)) {
+                sb.append(k + "=" + v + "&");
+            }
+        }
+        sb.append("key=" + apiKey);
+        return MD5Encode(sb.toString(), charset).toUpperCase();
+    }
+
+    /**
+     * 生成MD5摘要算法
+     * @author: Mr.lyon
+     * @createBy: 2018/6/3 15:00
+     * @params: [message, charset]
+     * @return: java.lang.String
+     **/
+    public static String MD5Encode(String message, String charset) {
+        String result = null;
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            byte[] buff;
+            if (charset == null || "".equals(charset)) {
+                buff = messageDigest.digest(message.getBytes());
+            } else {
+                buff = messageDigest.digest(message.getBytes(charset));
+            }
+            StringBuffer sb = new StringBuffer();
+            int digital;
+            for (int i = 0; i < buff.length; i++) {
+                digital = buff[i];
+                if(digital < 0) {
+                    digital += 256;
+                }
+                if(digital < 16){
+                    sb.append("0");
+                }
+                sb.append(Integer.toHexString(digital));
+            }
+            result = sb.toString().toUpperCase();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 生成 HMAC_SHA256
+     * @author: Mr.lyon
+     * @createBy: 2018/6/3 15:01
+     * @params: [data, key]
+     * @return: java.lang.String
+     **/
+    public static String HMAC_SHA256(String data, String key) throws Exception {
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+        byte[] array = sha256_HMAC.doFinal(data.getBytes("UTF-8"));
+        StringBuilder sb = new StringBuilder();
+        for (byte item : array) {
+            sb.append(Integer.toHexString((item & 0xFF) | 0x100).substring(1, 3));
+        }
+        return sb.toString().toUpperCase();
+    }
+
+
+    /**
+     * XML格式字符串转换为Map
+     * @author: Mr.lyon
+     * @createBy: 2018/6/3 15:02
+     * @params: [xmlStr]
+     * @return: java.util.Map
+     **/
+    public static Map xmlToMap(String xmlStr)  {
+        try (InputStream inputStream = new ByteArrayInputStream(xmlStr.getBytes("UTF-8"))) {
+            Map<String, String> data = new HashMap<>();
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document doc = documentBuilder.parse(inputStream);
+            doc.getDocumentElement().normalize();
+            NodeList nodeList = doc.getDocumentElement().getChildNodes();
+            for (int idx = 0; idx < nodeList.getLength(); ++idx) {
+                Node node = nodeList.item(idx);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    org.w3c.dom.Element element = (org.w3c.dom.Element) node;
+                    data.put(element.getNodeName(), element.getTextContent());
+                }
+            }
+            return data;
+        } catch (Exception ex) {
+            log.warn("xml转换map异常,message: {}. XML content: {}", ex.getMessage(), xmlStr);
+        }
+        return null;
+    }
+
+
+    /**
+     * map转换为xml字符串
+     * @author: Mr.lyon
+     * @createBy: 2018/6/3 15:02
+     * @params: [params]
+     * @return: java.lang.String
+     **/
+    public static String mapToXml(Map params) {
+        StringBuilder sb = new StringBuilder();
+        Set set = params.entrySet();
+        Iterator it = set.iterator();
+        sb.append("<xml>\n");
+        while (it.hasNext()) {
+            Map.Entry entry = (Map.Entry) it.next();
+            String k = (String) entry.getKey();
+            Object v = entry.getValue();
+            sb.append("<" + k + ">").append(v).append("</" + k + ">\n");
+        }
+        sb.append("</xml>");
+        return sb.toString();
+    }
+
+    /**
+     * 生成32位随机数字
+     * @author: Mr.lyon
+     * @createBy: 2018/6/3 15:02
+     * @params: []
+     * @return: java.lang.String
+     **/
+    public static String genNonceStr() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    /**
+     * 获取当前时间戳，单位秒(10位)
+     * @author: Mr.lyon
+     * @createBy: 2018/6/3 15:02
+     * @params: []
+     * @return: java.lang.String
+     **/
+    public static long getTimestamp() {
+        return System.currentTimeMillis() / 1000;
+    }
+
+    /**
+     * 生成32位字符串
+     * @author: Mr.lyon
+     * @createBy: 2018/6/3 15:02
+     * @params: []
+     * @return: java.lang.String
+     **/
+    public static String generateNum(int length) {
+        StringBuffer result = new StringBuffer();
+        final String sources = "0123456789";
+        if (length < 0 && length > 512) {
+            return null;
+        }
+        Random rand = new Random();
+        for (int i = 0; i < length; i++) {
+            result.append(sources.charAt(rand.nextInt(9)) + "");
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(genNonceStr());
+        System.out.println(getTimestamp());
+        System.out.println(generateNum(6));
+        System.out.println(MD5Encode("hello", "UTF-8"));
     }
 
     public static String getIp(HttpServletRequest request) {
--- spring-boot-pay/src/main/java/com/andy/pay/modules/weixin/service/WeChatPayService.java
+++ spring-boot-pay/src/main/java/com/andy/pay/modules/weixin/service/WeChatPayService.java
@@ -2,15 +2,20 @@ package com.andy.pay.modules.weixin.service;
 
 import com.andy.pay.common.enums.OrderStatusEnum;
 import com.andy.pay.common.property.AppProperty;
+import com.andy.pay.common.utils.AppUtils;
 import com.andy.pay.common.utils.HttpUtils;
 import com.andy.pay.common.utils.QRCodeUtil;
 import com.andy.pay.mapper.OrderMapper;
 import com.andy.pay.mapper.UserMapper;
-import com.andy.pay.modules.weixin.util.WeChatPayUtil;
-import com.andy.pay.modules.weixin.util.WeChatUtil;
 import com.andy.pay.object.entity.Order;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,9 +40,11 @@ public class WeChatPayService {
     private UserMapper userMapper;
 
     @Autowired
-    private AppProperty appProperty;
+    private static AppProperty appProperty;
 
     private static final String URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+
+    private static final String REFUND_URL = "https://api.mch.weixin.qq.com/secapi/pay/refund";
 
     /**
      * App支付
@@ -61,20 +68,20 @@ public class WeChatPayService {
         Map<String, String> params = new HashMap<>();
         params.put("appid", appProperty.getWeChat().getAppid());             //appId
         params.put("mch_id", appProperty.getWeChat().getMchId());            //微信支付商户号
-        params.put("nonce_str", WeChatUtil.genNonceStr());                   //随机字符串
+        params.put("nonce_str", AppUtils.genNonceStr());                     //随机字符串
         params.put("body", "App weChat pay!");                               //商品描述
         params.put("out_trade_no", order.getOutTradeNum());                  //商户订单号
         params.put("total_fee", order.getTotalFee().toString());             //总金额(分)
         params.put("spbill_create_ip", order.getCreateIp());                 //订单生成的机器IP，指用户浏览器端IP
         params.put("notify_url", appProperty.getWeChat().getNotifyUrl());    //回调url
-        params.put("trade_type", "APP");                                     // 交易类型:JSAPI=公众号支付、NATIVE=扫码支付、APP=app支付
-        String sign = WeChatUtil.createSign("UTF-8", params, appProperty.getWeChat().getApiKey());
+        params.put("trade_type", "APP");                                     // 交易类型:JS_API=公众号支付、NATIVE=扫码支付、APP=app支付
+        String sign = AppUtils.createSign("UTF-8", params, appProperty.getWeChat().getApiKey());
         params.put("sign", "sign");
-        String xmlData = WeChatUtil.mapToXml(params);
+        String xmlData = AppUtils.mapToXml(params);
         log.info("xmlData:{}", xmlData);
         String wxRetXmlData = HttpUtils.sendPostXml(appProperty.getWeChat().getUrl().getPayUrl(), xmlData);
         log.info("微信返回数据:{}", wxRetXmlData);
-        Map<String, String> retData = WeChatUtil.xmlToMap(wxRetXmlData);
+        Map<String, String> retData = AppUtils.xmlToMap(wxRetXmlData);
         log.info("微信返回信息:{}", retData);
     }
 
@@ -91,13 +98,13 @@ public class WeChatPayService {
             throw new RuntimeException("订单不存在！");
         }
 
-        String refundNo = WeChatUtil.genNonceStr();
-        String param = WeChatPayUtil.getWxRefundParam("", "", "");
+        String refundNo = AppUtils.genNonceStr();
+        String param = getWxRefundParam("", "", "");
         if (param == null) {
             throw new RuntimeException("参数封装错误！");
         }
         try {
-            if (WeChatPayUtil.wxRefund(param)) {
+            if (wxRefund(param)) {
                 //TODO退款成功
             } else {
                 //退款失败
@@ -138,11 +145,11 @@ public class WeChatPayService {
         params.put("spbill_create_ip", spbillCreateIp);
         params.put("notify_url", notifyUrl);
         params.put("trade_type", "NATIVE");
-        String sign = WeChatUtil.createSign("UTF-8", params, appProperty.getWeChat().getApiKey());
+        String sign = AppUtils.createSign("UTF-8", params, appProperty.getWeChat().getApiKey());
         params.put("sign", sign);
-        String requestXML = WeChatUtil.mapToXml(params);
+        String requestXML = AppUtils.mapToXml(params);
         String responseXml = HttpUtils.sendPostXml(URL, requestXML);
-        Map<String, String> mapResult = WeChatUtil.xmlToMap(responseXml);
+        Map<String, String> mapResult = AppUtils.xmlToMap(responseXml);
         log.info("微信相应结果为:{}", mapResult);
         //return_code为微信返回的状态码，SUCCESS表示成功，return_msg 如非空，为错误原因 签名失败 参数格式校验错误
         if (mapResult.get("return_code").toString().equalsIgnoreCase("SUCCESS") && mapResult.get("result_code").toString().equalsIgnoreCase("SUCCESS")) {
@@ -167,5 +174,61 @@ public class WeChatPayService {
         log.info("向微信响应收到支付回调成功");
     }
 
+    /**微信退参数拼装
+     * @author: Mr.lyon
+     * @createBy: 2018/6/3 15:04
+     * @params: [wxOrderId, outTradeNum, totalFee]
+     * @return: java.lang.String
+     **/
+    public String getWxRefundParam(String wxOrderId, String outTradeNum,String totalFee) {
+        String data;
+        try {
+            String nonceStr = AppUtils.genNonceStr();
+            SortedMap<String, String> params = new TreeMap<>();
+            params.put("appid", appProperty.getWeChat().getAppid());
+            params.put("mch_id", appProperty.getWeChat().getMchId());
+            params.put("nonce_str", nonceStr);
+            params.put("transaction_id", wxOrderId);
+            params.put("out_trade_no", outTradeNum);
+            params.put("total_fee", totalFee);
+            params.put("refund_fee", totalFee);
+            params.put("sign", AppUtils.createSign("UTF-8", params, appProperty.getWeChat().getApiKey()));
+            data = AppUtils.mapToXml(params);
+        } catch (Exception e) {
+            log.error("微信退款参数封装异常！");
+            return null;
+        }
+        return data;
+    }
+
+    /**微信退款
+     * @author: Mr.lyon
+     * @createBy: 2018/6/3 15:05
+     * @params: [xmlData]
+     * @return: void
+     **/
+    public static boolean wxRefund(String xmlData) throws Exception {
+        log.info("****************微信退款开始****************");
+        CloseableHttpClient httpClient = HttpUtils.sslHttpsClient(appProperty.getWeChat().getCertPath(), appProperty.getWeChat().getMchId());
+        HttpPost httpPost = new HttpPost(REFUND_URL);
+        StringEntity entity = new StringEntity(xmlData.toString(), "UTF-8");
+        httpPost.setEntity(entity);
+        HttpResponse response = httpClient.execute(httpPost);
+        HttpEntity responseData = response.getEntity();
+        String result = EntityUtils.toString(responseData, "UTF-8");
+
+        log.info("responseXml:{}", result);
+        Map<String, String> mapData = AppUtils.xmlToMap(result);
+        log.info("responseMap:{}", mapData);
+        //return_code为微信返回的状态码，SUCCESS表示申请退款成功，return_msg 如非空，为错误原因 签名失败 参数格式校验错误
+        if (mapData.get("return_code").toString().equalsIgnoreCase("SUCCESS")) {
+            log.info("****************退款申请成功**********************");
+            //修改订单状态为退款
+            return true;
+        } else {
+            log.info("*****************退款申请失败*********************");
+            return false;
+        }
+    }
 
 }
