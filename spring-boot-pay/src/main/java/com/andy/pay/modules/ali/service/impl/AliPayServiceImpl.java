2722bfa4b67cdbd17aca7ac434d42cb25152fb9f
janlle <rethinkt@163.com>
2018-06-17 15:53:38 +0800

    add some code

--- spring-boot-pay/src/main/java/com/andy/pay/modules/ali/service/impl/AliPayServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.andy.pay.modules.ali.service.impl;
-
-import com.andy.pay.modules.ali.service.IAliPayService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AliPayServiceImpl implements IAliPayService {
-
-}
