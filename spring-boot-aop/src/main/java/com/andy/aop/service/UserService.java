b7753305f1b37a8cdd6190b022f7053356024e07
leone <exklin@gmail.com>
2018-06-21 00:19:58 +0800

    add new model aop

---
+++ spring-boot-aop/src/main/java/com/andy/aop/service/UserService.java
@@ -0,0 +1,9 @@
+package com.andy.aop.service;
+
+import com.andy.aop.entity.User;
+
+public interface UserService {
+
+    User user(Long id);
+
+}
---
+++ spring-boot-aop/src/main/java/com/andy/aop/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.andy.aop.service.impl;
+
+import com.andy.aop.entity.User;
+import com.andy.aop.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-06-21 00:14
+ **/
+@Slf4j
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User user(Long id) {
+
+        return null;
+    }
+}
