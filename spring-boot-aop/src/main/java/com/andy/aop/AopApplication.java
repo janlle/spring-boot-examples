cad0355cedebab735312a3e2d3ff4600e05c047c
leone <janlle@126.com>
2018-06-21 00:19:58 +0800

    add new model aop

---
+++ spring-boot-aop/src/main/java/com/andy/aop/AopApplication.java
@@ -0,0 +1,15 @@
+package com.andy.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-06-21 00:09
+ **/
+@SpringBootApplication
+public class AopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AopApplication.class, args);
+    }
+}
