1aa7ba785d5cafaabc6e74e8e3088099bb1f811b
leone <exklin@gmail.com>
2018-06-28 22:26:44 +0800

    update some file

--- spring-boot-concurrency/src/main/java/com/andy/concurrency/controller/TestController.java
+++ spring-boot-concurrency/src/main/java/com/andy/concurrency/controller/TestController.java
@@ -1,23 +1,25 @@
 package com.andy.concurrency.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author: Mr.ruoLin
  * @createBy: 2018-04-22 15:17
  **/
 @Slf4j
-@Controller
+@RestController
 public class TestController {
+
+    private int a = 0;
 
     @ResponseBody
     @RequestMapping("/test")
-    public String test(){
-        log.info("test method...");
-        return "test";
+    public void test(){
+        log.info("test method...a={}", a);
+        a = a++;
     }
 
 
