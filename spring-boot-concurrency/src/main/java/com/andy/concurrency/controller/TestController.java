2e15a7570171d05b43185808505573bd6a659152
leone <janlle@126.com>
2018-06-28 22:47:51 +0800

    update some file

--- spring-boot-concurrency/src/main/java/com/andy/concurrency/controller/TestController.java
+++ spring-boot-concurrency/src/main/java/com/andy/concurrency/controller/TestController.java
@@ -1,9 +1,15 @@
 package com.andy.concurrency.controller;
 
+import com.andy.concurrency.example.commonUnsafe.HashMapExample;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
 /**
  * @author: Mr.ruoLin
@@ -13,16 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private static Map<Integer, Boolean> data = new HashMap<>();
+
+    Stack stack = new Stack();
+
+    {
+        for (int i = 0; i < 10000; i++) {
+            data.put(i, true);
+            stack.push(i);
+        }
+        log.info("初始化map...");
+    }
+
     private int a = 0;
 
     @ResponseBody
     @RequestMapping("/test")
     public void test(){
-        log.info("test method...a={}", a);
-        a = a++;
+        log.info("test method...a={}", stack.pop());
     }
-
-
-
 
 }
