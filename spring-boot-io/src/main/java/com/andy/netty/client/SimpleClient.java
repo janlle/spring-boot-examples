03964bf9f5ff25d4d4bc2c7a0fb4c6dae50d05ea
leone <janlle@126.com>
2018-06-29 21:33:38 +0800

    add some code

---
+++ spring-boot-io/src/main/java/com/andy/netty/client/SimpleClient.java
@@ -0,0 +1,12 @@
+package com.andy.netty.client;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-06-29 21:10
+ **/
+public class SimpleClient {
+
+
+
+
+}
---
+++ spring-boot-io/src/main/java/com/andy/netty/server/SimpleServerHandler.java
@@ -0,0 +1,13 @@
+package com.andy.netty.server;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-06-29 21:25
+ **/
+public class SimpleServerHandler extends ChannelInboundHandlerAdapter {
+
+
+
+}
