a45e08db535447c477c381afbd16951b7ed2e811
lyon <rethinkt@163.com>
2018-06-29 21:01:59 +0800

    update new model

---
+++ spring-boot-io/src/main/java/com/andy/IOApplication.java
@@ -0,0 +1,15 @@
+package com.andy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-06-29 20:58
+ **/
+@SpringBootApplication
+public class IOApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IOApplication.class, args);
+    }
+}
