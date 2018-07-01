98183934a887c42b8f5cedcea1f42d1f17a65650
leone <janlle@126.com>
2018-07-01 11:45:05 +0800

    add some code

--- spring-boot-spring/src/main/java/com/andy/spring/App.java
+++ spring-boot-spring/src/main/java/com/andy/spring/App.java
@@ -1,9 +1,14 @@
 package com.andy.spring;
 
 import com.andy.spring.beans.Person;
+import com.andy.spring.beans.Student;
+import com.andy.spring.scan.Teacher;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * @Author: Mr.lyon
@@ -15,13 +20,17 @@ public class App {
 
 //        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("D:\\spring.xml");
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+//        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("spring.xml"));
 
-        System.out.println(applicationContext.getApplicationName());
-        System.out.println(applicationContext.getDisplayName());
-        System.out.println(applicationContext.getId());
-        System.out.println(applicationContext.getStartupDate());
+        System.out.println("getApplicationName:" + applicationContext.getApplicationName());
+        System.out.println("getDisplayName:" + applicationContext.getDisplayName());
+        System.out.println("getId:" + applicationContext.getId());
+        System.out.println("getStartupDate:" + applicationContext.getStartupDate());
 
-        Person person1 = (Person) applicationContext.getBean("person");
+        Person person1 = applicationContext.getBean("001", Person.class);
+        Teacher teacher = applicationContext.getBean("teach", Teacher.class);
+        Student student1 = (Student) applicationContext.getBean("student");
+
 
         person1.hello("james");
 
