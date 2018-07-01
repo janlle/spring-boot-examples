cd7278758466bd769245d8b8760834181685cca4
leone <exklin@gmail.com>
2018-07-01 11:45:05 +0800

    add some code

--- spring-boot-spring/src/main/java/com/andy/spring/config/SpringConfig.java
+++ spring-boot-spring/src/main/java/com/andy/spring/config/SpringConfig.java
@@ -1,8 +1,22 @@
 package com.andy.spring.config;
 
+import com.andy.spring.beans.Student;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @Author: Mr.lyon
  * @CreateBy: 2018-07-01 11:17
  **/
+@Configuration
 public class SpringConfig {
+
+
+    @Bean
+    public Student student() {
+        System.out.println("Java config init");
+        return new Student();
+    }
+
+
 }
