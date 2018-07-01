59261357883ecbbd8414c86225cf76603b997b53
leone <janlle@126.com>
2018-07-01 13:57:25 +0800

    add some code

--- spring-boot-security/src/main/java/com/andy/security/encrypt/AESDemo.java
+++ spring-boot-security/src/main/java/com/andy/security/encrypt/AESDemo.java
@@ -19,115 +19,75 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Hex;
 
 public class AESDemo {
-    
+
     private static String src = "TestAES";
-    
-    public static void jdkAES (){
+
+    public static void jdkAES() {
         try {
-            
-            
             //生成Key
             KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-            keyGenerator.init(128);  
+            keyGenerator.init(128);
             //keyGenerator.init(128, new SecureRandom("seedseedseed".getBytes()));  
             //使用上面这种初始化方法可以特定种子来生成密钥，这样加密后的密文是唯一固定的。
             SecretKey secretKey = keyGenerator.generateKey();
             byte[] keyBytes = secretKey.getEncoded();
-            
+
             //Key转换
             Key key = new SecretKeySpec(keyBytes, "AES");
-            
+
             //加密
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, key);
             byte[] encodeResult = cipher.doFinal(AESDemo.src.getBytes());
-            System.out.println("AESencode : " + Hex.toHexString(encodeResult) );
-            
+            System.out.println("AESencode : " + Hex.toHexString(encodeResult));
+
             //解密
             cipher.init(Cipher.DECRYPT_MODE, key);
             byte[] decodeResult = cipher.doFinal(encodeResult);
-            System.out.println("AESdecode : " + new String (decodeResult));
-            
-            
-        
-        
-        } catch (NoSuchAlgorithmException e) {
-            // TODO 自动生成的 catch 块
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            // TODO 自动生成的 catch 块
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            // TODO 自动生成的 catch 块
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            // TODO 自动生成的 catch 块
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            // TODO 自动生成的 catch 块
+            System.out.println("AESdecode : " + new String(decodeResult));
+
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    
+
     }
-    
-    
-    public static void bcAES (){
+
+
+    public static void bcAES() {
         try {
-            
             //使用BouncyCastle 的DES加密
             Security.addProvider(new BouncyCastleProvider());
-            
             //生成Key
-            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES","BC");
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES", "BC");
             keyGenerator.getProvider();
-            keyGenerator.init(128);  
+            keyGenerator.init(128);
             SecretKey secretKey = keyGenerator.generateKey();
             byte[] keyBytes = secretKey.getEncoded();
-            
+
             //Key转换
             Key key = new SecretKeySpec(keyBytes, "AES");
-            
             //加密
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, key);
             byte[] encodeResult = cipher.doFinal(AESDemo.src.getBytes());
-            System.out.println("AESencode : " + Hex.toHexString(encodeResult) );
-            
+            System.out.println("AESencode : " + Hex.toHexString(encodeResult));
+
             //解密
             cipher.init(Cipher.DECRYPT_MODE, key);
             byte[] decodeResult = cipher.doFinal(encodeResult);
-            System.out.println("AESdecode : " + new String (decodeResult));
-            
-            
-        
-        
-        } catch (NoSuchAlgorithmException e) {
-            // TODO 自动生成的 catch 块
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            // TODO 自动生成的 catch 块
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            // TODO 自动生成的 catch 块
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            // TODO 自动生成的 catch 块
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            // TODO 自动生成的 catch 块
-            e.printStackTrace();
-        } catch (NoSuchProviderException e) {
-            // TODO 自动生成的 catch 块
+            System.out.println("AESdecode : " + new String(decodeResult));
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    
+
     }
-    
+
 
     public static void main(String[] args) {
         jdkAES();
         bcAES();
-
     }
+
 
 }
--- spring-boot-security/src/main/java/com/andy/security/encrypt/Base64Demo.java
+++ spring-boot-security/src/main/java/com/andy/security/encrypt/Base64Demo.java
@@ -6,53 +6,53 @@ import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 public class Base64Demo {
-    
+
     private static String src = "aced";
-    
+
     public static void main(String[] args) {
         Base64Demo.jdkBase64();
-        Base64Demo.commonsCodecBase64 ();
-        Base64Demo.bouncyCastleBase64 ();
+        Base64Demo.commonsCodecBase64();
+        Base64Demo.bouncyCastleBase64();
     }
-    
+
     //使用JDK的base64实现，
-    public static void jdkBase64 (){
+    public static void jdkBase64() {
         BASE64Encoder encoder = new BASE64Encoder();
         String encode = encoder.encode(Base64Demo.src.getBytes());
         System.out.println("encode:  " + encode);
-        
+
         BASE64Decoder decoder = new BASE64Decoder();
         try {
-            String decode = new String ( decoder.decodeBuffer(encode));
+            String decode = new String(decoder.decodeBuffer(encode));
             System.out.println("decode:  " + decode);
         } catch (IOException e) {
             e.printStackTrace();
-        }    
+        }
     }
-    
-    
+
+
     //使用apache的commonsCodec实现
-    public static void commonsCodecBase64 (){
+    public static void commonsCodecBase64() {
         byte[] encodeBytes = org.apache.commons.codec.binary.Base64.encodeBase64(Base64Demo.src.getBytes());
-        String encode = new String (encodeBytes);
+        String encode = new String(encodeBytes);
         System.out.println("encode:  " + encode);
-        
+
         byte[] decodeBytes = org.apache.commons.codec.binary.Base64.decodeBase64(encode);
         String decode = new String(decodeBytes);
         System.out.println("decode:  " + decode);
-        
+
     }
-    
+
     //使用bouncyCastlede实现
-    public static void bouncyCastleBase64 () {
-        byte[] encodeBytes = org.bouncycastle.util.encoders.Base64.encode(Base64Demo.src.getBytes()) ;
-        String encode = new String (encodeBytes);
+    public static void bouncyCastleBase64() {
+        byte[] encodeBytes = org.bouncycastle.util.encoders.Base64.encode(Base64Demo.src.getBytes());
+        String encode = new String(encodeBytes);
         System.out.println("encode:  " + encode);
 
         byte[] decodeBytes = org.bouncycastle.util.encoders.Base64.decode(encode);
         String decode = new String(decodeBytes);
         System.out.println("decode:  " + decode);
-        
+
     }
 
 }
