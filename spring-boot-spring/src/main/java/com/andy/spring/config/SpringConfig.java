e9c0ecf9c49fd5d500bd3ca0b76bef3aaa32f62f
leone <exklin@gmail.com>
2018-07-01 11:55:17 +0800

    add some code

--- spring-boot-spring/src/main/java/com/andy/spring/config/SpringConfig.java
+++ spring-boot-spring/src/main/java/com/andy/spring/config/SpringConfig.java
@@ -1,6 +1,9 @@
 package com.andy.spring.config;
 
 import com.andy.spring.beans.Student;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,13 +12,24 @@ import org.springframework.context.annotation.Configuration;
  * @CreateBy: 2018-07-01 11:17
  **/
 @Configuration
-public class SpringConfig {
+public class SpringConfig implements ApplicationContextAware {
 
+    private static ApplicationContext applicationContext;
 
     @Bean
     public Student student() {
         System.out.println("Java config init");
         return new Student();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
     }
 
 
