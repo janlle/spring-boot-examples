d2b37ff176f5d3525a9ea6712dfa41aa26ab2646
leone <janlle@126.com>
2018-07-01 12:01:33 +0800

    add some code

--- spring-boot-mvc/src/main/java/com/andy/mvc/event/EventPublisher.java
+++ spring-boot-mvc/src/main/java/com/andy/mvc/event/EventPublisher.java
@@ -5,14 +5,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EventPubLisher {
-
+public class EventPublisher {
 
     @Autowired
     private ApplicationContext applicationContext;
 
     // 事件发布方法
-    public void pushListener(String msg) {
+    public void publish(String msg) {
         applicationContext.publishEvent(new AppEvent(this, msg));
     }
 
