d4ee248727ce2ca05481876a5a3b17cd0c456333
leone <exklin@gmail.com>
2018-07-01 15:03:47 +0800

    add some code

--- spring-boot-security/src/main/java/com/andy/security/encrypt/MD.java
+++ spring-boot-security/src/main/java/com/andy/security/encrypt/MD.java
@@ -1,21 +1,25 @@
 package com.andy.security.encrypt;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
 import java.util.Scanner;
 
-/*
- * 验证MD5
- * 1.初始化MessageDigest信息摘要对象
- * 2.传入需要计算的字符串更新摘要信息
- * 3.计算信息摘要
- * 4.将byte[] 转换为找度为32位的16进制字符串
+/**
+ * 消息摘要算法--MD
+ *
  * @Author: Mr.lyon
- * @CreateBy: 2018-07-01 14:02
+ * @CreateBy: 2018-07-01 14:36
  **/
 public class MD {
 
     /**
-     * 生成md5 有传入参数字符串
+     * 1.初始化MessageDigest信息摘要对象
+     * 2.传入需要计算的字符串更新摘要信息
+     * 3.计算信息摘要
+     * 4.将byte[] 转换为找度为32位的16进制字符串
      *
      * @author: Mr.lyon
      * @createBy: 2018-07-01 14:02
@@ -46,18 +50,51 @@ public class MD {
     }
 
 
+    /**
+     * @author: Mr.lyon
+     * @createBy: 2018-07-01 14:52
+     * @params: [content]
+     * @return: java.lang.String
+     **/
     public static String generateMD2(String content) {
-
-
-        return null;
+        StringBuffer sb = new StringBuffer();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD2");
+            byte[] hashCode = messageDigest.digest(content.getBytes());
+            for (byte b : hashCode) {
+                sb.append(Character.forDigit(b >> 4 & 0xf, 16));
+                sb.append(Character.forDigit(b & 0xf, 16));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 
+
+    //Bouncy Castle实现MD4加密
+    public static String generateMD4(String content) {
+        StringBuffer sb = new StringBuffer();
+        //通过这种方式给JDK动态添加一个provider,就可以通过这种方式获得JDK本身不支持的MD4了
+        try {
+            Security.addProvider(new BouncyCastleProvider());
+            MessageDigest messageDigest = MessageDigest.getInstance("MD4");
+            byte[] hashCode = messageDigest.digest(content.getBytes());
+            for (byte b : hashCode) {
+                sb.append(Character.forDigit(b >> 4 & 0xf, 16));
+                sb.append(Character.forDigit(b & 0xf, 16));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("请输入要加密的内容:");
         String input = scanner.nextLine();
-        String result = MD.generateMD5(input);
+        String result = MD.generateMD4(input);
         System.out.println(result);
     }
 
