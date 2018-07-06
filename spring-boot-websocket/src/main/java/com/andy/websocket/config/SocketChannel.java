2ca14bc8bb8cf8e86ea0eff42811fb5b25d7e5eb
leone <janlle@126.com>
2018-07-06 21:47:23 +0800

    update some code

---
+++ spring-boot-websocket/src/main/java/com/andy/websocket/config/SocketChannel.java
@@ -0,0 +1,112 @@
+package com.andy.websocket.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-07-06 21:39
+ **/
+@Slf4j
+@Component
+@ServerEndpoint(value = "/ws")
+public class SocketChannel {
+
+    //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
+    private static int onlineCount = 0;
+
+    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
+    private static CopyOnWriteArraySet<SocketChannel> socketChannel = new CopyOnWriteArraySet<>();
+
+    //与某个客户端的连接会话，需要通过它来给客户端发送数据
+    private Session session;
+
+    /**
+     * 连接建立成功调用的方法
+     */
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+        socketChannel.add(this);     //加入set中
+        addOnlineCount();           //在线数加1
+        log.info("有新连接加入！当前在线人数为" + getOnlineCount());
+        try {
+            sendMessage("hello collect init");
+        } catch (IOException e) {
+            log.info("IO异常");
+        }
+    }
+
+    /**
+     * 连接关闭调用的方法
+     */
+    @OnClose
+    public void onClose() {
+        socketChannel.remove(this);  //从set中删除
+        subOnlineCount();           //在线数减1
+        log.info("有一连接关闭！当前在线人数为" + getOnlineCount());
+    }
+
+    /**
+     * 收到客户端消息后调用的方法
+     *
+     * @param message 客户端发送过来的消息
+     */
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        log.info("来自客户端的消息:" + message);
+
+        //群发消息
+        for (SocketChannel item : socketChannel) {
+            try {
+                item.sendMessage(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        log.info("发生错误");
+        error.printStackTrace();
+    }
+
+
+    public void sendMessage(String message) throws IOException {
+        this.session.getBasicRemote().sendText(message);
+        //this.session.getAsyncRemote().sendText(message);
+    }
+
+    /**
+     * 发生错误时调用
+     * 群发自定义消息
+     */
+    public static void sendInfo(String message) {
+        for (SocketChannel item : socketChannel) {
+            try {
+                item.sendMessage(message);
+            } catch (IOException e) {
+                continue;
+            }
+        }
+    }
+
+    public static synchronized int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public static synchronized void addOnlineCount() {
+        SocketChannel.onlineCount++;
+    }
+
+    public static synchronized void subOnlineCount() {
+        SocketChannel.onlineCount--;
+    }
+
+}
