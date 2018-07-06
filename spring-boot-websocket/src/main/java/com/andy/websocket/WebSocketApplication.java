16b9d264ab68124eebc54baa08c679b57fbcc356
lyon <rethinkt@163.com>
2018-07-06 19:27:37 +0800

    update some code

---
+++ spring-boot-websocket/src/main/java/com/andy/websocket/WebSocketApplication.java
@@ -0,0 +1,15 @@
+package com.andy.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-07-06 19:27
+ **/
+@SpringBootApplication
+public class WebSocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketApplication.class, args);
+    }
+}
