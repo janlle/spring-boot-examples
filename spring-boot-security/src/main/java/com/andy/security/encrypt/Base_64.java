f1e9e202b84a6d96ef9073ff11e2a5728b97bbca
lyon <rethinkt@163.com>
2018-07-06 23:42:29 +0800

    update some code

--- spring-boot-security/src/main/java/com/andy/security/encrypt/Base_64.java
+++ spring-boot-security/src/main/java/com/andy/security/encrypt/Base_64.java
@@ -1,16 +1,18 @@
 package com.andy.security.encrypt;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 public class Base_64 {
 
-    private static String src = "aced";
+    private static String src = "hello world";
 
     public static void main(String[] args) {
         Base_64.jdkBase64(src);
+        Base_64.jdk8Base64(src);
         Base_64.commonsCodecBase64(src);
         Base_64.bouncyCastleBase64(src);
     }
@@ -25,6 +27,20 @@ public class Base_64 {
             String decode = new String(decoder.decodeBuffer(encode));
             System.out.println("decode:  " + decode);
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //使用JDK8的base64实现，
+    public static void jdk8Base64(String content) {
+        Base64.Encoder encoder = Base64.getEncoder();
+        Base64.Decoder decoder = Base64.getDecoder();
+        try {
+            byte[] encode = encoder.encode(content.getBytes());
+            System.out.println("encode:  " + encode);
+            String decode = new String(decoder.decode(encode));
+            System.out.println("decode:  " + decode);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
