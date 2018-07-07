91831f7c11d07b4b8de4c67cdae79d7d103f73fa
leone <exklin@gmail.com>
2018-07-07 11:14:48 +0800

    update some code

--- spring-boot-security/src/main/java/com/andy/security/encrypt/RSA.java
+++ spring-boot-security/src/main/java/com/andy/security/encrypt/RSA.java
@@ -26,174 +26,62 @@ import javax.crypto.Cipher;
 @Slf4j
 public class RSA {
 
-    //Base64 编码/解码器 JDK1.8
-    private static Base64.Decoder decoder = Base64.getDecoder();
+    private static String src = "上帝，god gave to something！@#*5";
+
     private static Base64.Encoder encoder = Base64.getEncoder();
-
-    /** 算法名称 */
-    private static final String ALGORITHM =  "RSA";
-    public static final String SIGNATURE_ALGORITHM = "MD5withRSA";
-    /** 默认密钥大小 */
-    private static final int KEY_SIZE = 1024;
-    /** 用来指定保存密钥对的文件名和存储的名称 */
-    private static final String PUBLIC_KEY_NAME = "publicKey";
-    private static final String PRIVATE_KEY_NAME = "privateKey";
-    private static final String PUBLIC_FILENAME = "publicKey.properties";
-    private static final String PRIVATE_FILENAME = "privateKey.properties";
-    /** 密钥对生成器 */
-    private static KeyPairGenerator keyPairGenerator = null;
-    /** 缓存的密钥对 */
-    private static KeyPair keyPair = null;
-
-    private static KeyFactory keyFactory = null;
-
-    /** 初始化密钥工厂 */
-    static{
-        try {
-            keyPairGenerator = KeyPairGenerator.getInstance(ALGORITHM);
-            keyFactory = KeyFactory.getInstance(ALGORITHM);
-        } catch (NoSuchAlgorithmException e) {
-            log.error(e.getMessage(),e);
-        }
-    }
-
+    private static Base64.Decoder decoder = Base64.getDecoder();
 
     public static void main(String[] args) throws Exception {
-
-        String pubFile = "d:/pub.key";
-        String priFile = "d:/pri.key";
-
-        makekeyfile(pubFile, priFile);
-
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(pubFile));
-        RSAPublicKey pubkey = (RSAPublicKey) ois.readObject();
-        ois.close();
-
-        ois = new ObjectInputStream(new FileInputStream(priFile));
-        RSAPrivateKey prikey = (RSAPrivateKey) ois.readObject();
-        ois.close();
-
-        // 使用公钥加密
-        String msg = "pass3421li";
-        String enc = "UTF-8";
-
-        // 使用公钥加密私钥解密
-        System.out.println("原文: " + msg);
-        byte[] result = handleData(pubkey, msg.getBytes(enc), 1);
-        System.out.println("加密: " + new String(result, enc));
-        byte[] deresult = handleData(prikey, result, 0);
-        System.out.println("解密: " + new String(deresult, enc));
-
-        msg = "demoiak3";
-        // 使用私钥加密公钥解密
-        System.out.println("原文: " + msg);
-        byte[] result2 = handleData(prikey, msg.getBytes(enc), 1);
-        System.out.println("加密: " + new String(result2, enc));
-        byte[] deresult2 = handleData(pubkey, result2, 0);
-        System.out.println("解密: " + new String(deresult2, enc));
-
+        System.out.println("原始字符串：" + src);
+        jdkRSA();
     }
 
+    public static void jdkRSA() throws Exception {
+        //1.初始化密钥
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(512);//密钥长度为64的整数倍，最大是65536
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        RSAPublicKey rsaPublicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) keyPair.getPrivate();
+        System.out.println("RSA公钥：" + encoder.encodeToString(rsaPublicKey.getEncoded()));
+        System.out.println("RSA私钥：" + encoder.encodeToString(rsaPrivateKey.getEncoded()));
 
-    /**
-     * 用私钥对信息生成数字签名
-     *
-     * @param content    加密数据
-     * @param privateKey 私钥
-     * @return
-     * @throws Exception
-     */
-    public static String sign(String content, String privateKey) throws Exception {
-        // 解密由base64编码的私钥
-        byte[] keyBytes = decoder.decode(privateKey);
-        // 构造PKCS8EncodedKeySpec对象
-        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(keyBytes);
-        // KEY_ALGORITHM 指定的加密算法
+        //2.1私钥加密，公钥解密【加密】
+        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(rsaPrivateKey.getEncoded());
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        // 取私钥匙对象
-        PrivateKey priKey = keyFactory.generatePrivate(pkcs8KeySpec);
-        // 用私钥对信息生成数字签名
-        Signature signature = Signature.getInstance("MD5withRSA");
-        signature.initSign(priKey);
-        signature.update(content.getBytes());
-        return encoder.encode(signature.sign()).toString();
+        PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+        byte[] result = cipher.doFinal(src.getBytes());
+        System.out.println("JDK RSA私钥加密：" + encoder.encodeToString(result));
+
+        //2.2私钥加密，公钥解密【解密】
+        X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(rsaPublicKey.getEncoded());
+        keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+        result = cipher.doFinal(result);
+        System.out.println("JDK RSA公钥解密：" + new String(result));
+
+        //3.1公钥加密，私钥解密【加密】
+        x509EncodedKeySpec = new X509EncodedKeySpec(rsaPublicKey.getEncoded());
+        keyFactory = KeyFactory.getInstance("RSA");
+        publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        result = cipher.doFinal(src.getBytes());
+        System.out.println("JDK RSA公钥加密：" + encoder.encodeToString(result));
+
+        //3.2公约加密，私钥解密【解密】
+        pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(rsaPrivateKey.getEncoded());
+        keyFactory = KeyFactory.getInstance("RSA");
+        privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        result = cipher.doFinal(result);
+        System.out.println("JDK RSA私钥解密：" + new String(result));
     }
 
 
-    /**
-     * 校验数字签名
-     *
-     * @param content   加密数据
-     * @param publicKey 公钥
-     * @param sign      数字签名
-     * @return 校验成功返回true 失败返回false
-     * @throws Exception
-     */
-    public static boolean verify(String content, String publicKey, String sign)
-            throws Exception {
-        // 解密由base64编码的公钥
-        byte[] keyBytes = decoder.decode(publicKey);
-        // 构造X509EncodedKeySpec对象
-        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
-        // KEY_ALGORITHM 指定的加密算法
-        KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
-        // 取公钥匙对象
-        PublicKey pubKey = keyFactory.generatePublic(keySpec);
-        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
-        signature.initVerify(pubKey);
-        signature.update(content.getBytes());
-        // 验证签名是否正常
-        return signature.verify(decoder.decode(sign));
-    }
-
-    public static void makekeyfile(String pubKeyFile, String privateKeyFile) throws Exception {
-        // KeyPairGenerator类用于生成公钥和私钥对，基于RSA算法生成对象  
-        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
-        // 初始化密钥对生成器，密钥大小为1024位  
-        keyPairGen.initialize(1024);
-        // 生成一个密钥对，保存在keyPair中  
-        KeyPair keyPair = keyPairGen.generateKeyPair();
-
-        // 得到私钥  
-        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-
-        // 得到公钥  
-        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-
-        // 生成私钥  
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(privateKeyFile));
-        oos.writeObject(privateKey);
-        oos.flush();
-        oos.close();
-
-        oos = new ObjectOutputStream(new FileOutputStream(pubKeyFile));
-        oos.writeObject(publicKey);
-        oos.flush();
-        oos.close();
-        System.out.println("make file ok!");
-    }
 
 
-    public static byte[] handleData(Key k, byte[] data, int encrypt)
-            throws Exception {
-
-        if (k != null) {
-
-            Cipher cipher = Cipher.getInstance("RSA");
-
-            if (encrypt == 1) {
-                cipher.init(Cipher.ENCRYPT_MODE, k);
-                byte[] resultBytes = cipher.doFinal(data);
-                return resultBytes;
-            } else if (encrypt == 0) {
-                cipher.init(Cipher.DECRYPT_MODE, k);
-                byte[] resultBytes = cipher.doFinal(data);
-                return resultBytes;
-            } else {
-                System.out.println("参数必须为: 1 加密 0解密");
-            }
-        }
-        return null;
-    }
 
 }
