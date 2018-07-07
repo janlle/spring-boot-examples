bef5b7d4b9fa03429343b83574d722451f227b20
leone <exklin@gmail.com>
2018-07-07 10:21:12 +0800

    update some code

--- spring-boot-security/src/main/java/com/andy/security/encrypt/MAC.java
+++ spring-boot-security/src/main/java/com/andy/security/encrypt/MAC.java
@@ -26,7 +26,7 @@ public class MAC {
     SHA系列：HmacSHA1、HmacSHA224、HmacSHA256、HmacSHA384、HmacSHA512
     */
 
-    private static final String FINAL_KEY = "a";
+    private static final String FINAL_KEY = "b";
 
     private static final String SRC = "hello";
 
@@ -34,7 +34,7 @@ public class MAC {
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.println("请输入要加密的内容:");
 //        String input = scanner.nextLine();
-        String result = MAC.HmacSHA512(SRC);
+        String result = MAC.HmacSHA384(SRC);
         System.out.println(result);
     }
 
@@ -50,13 +50,9 @@ public class MAC {
             SecretKey secretKey = generator.generateKey();
             // 获得密钥
             byte[] key = secretKey.getEncoded();
-            // 添加BouncyCastle的支持
-            Security.addProvider(new BouncyCastleProvider());
             // 还原密钥
-            SecretKey secretKeySpec = new SecretKeySpec("a".getBytes(), "HmacMD2");
-            // 实例化Mac
+            SecretKey secretKeySpec = new SecretKeySpec(FINAL_KEY.getBytes(), "HmacMD2");
             Mac mac = Mac.getInstance(secretKeySpec.getAlgorithm());
-            // 初始化mac
             mac.init(secretKeySpec);
             // 执行消息摘要
             byte[] digest = mac.doFinal(content.getBytes());
@@ -64,29 +60,18 @@ public class MAC {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return null;
     }
 
     public static String HmacMD4(String content) {
         try {
-            // 添加BouncyCastle的支持
             Security.addProvider(new BouncyCastleProvider());
-            // 初始化HmacMD5摘要算法的密钥产生器
             KeyGenerator generator = KeyGenerator.getInstance("HmacMD4");
-            // 产生密钥
             SecretKey secretKey = generator.generateKey();
-            // 获得密钥
             byte[] key = secretKey.getEncoded();
-            // 添加BouncyCastle的支持
-            Security.addProvider(new BouncyCastleProvider());
-            // 还原密钥
-            SecretKey secretKeySpec = new SecretKeySpec("a".getBytes(), "HmacMD4");
-            // 实例化Mac
+            SecretKey secretKeySpec = new SecretKeySpec(FINAL_KEY.getBytes(), "HmacMD4");
             Mac mac = Mac.getInstance(secretKeySpec.getAlgorithm());
-            // 初始化mac
             mac.init(secretKeySpec);
-            // 执行消息摘要
             byte[] digest = mac.doFinal(content.getBytes());
             return new HexBinaryAdapter().marshal(digest).toLowerCase();
         } catch (Exception e) {
@@ -101,7 +86,6 @@ public class MAC {
             KeyGenerator generator = KeyGenerator.getInstance("HmacMD5");
             SecretKey secretKey = generator.generateKey();
             byte[] key = secretKey.getEncoded();
-            //SecretKey secretKeySpec = new SecretKeySpec(key, "HmacMD5");
             SecretKey secretKeySpec = new SecretKeySpec(FINAL_KEY.getBytes(), "HmacMD5");
             Mac mac = Mac.getInstance(secretKeySpec.getAlgorithm());
             mac.init(secretKeySpec);
@@ -119,7 +103,6 @@ public class MAC {
             KeyGenerator generator = KeyGenerator.getInstance("HmacSHA1");
             SecretKey secretKey = generator.generateKey();
             byte[] key = secretKey.getEncoded();
-            //SecretKey secretKeySpec = new SecretKeySpec(key, "HmacSHA1");
             SecretKey secretKeySpec = new SecretKeySpec(FINAL_KEY.getBytes(), "HmacSHA1");
             Mac mac = Mac.getInstance(secretKeySpec.getAlgorithm());
             mac.init(secretKeySpec);
@@ -133,9 +116,7 @@ public class MAC {
 
     public static String HmacSHA224(String content) {
         try {
-            // 添加BouncyCastle的支持
             Security.addProvider(new BouncyCastleProvider());
-            // 初始化HmacMD5摘要算法的密钥产生器
             KeyGenerator generator = KeyGenerator.getInstance("HmacSHA224");
             SecretKey secretKey = generator.generateKey();
             byte[] key = secretKey.getEncoded();
@@ -169,12 +150,19 @@ public class MAC {
 
     public static String HmacSHA384(String content) {
         try {
+            // 初始化HmacMD5摘要算法的密钥产生器
             KeyGenerator generator = KeyGenerator.getInstance("HmacSHA384");
+            // 产生密钥
             SecretKey secretKey = generator.generateKey();
+            // 获得密钥
             byte[] key = secretKey.getEncoded();
+            // 还原密钥
             SecretKey secretKeySpec = new SecretKeySpec(FINAL_KEY.getBytes(), "HmacSHA384");
+            // 实例化Mac
             Mac mac = Mac.getInstance(secretKeySpec.getAlgorithm());
+            //初始化mac
             mac.init(secretKeySpec);
+            //执行消息摘要
             byte[] digest = mac.doFinal(content.getBytes());
             return new HexBinaryAdapter().marshal(digest).toLowerCase();
         } catch (Exception e) {
@@ -193,7 +181,6 @@ public class MAC {
             // 获得密钥
             byte[] key = secretKey.getEncoded();
             // 还原密钥
-            //SecretKey secretKeySpec = new SecretKeySpec(key, "HmacSHA1");
             SecretKey secretKeySpec = new SecretKeySpec(FINAL_KEY.getBytes(), "HmacSHA512");
             // 实例化Mac
             Mac mac = Mac.getInstance(secretKeySpec.getAlgorithm());
@@ -203,7 +190,6 @@ public class MAC {
             byte[] digest = mac.doFinal(content.getBytes());
             //转为十六进制的字符串
             return new HexBinaryAdapter().marshal(digest).toLowerCase();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
