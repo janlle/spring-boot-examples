9bcdd3595c8a9a5cdcab8bfca833fc43b3b7e596
leone <exklin@gmail.com>
2018-07-07 14:41:09 +0800

    update some code

--- spring-boot-security/src/main/java/com/andy/security/entity/rbac/Role.java
+++ spring-boot-security/src/main/java/com/andy/security/entity/rbac/Role.java
@@ -3,36 +3,53 @@ package com.andy.security.entity.rbac;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.*;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * 角色信息
+ *
  * @author: Mr.ruoLin
  * @createBy: 2018-04-19
  **/
 @Data
 @ApiModel("角色实体")
+@Table(name = "t_role")
 public class Role {
 
-    @ApiModelProperty("角色id")
-    private Integer     roleId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long roleId;
 
-    @ApiModelProperty("角色名称")
-    private String      role;
+    @Column(columnDefinition = "varchar(64) NOT NULL COMMENT '角色名称'")
+    private String roleName;
 
-    @ApiModelProperty("是否可用")
-    private Boolean     available;
+    @Column(columnDefinition = "varchar(128) NOT NULL COMMENT '角色的详情'")
+    private String description;
 
-    @ApiModelProperty("角色的详情")
-    private String      description;
+    @CreatedDate
+    @Column(columnDefinition = "timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'")
+    private Date createTime;
+
+    @LastModifiedDate
+    @Column(columnDefinition = "timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间'")
+    private Date updateTime;
+
+    @Column(columnDefinition = "bit NOT NULL COMMENT '是否可用'")
+    private Boolean disable;
+
+    @Column(columnDefinition = "bit NOT NULL COMMENT '是否删除'")
+    private Boolean deleted;
 
     @ApiModelProperty("对应的权限集合")
     private Set<Permission> permissions = new HashSet<>();
 
     @ApiModelProperty("对应的角色集合")
     private Set<User> users = new HashSet<>();
-
 
 }
