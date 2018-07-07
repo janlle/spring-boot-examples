8e386cc6c0419b01621b3bdcfe2bf87910bed19b
leone <janlle@126.com>
2018-07-07 13:34:20 +0800

    update some code

--- spring-boot-security/src/main/java/com/andy/security/web/controller/UserController.java
+++ spring-boot-security/src/main/java/com/andy/security/web/controller/UserController.java
@@ -16,44 +16,44 @@ import com.andy.security.entity.User;
 @Controller
 public class UserController {
 
-	@ResponseBody
-	@GetMapping("/users")
-	public List<User> query() {
-		List<User> users = new ArrayList<User>();
-		users.add(new User(1001, "andy", "12345",23200.0, new Date()));
-		users.add(new User(1001, "andy", "12345",23200.2, new Date()));
-		users.add(new User(1001, "andy", "12345",23200.0, new Date()));
-		users.add(new User(1001, "andy", "12345",23200.0, new Date()));
-		return users;
-	}
+    @ResponseBody
+    @GetMapping("/users")
+    public List<User> query() {
+        List<User> users = new ArrayList<User>();
+        users.add(new User(1001, "andy", "12345", 23200.0, new Date()));
+        users.add(new User(1001, "andy", "12345", 23200.2, new Date()));
+        users.add(new User(1001, "andy", "12345", 23200.0, new Date()));
+        users.add(new User(1001, "andy", "12345", 23200.0, new Date()));
+        return users;
+    }
 
-	@PostMapping("/authentication/form")
-	public String login() {
-		return "login";
-	}
+    @PostMapping("/authentication/form")
+    public String login() {
+        return "login";
+    }
 
 
-	@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
 //	@PostAuthorize("hasRole('ROLE_ADMIN')")
 //	@PreFilter("")
 //	@PostFilter("")
-	@GetMapping("/roleAuth")
-	public String role() {
-		return "hello role it's security!";
-	}
+    @GetMapping("/roleAuth")
+    public String role() {
+        return "hello role it's security!";
+    }
 
-	@PreAuthorize("#id<10 and principal.username.equals(#username) and user.username.equals('abc')")
-	@PostAuthorize("returnObject%2==0")
-	@RequestMapping("/test")
-	public Integer test(Integer id, String username, User user) {
-		return id;
-	}
+    @PreAuthorize("#id<10 and principal.username.equals(#username) and user.username.equals('abc')")
+    @PostAuthorize("returnObject%2==0")
+    @RequestMapping("/test")
+    public Integer test(Integer id, String username, User user) {
+        return id;
+    }
 
-	@PreFilter("filterObject%2==0")
-	@PostFilter("filterObject%4==0")
-	@RequestMapping("/test2")
-	public List<Integer> test(List<Integer> idList) {
-		return idList;
-	}
+    @PreFilter("filterObject%2==0")
+    @PostFilter("filterObject%4==0")
+    @RequestMapping("/test2")
+    public List<Integer> test(List<Integer> idList) {
+        return idList;
+    }
 
 }
