b2397aae7c6a4e98b7baf6af917b15c4bd79094b
lyon <rethinkt@163.com>
2018-07-08 17:23:31 +0800

    update some code

--- spring-boot-data/src/main/java/com/andy/data/config/AppConfiguration.java
+++ spring-boot-data/src/main/java/com/andy/data/config/AppConfiguration.java
@@ -15,7 +15,6 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.lang.reflect.Method;
 import java.time.Duration;
@@ -29,39 +28,39 @@ import java.time.Duration;
 @Configuration
 public class AppConfiguration {
 
+    @Bean
+    public KeyGenerator KeyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+   /* @Bean
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        RedisCacheManager manager = new RedisCacheManager(redisTemplate);
+        manager.setUsePrefix(true);
+        RedisCachePrefix cachePrefix = new RedisPrefix("prefix");
+        manager.setCachePrefix(cachePrefix);
+        // 整体缓存过期时间
+        manager.setDefaultExpiration(3600L);
+        // 设置缓存过期时间。key和缓存过期时间，单位秒
+        Map<String, Long> expiresMap = new HashMap<>();
+        expiresMap.put("user", 1000L);
+        manager.setExpires(expiresMap);
+        return manager;
+    }*/
+
 //    @Bean
-//    public KeyGenerator KeyGenerator() {
-//        return new KeyGenerator() {
-//            @Override
-//            public Object generate(Object target, Method method, Object... params) {
-//                StringBuilder sb = new StringBuilder();
-//                sb.append(target.getClass().getName());
-//                sb.append(method.getName());
-//                for (Object obj : params) {
-//                    sb.append(obj.toString());
-//                }
-//                return sb.toString();
-//            }
-//        };
-//    }
-//
-//   /* @Bean
-//    public CacheManager cacheManager(RedisTemplate redisTemplate) {
-//        RedisCacheManager manager = new RedisCacheManager(redisTemplate);
-//        manager.setUsePrefix(true);
-//        RedisCachePrefix cachePrefix = new RedisPrefix("prefix");
-//        manager.setCachePrefix(cachePrefix);
-//        // 整体缓存过期时间
-//        manager.setDefaultExpiration(3600L);
-//        // 设置缓存过期时间。key和缓存过期时间，单位秒
-//        Map<String, Long> expiresMap = new HashMap<>();
-//        expiresMap.put("user", 1000L);
-//        manager.setExpires(expiresMap);
-//        return manager;
-//    }*/
-//
-//    @Bean
-//    CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+//    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
 //        //初始化一个RedisCacheWriter
 //        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
 //        //设置CacheManager的值序列化方式为JdkSerializationRedisSerializer,但其实RedisCacheConfiguration默认就是使用StringRedisSerializer序列化key，JdkSerializationRedisSerializer序列化value,所以以下注释代码为默认实现
@@ -76,7 +75,7 @@ public class AppConfiguration {
 //        RedisCacheManager cacheManager = new RedisCacheManager(redisCacheWriter, defaultCacheConfig);
 //        return cacheManager;
 //    }
-//
+
 //    @Bean
 //    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
 //        StringRedisTemplate template = new StringRedisTemplate(factory);
