fdc4a3a8bbd1050976c2d6beced1198dcf1c97de
lyon <rethinkt@163.com>
2018-07-08 15:59:37 +0800

    update some code

--- spring-boot-data/src/main/java/com/andy/data/controller/RedisController.java
+++ spring-boot-data/src/main/java/com/andy/data/controller/RedisController.java
@@ -1,7 +1,6 @@
 package com.andy.data.controller;
 
-import com.andy.data.jpa.repository.UserRepository;
-import com.andy.data.service.JpaService;
+import com.andy.data.service.RedisService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,37 +9,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author: Mr.ruoLin
- * @createBy: 2018-05-11 19:30
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-07-08 15:52
  **/
 @Slf4j
 @RestController
-@RequestMapping("/jpa")
-public class JpaController {
+@RequestMapping("/redis")
+public class RedisController {
 
     @Autowired
-    private JpaService jpaService;
-
-    @Autowired
-    private UserRepository userRepository;
+    private RedisService redisService;
 
     @GetMapping("/batchInsert")
     public String insertList(@RequestParam(required = false, defaultValue = "1") Integer count) {
-        long time = jpaService.insertList(count);
+        long time = redisService.insertList(count);
         return "批量插入" + count + "条数据一共用了:" + time + "豪秒！";
     }
 
     @GetMapping("/foreachInsert")
     public String insertForeach(@RequestParam(required = false, defaultValue = "1") Integer count) {
-        long time = jpaService.insertForeach(count);
+        long time = redisService.insertForeach(count);
         return "批量插入" + count + "条数据一共用了:" + time + "豪秒！";
     }
-
-//    @InitBinder
-//    protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {
-//        DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
-//        CustomDateEditor dateEditor = new CustomDateEditor(fmt, true);
-//        binder.registerCustomEditor(Date.class, dateEditor);
-//    }
 
 }
