814bfd64a3ae2de12952f2c4013a2361ccb14dc1
lyon <rethinkt@163.com>
2018-07-11 21:36:17 +0800

    update some code

--- spring-boot-docker/src/main/java/com/andy/docker/Application.java
+++ spring-boot-docker/src/main/java/com/andy/docker/Application.java
@@ -3,21 +3,11 @@ package com.andy.docker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @EnableEurekaServer
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-	
-	@GetMapping("/home")
-	public String home(){
-		return "hello docker";
-	}
-	
-	
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
