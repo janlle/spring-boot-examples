68e13823c15aedfecc935ed3870cbdf085555c2c
leone <janlle@126.com>
2018-07-11 21:32:44 +0800

    update some code

--- spring-boot-docker/src/main/java/com/andy/docker/controller/WebController.java
+++ spring-boot-docker/src/main/java/com/andy/docker/controller/WebController.java
@@ -3,12 +3,39 @@ package com.andy.docker.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController
 public class WebController {
-	
+
+    private Integer sum;
+
     @GetMapping("/hello_world")
     public String sayHelloWorld() {
         return "success-docker!";
+    }
+
+
+    @GetMapping("/debug")
+    public String debug(String name) throws Exception {
+        sum = new Integer(0);
+        System.out.println("hello");
+        sum += 3;
+        System.out.println("world");
+        sum = new Integer(5);
+        System.out.println("hello");
+        sum = 23;
+        System.out.println("hello");
+        if (sum > 23) {
+            System.out.println("haha");
+        }
+        for (int i = 1; i < 3; i++) {
+            System.out.println("i:" + i);
+        }
+        System.out.println("hello");
+        sum += 1;
+        System.out.println("world");
+        Thread.sleep(1000);
+        System.out.println("hello");
+        return "success-debug-" + name;
     }
 
 }
