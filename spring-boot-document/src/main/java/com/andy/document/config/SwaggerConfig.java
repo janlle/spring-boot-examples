d21b5f6c57ebc5a37195ba89dd68e5711746c546
leone <janlle@126.com>
2018-07-12 20:32:52 +0800

    update some code

--- spring-boot-document/src/main/java/com/andy/document/config/SwaggerConfig.java
+++ spring-boot-document/src/main/java/com/andy/document/config/SwaggerConfig.java
@@ -1,27 +1,18 @@
 package com.andy.document.config;
 
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
-
-import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 
 /**
  * @Author: Mr.lyon
