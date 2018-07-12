eac51a9742c72eb2dcb3ce1ef967eb68e66010bc
leone <janlle@126.com>
2018-07-12 23:22:57 +0800

    update some code

--- spring-boot-swagger/src/main/java/com/andy/swagger/SwaggerApplication.java
+++ spring-boot-swagger/src/main/java/com/andy/swagger/SwaggerApplication.java
@@ -3,12 +3,14 @@ package com.andy.swagger;
 import com.didispace.swagger.butler.EnableSwaggerButler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author: Mr.ruoLin
  * @createBy: 2018-05-29 22:29
  **/
-@EnableSwaggerButler
+//@EnableSwaggerButler
+@EnableSwagger2
 @SpringBootApplication
 public class SwaggerApplication {
     public static void main(String[] args) {
