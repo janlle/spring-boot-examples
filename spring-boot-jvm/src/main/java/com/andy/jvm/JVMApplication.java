2177e8cf1a51612986c4859b033686dbcd87080f
leone <janlle@126.com>
2018-07-14 13:17:28 +0800

    update some code

---
+++ spring-boot-jvm/src/main/java/com/andy/jvm/JVMApplication.java
@@ -0,0 +1,15 @@
+package com.andy.jvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-07-14 13:14
+ **/
+@SpringBootApplication
+public class JVMApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JVMApplication.class, args);
+    }
+}
