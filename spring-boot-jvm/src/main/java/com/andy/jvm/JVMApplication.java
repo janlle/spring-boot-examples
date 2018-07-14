ba0a9faa178a85810dae7386958ccba294fb1ea1
leone <exklin@gmail.com>
2018-07-14 14:35:14 +0800

    update some code

--- spring-boot-jvm/src/main/java/com/andy/jvm/JVMApplication.java
+++ spring-boot-jvm/src/main/java/com/andy/jvm/JVMApplication.java
@@ -9,7 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  **/
 @SpringBootApplication
 public class JVMApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(JVMApplication.class, args);
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println(args[i]);
+            }
+        }
     }
+
+
 }
