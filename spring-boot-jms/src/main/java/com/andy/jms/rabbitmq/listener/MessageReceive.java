5d0734bdc902b57d6f9bdc84597ccede75a6dbf8
leone <exklin@gmail.com>
2018-07-15 18:26:53 +0800

    update some code

--- spring-boot-jms/src/main/java/com/andy/jms/rabbitmq/listener/MessageReceive.java
+++ spring-boot-jms/src/main/java/com/andy/jms/rabbitmq/listener/MessageReceive.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-
 @Slf4j
 @Component
 public class MessageReceive {
@@ -13,31 +12,42 @@ public class MessageReceive {
     //-------------------------普通队列模式-------------------------------
     @RabbitListener(queues = RabbitMQConstant.QUEUE_A)
     public void receiveQueue(String msg) throws Exception {
-        Thread.sleep(6000);
+        Thread.sleep(2000);
         log.info("收到{}队列的消息是:{}", RabbitMQConstant.QUEUE_A, msg);
     }
 
-    //-------------------------交换机队列模式-------------------------------
-    @RabbitListener(queues = RabbitMQConstant.QUEUE_A)
-    public void receiveTopicA(String msg) throws Exception {
-        Thread.sleep(6000);
-        log.info("收到{}交换机的消息是:{}", RabbitMQConstant.TOPIC_EXCHANGE, msg);
-    }
 
+    //-------------------------topic类型的交换机(主题模式)-------------------------
     @RabbitListener(queues = RabbitMQConstant.QUEUE_B)
-    public void receiveTopicB(String msg) throws Exception {
-        Thread.sleep(6000);
+    public void receiveTopicA(String msg) throws Exception {
+        Thread.sleep(2000);
         log.info("收到{}交换机的消息是:{}", RabbitMQConstant.TOPIC_EXCHANGE, msg);
     }
 
-    //-------------------------headers模式-------------------------------
+    @RabbitListener(queues = RabbitMQConstant.QUEUE_C)
+    public void receiveTopicB(String msg) throws Exception {
+        Thread.sleep(2000);
+        log.info("收到{}交换机的消息是:{}", RabbitMQConstant.TOPIC_EXCHANGE, msg);
+    }
+
+
+
+
+    //-------------------------headers类型的交换机(首部模式)------------------------
     @RabbitListener(queues = RabbitMQConstant.QUEUE_B)
     public void receiveHeaders(byte[] msg) throws Exception {
         Thread.sleep(6000);
         log.info("收到{}交换机的消息是:{}", RabbitMQConstant.HEADERS_EXCHANGE, new String(msg));
     }
 
-    //-------------------------xxx模式-------------------------------
 
 
+
+    //-------------------------direct类型的交换机(直连模式)------------------------
+
+
+
+
+
+    //------------------------- fanout类型的交换机(广播模式)-----------------------
 }
