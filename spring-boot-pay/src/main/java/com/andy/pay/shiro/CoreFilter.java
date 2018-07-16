edfd0ede12764d37fe89b17c28a4a5af0bc03f06
leone <exklin@gmail.com>
2018-07-16 09:27:49 +0800

    update some code

---
+++ spring-boot-pay/src/main/java/com/andy/pay/shiro/CoreFilter.java
@@ -0,0 +1,34 @@
+package com.andy.pay.shiro;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.web.servlet.AbstractFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class CoreFilter extends AbstractFilter {
+
+    public CoreFilter() {
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.addHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
+        response.addHeader("Access-Control-Max-Age", "18000");
+        if (request.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            response.setStatus(HttpStatus.OK.value());
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+    }
+}
