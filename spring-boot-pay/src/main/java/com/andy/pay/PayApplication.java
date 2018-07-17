300c74744783939d28a1afdd7d4b0a3726fedf69
lyon <rethinkt@163.com>
2018-07-17 19:55:01 +0800

    update some code

--- spring-boot-pay/src/main/java/com/andy/pay/PayApplication.java
+++ spring-boot-pay/src/main/java/com/andy/pay/PayApplication.java
@@ -1,10 +1,8 @@
 package com.andy.pay;
 
-import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,8 +11,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * spring-boot 支付demo
  * nohup java -jar --server.port=8886 spring-boot-pay.jar > web.log &
  */
-@Slf4j
-@Controller
 @EnableSwagger2
 @SpringBootApplication
 @MapperScan(basePackages = "com.andy.pay.mapper")
--- spring-boot-pay/src/main/java/com/andy/pay/shiro/filter/CoreFilter.java
+++ spring-boot-pay/src/main/java/com/andy/pay/shiro/filter/CoreFilter.java
@@ -12,30 +12,37 @@ import org.apache.shiro.web.servlet.AbstractFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//public class CoreFilter extends AbstractFilter {
-//
-//    private static final Logger logger = LoggerFactory.getLogger(CoreFilter.class);
-//
-//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-//
-//        HttpServletRequest request = (HttpServletRequest) servletRequest;
-//        HttpServletResponse response = (HttpServletResponse) servletResponse;
-//
-//        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-//        response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-//        response.addHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
-//        response.addHeader("Access-Control-Max-Age", "18000");
-//
-//        if (request.getMethod().equals(RequestMethod.OPTIONS.name())) {
-//            response.setStatus(HttpStatus.OK.value());
-//        } else {
-//            logger.info("coreFilter.doFilter");
-//            filterChain.doFilter(servletRequest, servletResponse);
-//        }
-//
-//    }
-//
-//
-//}
+@Component
+public class CoreFilter extends AbstractFilter {
+
+
+    public CoreFilter() {
+        logger.info("core filter init...");
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(CoreFilter.class);
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.addHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
+        response.addHeader("Access-Control-Max-Age", "18000");
+
+        if (request.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            response.setStatus(HttpStatus.OK.value());
+        } else {
+            logger.info("coreFilter.doFilter");
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+    }
+
+
+}
