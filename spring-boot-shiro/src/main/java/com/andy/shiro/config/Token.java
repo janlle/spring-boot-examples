661e802a11624016ac4983f9c2317b8a7d410772
leone <janlle@126.com>
2018-07-17 23:55:31 +0800

    update some code

---
+++ spring-boot-shiro/src/main/java/com/andy/shiro/config/Token.java
@@ -0,0 +1,25 @@
+package com.andy.shiro.config;
+
+import org.apache.shiro.authc.UsernamePasswordToken;
+
+public class Token extends UsernamePasswordToken {
+
+    private String token;
+
+    public Token(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+
+
+    public Object getPrincipal() {
+        return this.getToken();
+    }
+
+    public Object getCredentials() {
+        return this.getToken();
+    }
+}
