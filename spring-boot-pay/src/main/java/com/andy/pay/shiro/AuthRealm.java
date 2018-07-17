629e0504fafcec48c30eae8556859b0a7b88d757
leone <exklin@gmail.com>
2018-07-17 14:08:41 +0800

    update some code

--- spring-boot-pay/src/main/java/com/andy/pay/shiro/AuthRealm.java
+++ spring-boot-pay/src/main/java/com/andy/pay/shiro/AuthRealm.java
@@ -17,7 +17,7 @@ import org.springframework.util.StringUtils;
 import javax.annotation.Resource;
 
 @Component
-public class TokenRealm extends AuthorizingRealm {
+public class AuthRealm extends AuthorizingRealm {
 
 
     @Autowired
