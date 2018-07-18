31f40580284b9a8d7a59339e8233dfedb605660e
leone <janlle@126.com>
2018-07-18 23:47:02 +0800

    update some code

---
+++ spring-boot-mvc/src/main/java/com/andy/mvc/shiro/ShiroFilterUrlProperties.java
@@ -0,0 +1,42 @@
+package com.andy.mvc.shiro;
+
+import java.util.List;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties(
+    prefix = "luwei.module.shiro"
+)
+@Configuration
+public class ShiroFilterUrlProperties {
+    private List<String> anonUrl;
+    private List<String> corsUrl;
+    private List<String> authUrl;
+
+    public ShiroFilterUrlProperties() {
+    }
+
+    public List<String> getAnonUrl() {
+        return this.anonUrl;
+    }
+
+    public void setAnonUrl(List<String> anonUrl) {
+        this.anonUrl = anonUrl;
+    }
+
+    public List<String> getCorsUrl() {
+        return this.corsUrl;
+    }
+
+    public void setCorsUrl(List<String> corsUrl) {
+        this.corsUrl = corsUrl;
+    }
+
+    public List<String> getAuthUrl() {
+        return this.authUrl;
+    }
+
+    public void setAuthUrl(List<String> authUrl) {
+        this.authUrl = authUrl;
+    }
+}
---
+++ spring-boot-mvc/src/main/java/com/andy/mvc/shiro/Token.java
@@ -0,0 +1,23 @@
+package com.andy.mvc.shiro;
+
+import org.apache.shiro.authc.AuthenticationToken;
+
+public class Token implements AuthenticationToken {
+    private String token;
+
+    public Token(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+
+    public Object getPrincipal() {
+        return this.getToken();
+    }
+
+    public Object getCredentials() {
+        return this.getToken();
+    }
+}
