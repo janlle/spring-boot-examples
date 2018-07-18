b137fddb7b6c1b7375c4d5995eb87bad08e6d897
lyon <rethinkt@163.com>
2018-07-18 23:47:02 +0800

    update some code

---
+++ spring-boot-mvc/src/main/java/com/andy/mvc/shiro/CorsFilter.java
@@ -0,0 +1,39 @@
+package com.andy.mvc.shiro;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.web.servlet.AbstractFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Slf4j
+public class CorsFilter extends AbstractFilter {
+
+    public CorsFilter() {
+        log.info("cors filter init...");
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.addHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
+        response.addHeader("Access-Control-Max-Age", "18000");
+        if (request.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            response.setStatus(HttpStatus.OK.value());
+        } else {
+            log.info("do filter ...");
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+    }
+}
