a277c5623792b81b93c06a3e75e3986288ce53f2
leone <janlle@126.com>
2018-07-21 14:35:49 +0800

    update some code

--- spring-boot-shiro/src/main/java/com/andy/shiro/config/AuthRealm.java
+++ spring-boot-shiro/src/main/java/com/andy/shiro/config/AuthRealm.java
@@ -13,12 +13,10 @@ import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
- * @author: Mr.ruoLin
+ * @author: Mr.lyon
  * @createBy: 2018-04-21 16:51
  **/
 @Slf4j
@@ -27,42 +25,78 @@ public class AuthRealm extends AuthorizingRealm {
     @Autowired
     private UserService userService;
 
-    //shiro认证时调用
+    Map<String, String> map = new HashMap<>();
+
+    {
+        map.put("james", "james");
+        map.put("kobe", "kobe");
+    }
+
+    // 认证时调用
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-        log.info("进入shiro的AuthorizingRealm认证方法...");
+        log.info("进入AuthorizingRealm认证方法...");
+        String name = (String) token.getPrincipal();
+
+        // 获取用户名和密码
         UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) token;
         String username = usernamePasswordToken.getUsername();
         String password = usernamePasswordToken.getPassword().toString();
-        User user = userService.findByUsername(username);
-        return new SimpleAuthenticationInfo(user, user.getPassword(), this.getClass().getName());
+        // 查询数据库
+//        User user = userService.findByUsername(username);
+        String pwd = getPassword(username);
+        return new SimpleAuthenticationInfo(username, pwd, this.getClass().getName());
+//        return new SimpleAuthenticationInfo(user, user.getPassword(), this.getClass().getName());
     }
 
-    //shiro授权时调用
+    // 授权时调用
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        log.info("进入shiro的AuthorizingRealm授权方法...");
-        User user = (User) principalCollection.fromRealm(this.getClass().getName()).iterator().next();
-        List<String> permissionList = new ArrayList<>();
-        List<String> roleList = new ArrayList<>();
-        Set<Role> roleSet = user.getRoles();
-        if (CollectionUtils.isNotEmpty(roleSet)) {
-            for (Role role : roleSet) {
-                roleList.add(role.getRole());
-                Set<Permission> permissionSet = role.getPermissions();
-                if (CollectionUtils.isNotEmpty(permissionSet)) {
-                    for (Permission permission : permissionSet) {
-                        permissionList.add(permission.getPermission());
-                    }
-                }
-            }
-        }
+        log.info("进入AuthorizingRealm授权方法...");
+
+//        User user = (User) principalCollection.fromRealm(this.getClass().getName()).iterator().next();
+//        List<String> permissionList = new ArrayList<>();
+//        List<String> roleList = new ArrayList<>();
+//        Set<Role> roleSet = user.getRoles();
+//        if (CollectionUtils.isNotEmpty(roleSet)) {
+//            for (Role role : roleSet) {
+//                roleList.add(role.getRole());
+//                Set<Permission> permissionSet = role.getPermissions();
+//                if (CollectionUtils.isNotEmpty(permissionSet)) {
+//                    for (Permission permission : permissionSet) {
+//                        permissionList.add(permission.getPermission());
+//                    }
+//                }
+//            }
+//        }
         SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-        info.addStringPermissions(permissionList);
-        info.addRoles(roleList);
-        log.info("permissionList:{}", permissionList);
-        log.info("roleList:{}", roleList);
+//        info.addStringPermissions(permissionList);
+//        info.addRoles(roleList);
+        info.setRoles(getRole());
+        info.setStringPermissions(getPermission());
+//        log.info("permissionList:{}", permissionList);
+//        log.info("roleList:{}", roleList);
         return info;
+    }
+
+    private Set<String> getPermission() {
+        Set<String> set = new HashSet<>();
+        set.add("user:update");
+        set.add("user:delete");
+        return set;
+    }
+
+
+    private String getPassword(String username) {
+
+        return map.get(username);
+    }
+
+    private Set<String> getRole() {
+        Set<String> set = new HashSet<>();
+        set.add("admin");
+        set.add("user");
+        return set;
     }
 
 
--- spring-boot-shiro/src/main/java/com/andy/shiro/test/JdbcRealmTest.java
+++ spring-boot-shiro/src/main/java/com/andy/shiro/test/JdbcRealmTest.java
@@ -1,34 +1,46 @@
 package com.andy.shiro.test;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
 import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.subject.Subject;
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
 
 /**
  * @Author: Mr.lyon
  * @CreateBy: 2018-07-21 13:06
  **/
 
-public class shiroAuthIniTest {
+public class JdbcRealmTest {
 
-    private SimpleAccountRealm accountRealm = new SimpleAccountRealm();
+    DruidDataSource dataSource = new DruidDataSource();
 
-    @Before
-    public void init() {
-        accountRealm.addAccount("james", "admin", "admin", "user");
+    {
+        dataSource.setUrl("jdbc:mysql://localhost:3306/boot");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
     }
 
     @Test
     public void shiroAuthTest() {
-        IniRealm iniRealm = new IniRealm("classpath:user.ini");
+        JdbcRealm jdbcRealm = new JdbcRealm();
+        jdbcRealm.setDataSource(dataSource);
 
         DefaultSecurityManager securityManager = new DefaultSecurityManager();
-        securityManager.setRealm(iniRealm);
+        securityManager.setRealm(jdbcRealm);
 
         SecurityUtils.setSecurityManager(securityManager);
         Subject subject = SecurityUtils.getSubject();
@@ -42,8 +54,6 @@ public class shiroAuthIniTest {
         subject.checkRoles("admin", "user");
         subject.checkPermission("user:delete");
         subject.checkPermission("user:update");
-//        subject.logout();
-//        System.out.println("is login:" + subject.isAuthenticated());
 
 
     }
