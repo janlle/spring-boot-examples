3a45c90ef8142a2d330f086cc8fb967c2b1cd3d8
lyon <rethinkt@163.com>
2018-07-22 16:08:25 +0800

    update some code

--- spring-boot-shiro/src/main/java/com/andy/shiro/entity/rbac/Role.java
+++ spring-boot-shiro/src/main/java/com/andy/shiro/entity/rbac/Role.java
@@ -1,39 +1,55 @@
 package com.andy.shiro.entity.rbac;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Date;
 
 /**
  * 角色信息
+ *
  * @author: Mr.ruoLin
  * @createBy: 2018-04-19
  **/
 @Data
+@Entity
 @ApiModel("角色实体")
+@Table(name = "sys_role")
 public class Role {
 
-    @ApiModelProperty("角色id")
-    private Integer     roleId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long roleId;
 
-    @ApiModelProperty("角色名称")
-    private String      role;
+    @Column(columnDefinition = "varchar(64) NOT NULL COMMENT '角色名称'")
+    private String roleName;
 
-    @ApiModelProperty("是否可用")
-    private Boolean     available;
+    @Column(columnDefinition = "varchar(128) NOT NULL COMMENT '角色的详情'")
+    private String description;
 
-    @ApiModelProperty("角色的详情")
-    private String      description;
+    @Column(columnDefinition = "bit NOT NULL COMMENT '是否可用'")
+    private Boolean disable;
 
-    @ApiModelProperty("对应的权限集合")
-    private Set<Permission> permissions = new HashSet<>();
+    @Column(columnDefinition = "bit NOT NULL COMMENT '是否删除'")
+    private Boolean deleted;
 
-    @ApiModelProperty("对应的角色集合")
-    private Set<User> users = new HashSet<>();
+    @CreatedDate
+    @Column(columnDefinition = "timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'")
+    private Date createTime;
 
+    @LastModifiedDate
+    @Column(columnDefinition = "timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间'")
+    private Date updateTime;
+
+//    @ApiModelProperty("对应的权限集合")
+//    @ManyToMany(mappedBy = "roles")
+//    private Set<Permission> permissions = new HashSet<>();
+
+//    @ApiModelProperty("对应的角色集合")
+//    @ManyToMany(mappedBy = "roles")
+//    private Set<User> users = new HashSet<>();
 
 }
