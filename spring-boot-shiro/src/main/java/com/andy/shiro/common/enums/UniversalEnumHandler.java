b5a56ed563184e871dff6deac4b9cf4b054577dd
leone <janlle@126.com>
2018-07-22 17:13:32 +0800

    update some code

---
+++ spring-boot-shiro/src/main/java/com/andy/shiro/common/enums/UniversalEnumHandler.java
@@ -0,0 +1,84 @@
+package com.andy.shiro.common.enums;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class UniversalEnumHandler<E extends BaseEnum> extends BaseTypeHandler<E> {
+
+    private Class<E> type;
+
+    private E[] enums;
+
+    public UniversalEnumHandler(Class<E> type) {
+        if (type == null)
+            throw new IllegalArgumentException("Type argument cannot be null");
+        this.type = type;
+        this.enums = type.getEnumConstants();
+        if (this.enums == null) {
+            throw new IllegalArgumentException(type.getSimpleName() + " does not represent an enum type.");
+        }
+    }
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, E parameter, JdbcType jdbcType) throws SQLException {
+        //BaseTypeHandler已经帮我们做了parameter的null判断
+        ps.setObject(i, parameter.code(), jdbcType.TYPE_CODE);
+    }
+
+    @Override
+    public E getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        // 根据数据库存储类型决定获取类型，本例子中数据库中存放String类型
+        String i = rs.getString(columnName);
+        if (rs.wasNull()) {
+            return null;
+        } else {
+            // 根据数据库中的value值，定位PersonType子类
+            return locateEnumStatus(i);
+        }
+    }
+
+    @Override
+    public E getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        // 根据数据库存储类型决定获取类型，本例子中数据库中存放String类型
+        String i = rs.getString(columnIndex);
+        if (rs.wasNull()) {
+            return null;
+        } else {
+            // 根据数据库中的value值，定位PersonType子类
+            return locateEnumStatus(i);
+        }
+    }
+
+    @Override
+    public E getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        // 根据数据库存储类型决定获取类型，本例子中数据库中存放String类型
+        String i = cs.getString(columnIndex);
+        if (cs.wasNull()) {
+            return null;
+        } else {
+            // 根据数据库中的value值，定位PersonType子类
+            return locateEnumStatus(i);
+        }
+    }
+
+    /**
+     * 枚举类型转换，由于构造函数获取了枚举的子类enums，让遍历更加高效快捷
+     *
+     * @param value 数据库中存储的自定义value属性
+     * @return value对应的枚举类
+     */
+    private E locateEnumStatus(String value) {
+        for (E e : enums) {
+            if (e.code().equals(value)) {
+                return e;
+            }
+        }
+        throw new IllegalArgumentException("未知的枚举类型：" + value + ",请核对" + type.getSimpleName());
+    }
+}
---
+++ spring-boot-shiro/src/main/java/com/andy/shiro/session/RedisSession.java
@@ -0,0 +1,40 @@
+package com.andy.shiro.session;
+
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.UnknownSessionException;
+import org.apache.shiro.session.mgt.eis.AbstractSessionDAO;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-07-21 15:27
+ **/
+public class RedisSession extends AbstractSessionDAO {
+
+    @Override
+    protected Serializable doCreate(Session session) {
+        return null;
+    }
+
+    @Override
+    protected Session doReadSession(Serializable sessionId) {
+        return null;
+    }
+
+    @Override
+    public void update(Session session) throws UnknownSessionException {
+
+    }
+
+    @Override
+    public void delete(Session session) {
+
+    }
+
+    @Override
+    public Collection<Session> getActiveSessions() {
+        return null;
+    }
+}
