c26f372846bcd56d2ef9bcbf8da3c22b633d957f
leone <janlle@126.com>
2018-07-23 22:38:49 +0800

    update some code

--- spring-boot-webflux/src/main/java/com/andy/flux/FluxApplication.java
+++ spring-boot-webflux/src/main/java/com/andy/flux/FluxApplication.java
@@ -3,11 +3,17 @@ package com.andy.flux;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-05-23 21:32
+ **/
 @EnableScheduling
 @ServletComponentScan
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class FluxApplication {
     public static void main(String[] args) {
         SpringApplication.run(FluxApplication.class, args);
