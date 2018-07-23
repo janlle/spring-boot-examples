38d6430b03679191733fd908a9ec8302d30e1e08
lyon <rethinkt@163.com>
2018-07-23 08:47:41 +0800

    update some code

---
+++ spring-boot-webflux/src/main/java/com/andy/flux/servlet/SyncServlet.java
@@ -0,0 +1,44 @@
+package com.andy.flux.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-07-22 22:19
+ **/
+@Slf4j
+@WebServlet(name = "syncServlet", urlPatterns = {"/syncServlet"})
+public class SyncServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long start = System.currentTimeMillis();
+        try {
+            doSomething();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        log.info("time:{}", +end - start);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+
+    private void doSomething() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(3);
+    }
+
+
+}
