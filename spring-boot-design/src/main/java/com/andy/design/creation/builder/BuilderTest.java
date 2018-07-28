fbd163b10178d0d645326b2424a3b68d41561630
leone <janlle@126.com>
2018-07-28 12:11:40 +0800

    add some java class

---
+++ spring-boot-design/src/main/java/com/andy/design/creation/builder/BuilderTest.java
@@ -0,0 +1,9 @@
+package com.andy.design.creation.builder;
+
+public class BuilderTest {
+
+    public static void main(String[] args) {
+        Builder builder = new Builder();
+        builder.produceMailSender(10);
+    }
+}
---
+++ spring-boot-design/src/main/java/com/andy/design/creation/builder/Sender.java
@@ -0,0 +1,11 @@
+package com.andy.design.creation.builder;
+
+/**
+ * @author: lyon
+ * @cerateBy: 2018-07-28
+ **/
+public interface Sender {
+
+    void send(String message);
+
+}
