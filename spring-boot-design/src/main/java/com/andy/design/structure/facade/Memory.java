8903619c7db4f8004b27660edb6c8b49b712bb35
lyon <rethinkt@163.com>
2018-07-29 11:19:42 +0800

    add some java class

---
+++ spring-boot-design/src/main/java/com/andy/design/structure/facade/Memory.java
@@ -0,0 +1,12 @@
+package com.andy.design.structure.facade;
+
+public class Memory {
+ 
+    public void startup() {
+        System.out.println("memory startup!");
+    }
+ 
+    public void shutdown() {
+        System.out.println("memory shutdown!");
+    }
+}
