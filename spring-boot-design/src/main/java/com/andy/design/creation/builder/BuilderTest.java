1218607c0c37145a96f8329b155910489e3dc2d2
leone <janlle@126.com>
2018-07-29 10:29:51 +0800

    add some java class

--- spring-boot-design/src/main/java/com/andy/design/creation/builder/BuilderTest.java
+++ spring-boot-design/src/main/java/com/andy/design/creation/builder/BuilderTest.java
@@ -1,9 +1,19 @@
 package com.andy.design.creation.builder;
 
+/**
+ * @author: lyon
+ * @cerateBy: 2018-07-29
+ **/
 public class BuilderTest {
 
     public static void main(String[] args) {
-        Builder builder = new Builder();
-        builder.produceMailSender(10);
+        UserDirector director = new UserDirector();
+        User user = director.constructUser(new Builder());
+
+        System.out.println(user.getAge());
+        System.out.println(user.getName());
+        System.out.println(user.getBirthday());
     }
+
+
 }
