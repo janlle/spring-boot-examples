23edcc24da26f32bb7c9b011a7276ee0a0c702da
lyon <rethinkt@163.com>
2018-07-29 10:29:51 +0800

    add some java class

--- spring-boot-design/src/main/java/com/andy/design/creation/prototype/Prototype.java
+++ spring-boot-design/src/main/java/com/andy/design/creation/prototype/Prototype.java
@@ -6,9 +6,34 @@ package com.andy.design.creation.prototype;
  **/
 public class Prototype implements Cloneable {
 
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        Prototype prototype = (Prototype) super.clone();
-        return prototype;
+    private String name;
+
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("finally");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Prototype pro = new Prototype();
+        System.out.println(pro.getName());
+        Prototype pro1 = (Prototype) pro.clone();
+        System.out.println(pro1.getName());
+    }
+
 }
+
