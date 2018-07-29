8af7cda64a374de291461cfd7cde39961dba6057
leone <janlle@126.com>
2018-07-29 11:19:42 +0800

    add some java class

---
+++ spring-boot-design/src/main/java/com/andy/design/structure/facade/CPU.java
@@ -0,0 +1,12 @@
+package com.andy.design.structure.facade;
+
+public class CPU {
+
+    public void startup() {
+        System.out.println("cpu startup!");
+    }
+
+    public void shutdown() {
+        System.out.println("cpu shutdown!");
+    }
+}
