f8426450b922589606509e9a43cc574aa69aa4ba
leone <exklin@gmail.com>
2018-08-01 22:57:54 +0800

    add some new class

---
+++ spring-boot-design/src/main/java/com/andy/design/structure/composite/Client.java
@@ -0,0 +1,22 @@
+package com.andy.design.structure.composite;
+
+/**
+ * 组合模式
+ *
+ * @author: lyon
+ * @since: 2018-08-01
+ **/
+public class Client {
+
+    public static void main(String[] args) {
+        Component root = new ConcreteCompany("root");
+        root.add(new Leaf("file A"));
+        root.add(new Leaf("file B"));
+        Component file = new ConcreteCompany("file1");
+        file.add(new Leaf("file C"));
+        file.add(new Leaf("file D"));
+        root.add(file);
+        root.display(1);
+    }
+
+}
---
+++ spring-boot-design/src/main/java/com/andy/design/structure/composite/Leaf.java
@@ -0,0 +1,34 @@
+package com.andy.design.structure.composite;
+
+/**
+ * 叶子节点
+ *
+ * @author: lyon
+ * @since: 2018-08-01
+ **/
+public class Leaf extends Component {
+
+    public Leaf(String name) {
+        super(name);
+    }
+
+    @Override
+    public void add(Component component) {
+
+    }
+
+    @Override
+    public void remove(Component component) {
+
+    }
+
+    @Override
+    public void display(int depth) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            sb.append("-");
+        }
+        System.out.println(sb.toString() + this.getName());
+    }
+
+}
