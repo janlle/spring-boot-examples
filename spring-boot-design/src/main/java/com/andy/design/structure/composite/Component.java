bc1e64380f53a6f3abc64f816b83312dfe655c1d
leone <exklin@gmail.com>
2018-08-01 22:47:18 +0800

    add some new class

---
+++ spring-boot-design/src/main/java/com/andy/design/structure/composite/Component.java
@@ -0,0 +1,32 @@
+package com.andy.design.structure.composite;
+
+/**
+ * @author: lyon
+ * @since: 2018-08-01
+ **/
+public abstract class Component {
+
+    protected String name;
+
+    public Component(String name) {
+        this.name = name;
+    }
+
+    public Component() {
+    }
+
+    public abstract void add(Component component);
+
+    public abstract void remove(Component component);
+
+    public abstract void display(int depth);
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
