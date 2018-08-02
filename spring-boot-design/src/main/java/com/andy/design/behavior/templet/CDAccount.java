0805bdcb54aacc1256088580da6ca0fa10544237
leone <exklin@gmail.com>
2018-08-02 22:10:33 +0800

    update some file

---
+++ spring-boot-design/src/main/java/com/andy/design/behavior/templet/CDAccount.java
@@ -0,0 +1,15 @@
+package com.andy.design.behavior.templet;
+
+public class CDAccount extends Account {
+
+    @Override
+    protected String doCalculateAccountType() {
+        return "Certificate of Deposite";
+    }
+
+    @Override
+    protected double doCalculateInterestRate() {
+        return 0.06;
+    }
+
+}
