a2424d1ec4bf23b77ca4d5add0e1062da48ae85a
lyon <rethinkt@163.com>
2018-08-02 22:18:12 +0800

    update some file

---
+++ spring-boot-design/src/main/java/com/andy/design/behavior/strategy/ConcreteStrategyA.java
@@ -0,0 +1,10 @@
+package com.andy.design.behavior.strategy;
+
+public class ConcreteStrategyA implements Strategy {
+
+    @Override
+    public void strategy() {
+        System.out.println("do some thing a");
+    }
+
+}
