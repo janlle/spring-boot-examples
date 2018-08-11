03a400f092136c381cf1c510bb376eb82d194cfb
lyon <rethinkt@163.com>
2018-08-11 15:18:36 +0800

    update some file

---
+++ spring-boot-data/src/main/java/com/andy/data/redis/RedisCacheService.java
@@ -0,0 +1,23 @@
+package com.andy.data.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *
+ * @author: lyon
+ * @since: 2018-08-11
+ **/
+@Slf4j
+@Service
+public class RedisCacheService {
+
+    @Cacheable(value = "userCatch")
+    public String userCatch() {
+        System.out.println("=====no catch====");
+        return "当前系统时间:" + System.currentTimeMillis();
+    }
+
+}
