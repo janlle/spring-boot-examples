1891bcda779771606686fa5bf61158a7670d76cd
leone <exklin@gmail.com>
2018-08-11 11:05:31 +0800

    update some file

--- spring-boot-data/src/main/java/com/andy/data/config/AppConfiguration.java
+++ spring-boot-data/src/main/java/com/andy/data/config/AppConfiguration.java
@@ -1,23 +1,13 @@
 package com.andy.data.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cache.CacheManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.cache.RedisCacheWriter;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.core.*;
 
 import java.lang.reflect.Method;
-import java.time.Duration;
 
 /**
  * @author: lyon
@@ -88,6 +78,52 @@ public class AppConfiguration {
 //        template.afterPropertiesSet();
 //        return template;
 //    }
+
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    /**
+     * 实例化 HashOperations 对象,可以使用 Hash 类型操作
+     */
+    @Bean
+    public HashOperations<String, String, String> hashOperations() {
+        return redisTemplate.opsForHash();
+    }
+
+    /**
+     * 实例化 ValueOperations 对象,可以使用 String 操作
+     */
+    @Bean
+    public ValueOperations<String, String> valueOperations() {
+        return redisTemplate.opsForValue();
+    }
+
+    /**
+     * 实例化 ListOperations 对象,可以使用 List 操作
+     *
+     * @return
+     */
+    @Bean
+    public ListOperations<String, String> listOperations() {
+        return redisTemplate.opsForList();
+    }
+
+    /**
+     * 实例化 SetOperations 对象,可以使用 Set 操作
+     */
+    @Bean
+    public SetOperations<String, String> setOperations() {
+        return redisTemplate.opsForSet();
+    }
+
+    /**
+     * 实例化 ZSetOperations 对象,可以使用 ZSet 操作
+     */
+    @Bean
+    public ZSetOperations<String, String> zSetOperations() {
+        return redisTemplate.opsForZSet();
+    }
 
 
 }
