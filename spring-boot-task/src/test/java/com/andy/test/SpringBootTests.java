1a14feaf44f707b62049e306c3b5d09f12035f1c
lyon <rethinkt@163.com>
2018-08-21 21:54:39 +0800

    feat(pom): add new file

---
+++ spring-boot-task/src/test/java/com/andy/test/SpringBootTests.java
@@ -0,0 +1,37 @@
+package com.andy.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * <p>
+ *
+ * @author: lyon
+ * @since: 2018-08-21
+ **/
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SpringBootTests {
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${hello.world}")
+    private String message;
+
+    @Test
+    public void testEnv() {
+        System.out.println(message);
+    }
+
+    @Test
+    public void testEnvironment() {
+        System.out.println(environment.getProperty("hello.world"));
+    }
+
+}
