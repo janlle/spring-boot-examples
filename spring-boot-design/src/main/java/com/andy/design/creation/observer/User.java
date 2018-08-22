32b1f893574ed09a9dc58640d81304b269d1b273
leone <janlle@126.com>
2018-08-22 23:26:48 +0800

    feat(class): add new file

---
+++ spring-boot-design/src/main/java/com/andy/design/creation/observer/User.java
@@ -0,0 +1,29 @@
+package com.andy.design.creation.observer;
+
+/**
+ * <p> 观察者 实现了update方法
+ *
+ * @author: Leone
+ * @since: 2018-08-22
+ **/
+public class User implements Observer {
+
+    private String name;
+
+    private String message;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String message) {
+        this.message = message;
+        read();
+    }
+
+    public void read() {
+        System.out.println(name + " 收到推送消息： " + message);
+    }
+
+}
