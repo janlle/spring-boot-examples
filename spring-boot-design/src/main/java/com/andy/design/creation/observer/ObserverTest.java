95f5bc44f4babe1433d2c3a4d203b3932d66c5af
leone <exklin@gmail.com>
2018-08-22 23:26:48 +0800

    feat(class): add new file

--- spring-boot-design/src/main/java/com/andy/design/creation/observer/ObserverTest.java
+++ spring-boot-design/src/main/java/com/andy/design/creation/observer/ObserverTest.java
@@ -10,10 +10,20 @@ public class ObserverTest {
 
 
     public static void main(String[] args) {
+        WeChatServer server = new WeChatServer();
 
+        Observer jack = new User("jack");
+        Observer andy = new User("andy");
+        Observer jerry = new User("jerry");
 
+        server.registerObserver(jack);
+        server.registerObserver(andy);
+        server.registerObserver(jerry);
+        server.setInfomation("PHP是世界上最好用的语言！");
 
-
+        System.out.println("------------------------");
+        server.removeObserver(jack);
+        server.setInfomation("JAVA是世界上最好用的语言！");
     }
 
 
