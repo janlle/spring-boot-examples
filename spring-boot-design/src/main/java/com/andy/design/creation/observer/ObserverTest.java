c09fc6de37ef3bd654beac7fe8d5ef3b3b50c140
leone <janlle@126.com>
2018-08-22 23:06:45 +0800

    feat(pom): add new file

---
+++ spring-boot-design/src/main/java/com/andy/design/creation/observer/ObserverTest.java
@@ -0,0 +1,22 @@
+package com.andy.design.creation.observer;
+
+/**
+ * <p> 观察者模式测试
+ *
+ * @author: Leone
+ * @since: 2018-08-22
+ **/
+public class ObserverTest {
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
+
+}
