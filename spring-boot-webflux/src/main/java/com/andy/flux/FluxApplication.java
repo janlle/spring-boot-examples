4e288b7a19b490e3415aa1881e9dcf132d6f4b71
leone <exklin@gmail.com>
2018-08-22 23:06:45 +0800

    feat(pom): add new file

--- spring-boot-webflux/src/main/java/com/andy/flux/FluxApplication.java
+++ spring-boot-webflux/src/main/java/com/andy/flux/FluxApplication.java
@@ -18,4 +18,16 @@ public class FluxApplication {
     public static void main(String[] args) {
         SpringApplication.run(FluxApplication.class, args);
     }
+
+//    /****
+//     * 配置常用的转换器和格式化配置（与Spring MVC 5配置方式一样）
+//     */
+//    @Override
+//    public void addFormatters(FormatterRegistry registry) {
+//        // 添加日期格式化转换
+//        DateFormatter dateFormatter = new DateFormatter(DATE_FORMAT);
+//        registry.addFormatter(dateFormatter);
+//
+//    }
+
 }
