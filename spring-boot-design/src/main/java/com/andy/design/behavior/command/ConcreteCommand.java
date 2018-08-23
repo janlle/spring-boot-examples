b239fa9fbdd5b5762367cb6589d0d113a22dbdf1
leone <exklin@gmail.com>
2018-08-23 23:45:53 +0800

    feat(class): add new class

---
+++ spring-boot-design/src/main/java/com/andy/design/behavior/command/ConcreteCommand.java
@@ -0,0 +1,12 @@
+package com.andy.design.behavior.command;
+
+import javax.sound.midi.Receiver;
+
+public class ConcreteCommand extends Command {
+    private Receiver receiver; //维持一个对请求接收者对象的引用
+
+    public void execute() {
+//        receiver.action(); //调用请求接收者的业务处理方法action()
+    }
+
+}
---
+++ spring-boot-design/src/main/java/com/andy/design/behavior/command/FunctionButton.java
@@ -0,0 +1,32 @@
+package com.andy.design.behavior.command;
+
+/**
+ * <p> FunctionButton：功能键类，请求发送者
+ *
+ * @author: Leone
+ * @since: 2018-08-23
+ **/
+public class FunctionButton {
+
+    private String name; //功能键名称
+    private Command command; //维持一个抽象命令对象的引用
+
+    public FunctionButton(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    //为功能键注入命令
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    //发送请求的方法
+    public void onClick() {
+        System.out.print("点击功能键：");
+        command.execute();
+    }
+}
