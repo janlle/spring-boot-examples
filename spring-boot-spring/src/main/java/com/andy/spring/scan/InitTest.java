3c8df31f037a6a06c88772b6327c8acdec7653a9
lyon <rethinkt@163.com>
2018-08-29 23:03:42 +0800

    feat(file): update

--- spring-boot-spring/src/main/java/com/andy/spring/scan/InitTest.java
+++ spring-boot-spring/src/main/java/com/andy/spring/scan/InitTest.java
@@ -2,7 +2,6 @@ package com.andy.spring.scan;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,12 +11,20 @@ import org.springframework.stereotype.Service;
  * @since: 2018-08-28
  **/
 @Slf4j
-@Service
+//@Service
 public class InitTest implements InitializingBean {
+
+    public InitTest() {
+        System.out.println("init");
+    }
 
     @Override
     public void afterPropertiesSet() throws Exception {
         log.info("init test");
+    }
+
+    public void init() {
+        System.out.println("customer init");
     }
 
 
