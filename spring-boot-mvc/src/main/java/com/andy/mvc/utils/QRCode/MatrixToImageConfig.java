e2277bbfdfd8075ec382b601e267bc378d75a506
lyon <rethinkt@163.com>
2018-09-02 18:50:34 +0800

    feat(class): update

--- spring-boot-mvc/src/main/java/com/andy/mvc/utils/QRCode/MatrixToImageConfig.java
+++ spring-boot-mvc/src/main/java/com/andy/mvc/utils/QRCode/MatrixToImageConfig.java
@@ -3,45 +3,40 @@ package com.andy.mvc.utils.QRCode;
 import java.awt.image.BufferedImage;
 
 /**
+ * <p>
  *
- */
+ * @author: Leone
+ **/
 public final class MatrixToImageConfig {
 
-  public static final int BLACK = 0xFF000000;
-  public static final int WHITE = 0xFFFFFFFF;
-  
-  private final int onColor;
-  private final int offColor;
+    public static final int BLACK = 0xFF000000;
+    public static final int WHITE = 0xFFFFFFFF;
 
-  /**
-   * Creates a default config with on color {@link #BLACK} and off color {@link #WHITE}, generating normal
-   * black-on-white barcodes.
-   */
-  public MatrixToImageConfig() {
-    this(BLACK, WHITE);
-  }
+    private final int onColor;
+    private final int offColor;
 
-  /**
-   * @param onColor pixel on color, specified as an ARGB value as an int
-   * @param offColor pixel off color, specified as an ARGB value as an int
-   */
-  public MatrixToImageConfig(int onColor, int offColor) {
-    this.onColor = onColor;
-    this.offColor = offColor;
-  }
 
-  public int getPixelOnColor() {
-    return onColor;
-  }
+    public MatrixToImageConfig() {
+        this(BLACK, WHITE);
+    }
 
-  public int getPixelOffColor() {
-    return offColor;
-  }
 
-  int getBufferedImageColorModel() {
-    // Use faster BINARY if colors match default
+    public MatrixToImageConfig(int onColor, int offColor) {
+        this.onColor = onColor;
+        this.offColor = offColor;
+    }
+
+    public int getPixelOnColor() {
+        return onColor;
+    }
+
+    public int getPixelOffColor() {
+        return offColor;
+    }
+
+    int getBufferedImageColorModel() {
 //    return onColor == BLACK && offColor == WHITE ? BufferedImage.TYPE_BYTE_BINARY : BufferedImage.TYPE_INT_RGB;
-	  return BufferedImage.TYPE_INT_ARGB;
-  }
+        return BufferedImage.TYPE_INT_ARGB;
+    }
 
 }
