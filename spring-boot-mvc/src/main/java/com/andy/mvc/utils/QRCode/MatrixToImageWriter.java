cdb17880a7bf5f876add5ee5119ccea150d7d3b2
lyon <rethinkt@163.com>
2018-09-02 18:50:04 +0800

    feat(class): update

--- spring-boot-mvc/src/main/java/com/andy/mvc/utils/QRCode/MatrixToImageWriter.java
+++ spring-boot-mvc/src/main/java/com/andy/mvc/utils/QRCode/MatrixToImageWriter.java
@@ -19,87 +19,69 @@ package com.andy.mvc.utils.QRCode;
 import com.google.zxing.common.BitMatrix;
 
 import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.OutputStream;
-import java.io.IOException;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
- * Writes a {@link com.google.zxing.common.BitMatrix} to {@link BufferedImage},
- * file or stream. Provided here instead of core since it depends on
- * Java SE libraries.
+ * <p>
  *
- * @author Sean Owen
- */
+ * @author: Leone
+ **/
 public final class MatrixToImageWriter {
 
-  private static final MatrixToImageConfig DEFAULT_CONFIG = new MatrixToImageConfig();
+    private static final MatrixToImageConfig DEFAULT_CONFIG = new MatrixToImageConfig();
 
-  private MatrixToImageWriter() {}
-
-  /**
-   * Renders a {@link com.google.zxing.common.BitMatrix} as an image, where "false" bits are rendered
-   * as white, and "true" bits are rendered as black.
-   */
-  public static BufferedImage toBufferedImage(BitMatrix matrix) {
-    return toBufferedImage(matrix, DEFAULT_CONFIG);
-  }
-
-  /**
-   * As {@link #toBufferedImage(com.google.zxing.common.BitMatrix)}, but allows customization of the output.
-   */
-  public static BufferedImage toBufferedImage(BitMatrix matrix, MatrixToImageConfig config) {
-    int width = matrix.getWidth();
-    int height = matrix.getHeight();
-    BufferedImage image = new BufferedImage(width, height, config.getBufferedImageColorModel());
-    int onColor = config.getPixelOnColor();
-    int offColor = config.getPixelOffColor();
-    for (int x = 0; x < width; x++) {
-      for (int y = 0; y < height; y++) {
-        image.setRGB(x, y, matrix.get(x, y) ? onColor : offColor);
-      }
+    private MatrixToImageWriter() {
     }
-    return image;
-  }
 
-  /**
-   * Writes a {@link com.google.zxing.common.BitMatrix} to a file.
-   *
-   * @see #toBufferedImage(com.google.zxing.common.BitMatrix)
-   */
-  public static void writeToFile(BitMatrix matrix, String format, File file) throws IOException {
-    writeToFile(matrix, format, file, DEFAULT_CONFIG);
-  }
 
-  /**
-   * As {@link #writeToFile(com.google.zxing.common.BitMatrix, String, File)}, but allows customization of the output.
-   */
-  public static void writeToFile(BitMatrix matrix, String format, File file, MatrixToImageConfig config) 
-      throws IOException {  
-    BufferedImage image = toBufferedImage(matrix, config);
-    if (!ImageIO.write(image, format, file)) {
-      throw new IOException("Could not write an image of format " + format + " to " + file);
+    public static BufferedImage toBufferedImage(BitMatrix matrix) {
+        return toBufferedImage(matrix, DEFAULT_CONFIG);
     }
-  }
 
-  /**
-   * Writes a {@link com.google.zxing.common.BitMatrix} to a stream.
-   *
-   * @see #toBufferedImage(com.google.zxing.common.BitMatrix)
-   */
-  public static void writeToStream(BitMatrix matrix, String format, OutputStream stream) throws IOException {
-    writeToStream(matrix, format, stream, DEFAULT_CONFIG);
-  }
 
-  /**
-   * As {@link #writeToStream(com.google.zxing.common.BitMatrix, String, OutputStream)}, but allows customization of the output.
-   */
-  public static void writeToStream(BitMatrix matrix, String format, OutputStream stream, MatrixToImageConfig config) 
-      throws IOException {  
-    BufferedImage image = toBufferedImage(matrix, config);
-    if (!ImageIO.write(image, format, stream)) {
-      throw new IOException("Could not write an image of format " + format);
+    public static BufferedImage toBufferedImage(BitMatrix matrix, MatrixToImageConfig config) {
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height, config.getBufferedImageColorModel());
+        int onColor = config.getPixelOnColor();
+        int offColor = config.getPixelOffColor();
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, matrix.get(x, y) ? onColor : offColor);
+            }
+        }
+        return image;
     }
-  }
+
+
+    public static void writeToFile(BitMatrix matrix, String format, File file) throws IOException {
+        writeToFile(matrix, format, file, DEFAULT_CONFIG);
+    }
+
+
+    public static void writeToFile(BitMatrix matrix, String format, File file, MatrixToImageConfig config)
+            throws IOException {
+        BufferedImage image = toBufferedImage(matrix, config);
+        if (!ImageIO.write(image, format, file)) {
+            throw new IOException("Could not write an image of format " + format + " to " + file);
+        }
+    }
+
+
+    public static void writeToStream(BitMatrix matrix, String format, OutputStream stream) throws IOException {
+        writeToStream(matrix, format, stream, DEFAULT_CONFIG);
+    }
+
+
+    public static void writeToStream(BitMatrix matrix, String format, OutputStream stream, MatrixToImageConfig config)
+            throws IOException {
+        BufferedImage image = toBufferedImage(matrix, config);
+        if (!ImageIO.write(image, format, stream)) {
+            throw new IOException("Could not write an image of format " + format);
+        }
+    }
 
 }
--- spring-boot-mvc/src/main/java/com/andy/mvc/utils/QRCode/MatrixToLogoImageConfig.java
+++ spring-boot-mvc/src/main/java/com/andy/mvc/utils/QRCode/MatrixToLogoImageConfig.java
@@ -5,22 +5,22 @@ import java.awt.Color;
 public class MatrixToLogoImageConfig {
 
 	//logo默认边框颜色
-	public static final Color DEFAULT_BORDERCOLOR = Color.RED;
+	public static final Color DEFAULT_BORDER_COLOR = Color.RED;
+
 	//logo默认边框宽度
 	public static final int DEFAULT_BORDER = 2;
+
 	//logo大小默认为照片的1/5
-	public static final int DEFAULT_LOGOPART = 5;
+	public static final int DEFAULT_LOGO_PART = 5;
 
 	private final int border = DEFAULT_BORDER;
+
 	private final Color borderColor;
+
 	private final int logoPart;
 	
-	/**
-	 * Creates a default config with on color {@link #BLACK} and off color
-	 * {@link #WHITE}, generating normal black-on-white barcodes.
-	 */
 	public MatrixToLogoImageConfig() {
-		this(DEFAULT_BORDERCOLOR, DEFAULT_LOGOPART);
+		this(DEFAULT_BORDER_COLOR, DEFAULT_LOGO_PART);
 	}
 
 	
