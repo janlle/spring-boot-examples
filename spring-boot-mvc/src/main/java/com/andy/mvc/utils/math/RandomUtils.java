f5ae7b9084bf7294b2c96bbf1c15750e601c626e
lyon <rethinkt@163.com>
2018-09-04 22:18:55 +0800

    feat(util): update

---
+++ spring-boot-mvc/src/main/java/com/andy/mvc/utils/math/RandomUtils.java
@@ -0,0 +1,78 @@
+package com.andy.mvc.utils.math;
+
+import java.util.Random;
+
+/**
+ * 随机数工具类
+ */
+public class RandomUtils {
+
+    private static Random random = new Random();
+
+    private static final String ALL_CHAR = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LETTER_CHAR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String NUMBER_CHAR = "0123456789";
+
+    /**
+     * 获取定长的随机数，包含大小写、数字
+     *
+     * @param length 随机数长度
+     * @return
+     */
+    public static String generateString(Integer length) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            sb.append(ALL_CHAR.charAt(random.nextInt(ALL_CHAR.length())));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 获取定长的随机数，包含大小写字母
+     *
+     * @param length 随机数长度
+     * @return
+     */
+    public static String generateMixString(Integer length) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            sb.append(LETTER_CHAR.charAt(random.nextInt(LETTER_CHAR.length())));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 获取定长的随机数，只包含小写字母
+     *
+     * @param length 随机数长度
+     * @return
+     */
+    public static String generateLowerString(Integer length) {
+        return generateMixString(length).toLowerCase();
+    }
+
+    /**
+     * 获取定长的随机数，只包含大写字母
+     *
+     * @param length 随机数长度
+     * @return
+     */
+    public static String generateUpperString(Integer length) {
+        return generateMixString(length).toUpperCase();
+    }
+
+    /**
+     * 获取定长的随机数，只包含数字
+     *
+     * @param length 随机数长度
+     * @return
+     */
+    public static String generateNumberString(Integer length) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            sb.append(NUMBER_CHAR.charAt(random.nextInt(NUMBER_CHAR.length())));
+        }
+        return sb.toString();
+    }
+
+}
