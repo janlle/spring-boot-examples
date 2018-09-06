903530a133951ec72fd126bbc319456de9a33e20
leone <exklin@gmail.com>
2018-09-06 20:40:26 +0800

    docs(all): update config

--- spring-boot-mvc/src/main/java/com/andy/mvc/utils/HttpUtil.java
+++ spring-boot-mvc/src/main/java/com/andy/mvc/utils/HttpUtil.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.http.*;
 import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
@@ -13,7 +12,6 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.ssl.SSLContexts;
@@ -30,13 +28,11 @@ import java.security.KeyStore;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
- * <p>
+ * <p> http工具类
  *
  * @author Leone
- * @since 2018-09-05
  **/
 @Slf4j
 public class HttpUtil {
@@ -51,8 +47,8 @@ public class HttpUtil {
     static {
         RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(3000).setConnectionRequestTimeout(1000).setSocketTimeout(4000).setExpectContinueEnabled(true).build();
         PoolingHttpClientConnectionManager pool = new PoolingHttpClientConnectionManager();
-        pool.setMaxTotal(300);                  // 连接池最大并发连接数
-        pool.setDefaultMaxPerRoute(50);         // 单路由最大并发数
+        pool.setMaxTotal(300);
+        pool.setDefaultMaxPerRoute(50);
         HttpRequestRetryHandler retryHandler = (IOException exception, int executionCount, HttpContext context) -> {
             if (executionCount > 1) {
                 return false;
@@ -95,29 +91,33 @@ public class HttpUtil {
      * @return
      */
     private static HttpRequest setHeaders(Map<String, Object> headers, HttpRequest request) {
-        for (Map.Entry entry : headers.entrySet()) {
-            if (!entry.getKey().equals("Cookie")) {
-                request.addHeader((String) entry.getKey(), (String) entry.getValue());
-            } else {
-                Map<String, Object> Cookies = (Map<String, Object>) entry.getValue();
-                for (Map.Entry entry1 : Cookies.entrySet()) {
-                    request.addHeader(new BasicHeader("Cookie", (String) entry1.getValue()));
-                }
+        if (null != headers && headers.size() > 0) {
+            for (Map.Entry<String, Object> entry : headers.entrySet()) {
+                request.addHeader(entry.getKey(), entry.getValue().toString());
             }
         }
         return request;
     }
 
-    public static String sendPostXml(String url, String xml) {
+    /**
+     * 发送post请求请求体为xml
+     *
+     * @param url
+     * @param xml
+     * @param headers
+     * @return
+     */
+    public static String sendPostXml(String url, String xml, Map<String, Object> headers) {
         String result = null;
         try {
             HttpPost httpPost = new HttpPost(url);
-            StringEntity entity = new StringEntity(xml, StandardCharsets.UTF_8);
+            setHeaders(headers, httpPost);
+            StringEntity entity = new StringEntity(xml, UTF8);
             httpPost.addHeader("Content-Type", "text/xml");
             httpPost.setEntity(entity);
             HttpResponse response = httpClient.execute(httpPost);
             HttpEntity responseData = response.getEntity();
-            result = EntityUtils.toString(responseData, StandardCharsets.UTF_8);
+            result = EntityUtils.toString(responseData, UTF8);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -131,16 +131,17 @@ public class HttpUtil {
      * @param json
      * @return
      */
-    public static String sendPostJson(String url, String json) {
+    public static String sendPostJson(String url, String json, Map<String, Object> headers) {
         String result = null;
         try {
             HttpPost httpPost = new HttpPost(url);
-            StringEntity stringEntity = new StringEntity(json, StandardCharsets.UTF_8);
+            setHeaders(headers, httpPost);
+            StringEntity stringEntity = new StringEntity(json, UTF8);
             stringEntity.setContentType("application/json");
             httpPost.setEntity(stringEntity);
             HttpResponse response = httpClient.execute(httpPost);
             HttpEntity responseData = response.getEntity();
-            result = EntityUtils.toString(responseData, StandardCharsets.UTF_8);
+            result = EntityUtils.toString(responseData, UTF8);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -148,72 +149,32 @@ public class HttpUtil {
     }
 
     /**
-     * 发送post
+     * 发送get请求
      *
      * @param url
      * @param params
+     * @param header
      * @return
      */
-    public static String sendPostMap(String url, Map<String, String> params) {
+    public static String sendGet(String url, Map<String, Object> params, Map<String, Object> header) {
         String result = null;
-        try {
-            HttpPost httpPost = new HttpPost(url);
-            List<NameValuePair> list = new ArrayList<>(params.size());
-            for (Map.Entry<String, String> entry : params.entrySet()) {
-                NameValuePair pair = new BasicNameValuePair(entry.getKey(), entry.getValue());
-                list.add(pair);
-            }
-            httpPost.setEntity(new UrlEncodedFormEntity(list, StandardCharsets.UTF_8));
-            httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-            HttpResponse response = httpClient.execute(httpPost);
-            result = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-
-    public static String sendGetNone(String url, Map<String, String> header) {
-        String result = null;
-        try {
-            HttpGet httpGet = new HttpGet(url);
-            if (null != header && header.size() > 0) {
-                for (Map.Entry<String, String> entity : header.entrySet()) {
-                    httpGet.setHeader(entity.getKey(), entity.getValue());
-                }
-            }
-            HttpResponse response = httpClient.execute(httpGet);
-            result = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-    public static String sendGetMap(String url, Map<String, String> params) {
-        String result = null;
-        List<NameValuePair> pairs = new ArrayList();
-        if (params != null && params.size() > 0) {
-            for (Map.Entry<String, String> entry : params.entrySet()) {
-                pairs.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
-            }
-        }
         try {
             URIBuilder builder = new URIBuilder(url);
-            builder.setParameters(pairs);
+            if (params != null && params.size() > 0) {
+                List<NameValuePair> pairs = new ArrayList();
+                for (Map.Entry<String, Object> entry : params.entrySet()) {
+                    pairs.add(new BasicNameValuePair(entry.getKey(), entry.getValue().toString()));
+                }
+                builder.setParameters(pairs);
+            }
             HttpGet httpGet = new HttpGet(builder.build());
+            setHeaders(header, httpGet);
             HttpResponse response = httpClient.execute(httpGet);
-            result = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+            result = EntityUtils.toString(response.getEntity(), UTF8);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return result;
-    }
-
-
-    public static void main(String[] args) {
-
     }
 
 }
