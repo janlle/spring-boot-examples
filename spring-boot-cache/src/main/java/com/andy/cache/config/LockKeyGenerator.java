57bdf8f39850182712c0b1179c48ed88d40ee9dc
leone <janlle@126.com>
2018-09-08 16:14:07 +0800

    feat(all): update

---
+++ spring-boot-cache/src/main/java/com/andy/cache/config/LockKeyGenerator.java
@@ -0,0 +1,56 @@
+package com.andy.cache.config;
+
+import com.andy.cache.anno.CacheLock;
+import com.andy.cache.anno.CacheParam;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.util.StringUtils;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
+/**
+ * <p> 生成规则实现
+ *
+ * @author Leone
+ * @since 2018-09-08
+ **/
+public class LockKeyGenerator implements CacheKeyGenerator {
+
+    @Override
+    public String getLockKey(ProceedingJoinPoint pjp) {
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        Method method = signature.getMethod();
+        CacheLock lockAnnotation = method.getAnnotation(CacheLock.class);
+        final Object[] args = pjp.getArgs();
+        final Parameter[] parameters = method.getParameters();
+        StringBuilder builder = new StringBuilder();
+        // 默认解析方法里面带 CacheParam 注解的属性,如果没有尝试着解析实体对象中的
+        for (int i = 0; i < parameters.length; i++) {
+            final CacheParam annotation = parameters[i].getAnnotation(CacheParam.class);
+            if (annotation == null) {
+                continue;
+            }
+            builder.append(lockAnnotation.delimiter()).append(args[i]);
+        }
+        if (StringUtils.isEmpty(builder.toString())) {
+            final Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+            for (int i = 0; i < parameterAnnotations.length; i++) {
+                final Object object = args[i];
+                final Field[] fields = object.getClass().getDeclaredFields();
+                for (Field field : fields) {
+                    final CacheParam annotation = field.getAnnotation(CacheParam.class);
+                    if (annotation == null) {
+                        continue;
+                    }
+                    field.setAccessible(true);
+                    builder.append(lockAnnotation.delimiter()).append(ReflectionUtils.getField(field, object));
+                }
+            }
+        }
+        return lockAnnotation.prefix() + builder.toString();
+    }
+}
