9210d1812f1e75d36228a74c0c42c9e9dcc3f166
leone <exklin@gmail.com>
2018-09-08 16:29:50 +0800

    feat(all): update

---
+++ spring-boot-cache/src/main/java/com/andy/cache/controller/BookController.java
@@ -0,0 +1,27 @@
+package com.andy.cache.controller;
+
+import com.andy.cache.anno.CacheLock;
+import com.andy.cache.anno.CacheParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *
+ * @author Leone
+ * @since 2018-09-08
+ **/
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @CacheLock(prefix = "books")
+    @GetMapping
+    public String query(@CacheParam(name = "token") @RequestParam String token) {
+        return "success - " + token;
+    }
+
+
+}
