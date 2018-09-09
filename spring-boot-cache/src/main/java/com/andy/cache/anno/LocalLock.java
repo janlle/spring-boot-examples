24130cc4d10f541c2eb3bca9c7cbb8a52768ca11
leone <janlle@126.com>
2018-09-09 13:18:20 +0800

    feat(all): update

--- spring-boot-cache/src/main/java/com/andy/cache/anno/LocalLock.java
+++ spring-boot-cache/src/main/java/com/andy/cache/anno/LocalLock.java
@@ -8,15 +8,7 @@ import java.lang.annotation.*;
 @Inherited
 public @interface LocalLock {
 
-    /**
-     * @author fly
-     */
     String key() default "";
 
-    /**
-     * 过期时间
-     *
-     * @author fly
-     */
     int expire() default 5;
 }
