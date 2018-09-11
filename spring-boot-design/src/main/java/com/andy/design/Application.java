02a0b9a2add39c47946100bcdc5117457749b417
leone <exklin@gmail.com>
2018-09-11 22:14:36 +0800

    feat(catch):update

---
+++ spring-boot-design/src/main/java/com/andy/design/Application.java
@@ -0,0 +1,18 @@
+package com.andy.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * <p>
+ *
+ * @author Leone
+ * @since 2018-09-11
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
--- spring-boot-websocket/src/main/java/com/andy/websocket/http/HttpsConfig.java
+++ spring-boot-websocket/src/main/java/com/andy/websocket/http/HttpsConfig.java
@@ -27,7 +27,6 @@ public class HttpsConfig {
     }
 
     @Bean
-
     public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
