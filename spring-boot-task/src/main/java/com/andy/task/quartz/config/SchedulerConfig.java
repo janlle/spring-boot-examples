70c698ddf6387fd38d0cb4f37d2c958c9d951ffb
leone <janlle@126.com>
2018-09-15 16:15:54 +0800

    feat(catch):update

--- spring-boot-task/src/main/java/com/andy/task/quartz/config/SchedulerConfig.java
+++ spring-boot-task/src/main/java/com/andy/task/quartz/config/SchedulerConfig.java
@@ -1,8 +1,6 @@
 package com.andy.task.quartz.config;
 
 
-import com.andy.task.quartz.jobs.SchedulingTaskA;
-import com.andy.task.quartz.jobs.SchedulingTaskB;
 import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
@@ -25,7 +23,7 @@ public class SchedulerConfig {
      *
      * @throws SchedulerException
      */
-    public void scheduleJobs() throws SchedulerException {
+    public void startAllJobs() throws SchedulerException {
         Scheduler scheduler = schedulerFactoryBean.getScheduler();
         scheduleJob1(scheduler);
         scheduleJob2(scheduler);
@@ -38,12 +36,7 @@ public class SchedulerConfig {
      * @throws SchedulerException
      */
     private void scheduleJob1(Scheduler scheduler) throws SchedulerException {
-        /*
-         *  此处可以先通过任务名查询数据库，如果数据库中存在该任务，则按照ScheduleRefreshDatabase类中的方法，更新任务的配置以及触发器
-         *  如果此时数据库中没有查询到该任务，则按照下面的步骤新建一个任务，并配置初始化的参数，并将配置存到数据库中
-         */
-        JobDetail jobDetail = JobBuilder.newJob(SchedulingTaskA.class).withIdentity("job1", "group1").build();
-        // 每5s执行一次
+        JobDetail jobDetail = JobBuilder.newJob(ActivityJob.class).withIdentity("job1", "group1").build();
         CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("0/5 * * * * ?");
         CronTrigger cronTrigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1").withSchedule(scheduleBuilder).build();
         scheduler.scheduleJob(jobDetail, cronTrigger);
@@ -56,9 +49,8 @@ public class SchedulerConfig {
      * @throws SchedulerException
      */
     private void scheduleJob2(Scheduler scheduler) throws SchedulerException {
-        JobDetail jobDetail = JobBuilder.newJob(SchedulingTaskB.class).withIdentity("job2", "group1").build();
+        JobDetail jobDetail = JobBuilder.newJob(CollectJob.class).withIdentity("job2", "group1").build();
         CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("0/10 * * * * ?");
-        // 每10s执行一次
         CronTrigger cronTrigger = TriggerBuilder.newTrigger().withIdentity("trigger2", "group1").withSchedule(scheduleBuilder).build();
         scheduler.scheduleJob(jobDetail, cronTrigger);
     }
--- spring-boot-task/src/main/java/com/andy/task/quartz/config/SchedulerListener.java
+++ spring-boot-task/src/main/java/com/andy/task/quartz/config/SchedulerListener.java
@@ -1,35 +1,32 @@
 package com.andy.task.quartz.config;
 
 import org.quartz.SchedulerException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.stereotype.Component;
 
-@Component
+import javax.annotation.Resource;
+
+
+/**
+ * <p>
+ *
+ * @author Leone
+ * @since 2018-09-15
+ **/
 @Configuration
-@EnableScheduling
 public class SchedulerListener implements ApplicationListener<ContextRefreshedEvent> {
 
-    @Autowired
-    public SchedulerConfig schedulerAllJob;
+    @Resource
+    public SchedulerConfig schedulerConfig;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         try {
-            schedulerAllJob.scheduleJobs();
+            schedulerConfig.startAllJobs();
         } catch (SchedulerException e) {
             e.printStackTrace();
         }
-    }
-
-    @Bean
-    public SchedulerFactoryBean schedulerFactoryBean() {
-        return new SchedulerFactoryBean();
     }
 
 }
