499519d19f242b05e9ff22edd26a99e14439d26a
leone <janlle@126.com>
2018-09-29 19:15:48 +0800

    Merge branch 'master' of https://github.com/fooelliot/spring-boot-examples
    # Conflicts:
    #	spring-boot-task/src/main/java/com/andy/task/quartz/config/QuartzConfig.java

--- spring-boot-cache/src/main/java/com/andy/cache/anno/LocalLock.java
+++ spring-boot-cache/src/main/java/com/andy/cache/anno/LocalLock.java
@@ -2,10 +2,17 @@ package com.andy.cache.anno;
 
 import java.lang.annotation.*;
 
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
+
+/**
+ * <p>
+ *
+ * @author Leone
+ * @since 2018-09-29
+ **/
 @Documented
 @Inherited
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface LocalLock {
 
     String key() default "";
