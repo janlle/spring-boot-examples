044c94f3c065aec6d3d6ae232bcd2bdfdfc45bfc
leone <exklin@gmail.com>
2018-09-29 19:15:48 +0800

    Merge branch 'master' of https://github.com/fooelliot/spring-boot-examples
    # Conflicts:
    #	spring-boot-task/src/main/java/com/andy/task/quartz/config/QuartzConfig.java

--- spring-boot-cache/src/main/java/com/andy/cache/CacheApplication.java
+++ spring-boot-cache/src/main/java/com/andy/cache/CacheApplication.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 @EnableCaching
 @SpringBootApplication
 public class CacheApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CacheApplication.class, args);
     }
