c44e7915c7f6e23e600d01525d15c3915f75f1df
leone <janlle@126.com>
2018-09-30 10:14:50 +0800

    fix(all):update

--- spring-boot-aop/src/main/java/com/andy/aop/introspection/Point.java
+++ spring-boot-aop/src/main/java/com/andy/aop/introspection/Point.java
@@ -34,4 +34,11 @@ public class Point {
         this.y = y;
     }
 
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
--- spring-boot-aop/src/main/java/com/andy/aop/introspection/Reflect.java
+++ spring-boot-aop/src/main/java/com/andy/aop/introspection/Reflect.java
@@ -1,5 +1,7 @@
 package com.andy.aop.introspection;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
 
@@ -14,22 +16,36 @@ public class Reflect {
     public static void main(String[] args) throws Exception {
         Point point = new Point(2, 5);
         String proName = "x";
+        System.out.println(point);
         getProperty(point, proName);
         setProperty(point, proName);
+
+
+        // 获取 javaBean 实体详细信息
+        BeanInfo beanInfo = Introspector.getBeanInfo(Point.class);
+        // 获取 JavaBean 属性的实例对象
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+        System.out.println("propertyDescriptors.length:" + propertyDescriptors.length);
+        for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+            System.out.println("getName" + propertyDescriptor.getName());  //获取实体的对象名
+            Method method = propertyDescriptor.getReadMethod();
+            System.out.println("invoke" + method.invoke(point));
+        }
+
     }
 
     private static void getProperty(Point point, String proName) throws Exception {
         PropertyDescriptor proDescriptor = new PropertyDescriptor(proName, Point.class);
         Method methodGetX = proDescriptor.getReadMethod();
-        Object objx = methodGetX.invoke(point);
-        System.out.println(objx);// 2
+        Object obj = methodGetX.invoke(point);
+        System.out.println(obj);            // 2
     }
 
     private static void setProperty(Point point, String proName) throws Exception {
         PropertyDescriptor proDescriptor = new PropertyDescriptor(proName, Point.class);
         Method methodSetX = proDescriptor.getWriteMethod();
         methodSetX.invoke(point, 8);
-        System.out.println(point.getX());// 8
+        System.out.println(point.getX());   // 8
     }
 
 
