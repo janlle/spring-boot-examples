5f3b4da4038971f750839123fefbb44d5336c138
leone <janlle@126.com>
2018-10-07 19:43:30 +0800

    fix(all):update

--- spring-boot-pay/src/main/java/com/andy/pay/weixin/entity/WxPayResp.java
+++ spring-boot-pay/src/main/java/com/andy/pay/weixin/entity/WxPayResp.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-public class WXPayResp implements Serializable {
+public class WxPayResp implements Serializable {
 
     private String orderNum;
 
