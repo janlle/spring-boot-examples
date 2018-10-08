32221c09824f5fa1aaaafb6ab7833aac5db74ede
leone <janlle@126.com>
2018-10-08 18:35:40 +0800

    fix(all):update batch project

---
+++ spring-boot-batch/src/main/java/com/andy/batch/BatchApplication.java
@@ -0,0 +1,19 @@
+package com.andy.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * @author Leone
+ * @since 2018-10-08
+ **/
+@SpringBootApplication
+public class BatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+
+}
