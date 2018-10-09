2338829dec8ce02139b105b0589a4ff91f64f8c7
leone <janlle@126.com>
2018-10-09 14:12:16 +0800

    fix(all):update batch module

--- spring-boot-batch/src/main/java/com/andy/batch/process/CsvItemProcessor.java
+++ spring-boot-batch/src/main/java/com/andy/batch/process/CsvItemProcessor.java
@@ -15,11 +15,9 @@ public class CsvItemProcessor extends ValidatingItemProcessor<Person> {
 
     @Override
     public Person process(Person item) throws ValidationException {
-
-        // 需执行super.proces：（item）才会调用自定义校验器。
+        // 需执行super.process (item) 才会调用自定义校验器
         super.process(item);
-
-        // 对数据做简单的处理，若民族为汉族，则数据转换成01，其余转换成02。
+        // 对数据做简单的处理，若民族为汉族，则数据转换成01，其余转换成02
         if (item.getNation().equals("汉族")) {
             item.setNation("01");
         } else {
---
+++ spring-boot-batch/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table person (
+  id      int primary key auto_increment,
+  name    varchar(48),
+  age     int,
+  nation  varchar(48),
+  address varchar(48)
+);
