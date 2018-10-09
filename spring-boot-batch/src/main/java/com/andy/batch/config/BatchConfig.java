5ffc93ad5ff6e45e351277d3ca9f129c6ab0b2e0
leone <janlle@126.com>
2018-10-09 11:24:57 +0800

    fix(all):update batch project batch config

--- spring-boot-batch/src/main/java/com/andy/batch/config/BatchConfig.java
+++ spring-boot-batch/src/main/java/com/andy/batch/config/BatchConfig.java
@@ -32,7 +32,7 @@ import javax.sql.DataSource;
 
 
 /**
- * <p>
+ * <p>Spring Batch的主要组成部分只需注册成Spring的bean即可。若想开启批处理的支持还需在配置类上使用@EnableBatchProcessing
  *
  * @author Leone
  * @since 2018-10-08
@@ -41,13 +41,102 @@ import javax.sql.DataSource;
 @EnableBatchProcessing
 public class BatchConfig {
 
+    /**
+     * JobRepository：用户注册Job的容器
+     *
+     * @param dataSource
+     * @param transactionManager
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public JobRepository jobRepository(DataSource dataSource, PlatformTransactionManager transactionManager)
+            throws Exception {
+        //jobRepository的定义需要dataSource和transactionManager，Spring Boot已为我们自动配置了这两个类，Spring可通过方法注入已有的Bean。
+        JobRepositoryFactoryBean jobRepositoryFactoryBean = new JobRepositoryFactoryBean();
+        jobRepositoryFactoryBean.setDataSource(dataSource);
+        jobRepositoryFactoryBean.setTransactionManager(transactionManager);
+        jobRepositoryFactoryBean.setDatabaseType("mysql");
+        return jobRepositoryFactoryBean.getObject();
+    }
+
+    /**
+     * JobLauncher：用来启动Job的接口
+     *
+     * @param dataSource
+     * @param transactionManager
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public SimpleJobLauncher jobLauncher(DataSource dataSource, PlatformTransactionManager transactionManager) throws Exception {
+        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+        jobLauncher.setJobRepository(jobRepository(dataSource, transactionManager));
+        return jobLauncher;
+    }
+
+
+    /**
+     * Job：我们要实际执行的任务，包含一个或多个Step
+     *
+     * @param jobBuilderFactory
+     * @param step
+     * @return
+     */
+    @Bean
+    public Job importJob(JobBuilderFactory jobBuilderFactory, Step step) {
+        return jobBuilderFactory
+                .get("importJob")
+                .incrementer(new RunIdIncrementer())
+                // 为Job指定Step
+                .flow(step)
+                .end()
+                .build();
+    }
+
+
+    /**
+     * Step：Step-步骤包含ItemReader，ItemProcessor和ItemWriter
+     *
+     * @param stepBuilderFactory
+     * @param reader
+     * @param writer
+     * @param processor
+     * @return
+     */
+    @Bean
+    public Step step1(StepBuilderFactory stepBuilderFactory,
+                      ItemReader<Person> reader,
+                      ItemWriter<Person> writer,
+                      ItemProcessor<Person, Person> processor) {
+        //1批处理每次提交65000条数据。
+        return stepBuilderFactory.get("step1").<Person, Person>chunk(65000)
+                //2给step绑定reader
+                .reader(reader)
+                //3给step绑定processor
+                .processor(processor)
+                //4给step绑定writer
+                .writer(writer)
+                .build();
+    }
+
+
+    /**
+     * ItemReader：用来读取数据的接口
+     *
+     * @return
+     * @throws Exception
+     */
     @Bean
     public ItemReader<Person> reader() throws Exception {
-        FlatFileItemReader<Person> reader = new FlatFileItemReader<Person>();//1使用FlatFileItemReader读取文件。
-        reader.setResource(new ClassPathResource("people.csv"));//2使用FlatFileItemReader的setResource方法设置csv文件的路径。
-        reader.setLineMapper(new DefaultLineMapper<Person>() {{//3在此处对cvs文件的数据和领域模型类做对应映射。
+        //1使用FlatFileItemReader读取文件
+        FlatFileItemReader<Person> reader = new FlatFileItemReader<Person>();
+        //2使用FlatFileItemReader的setResource方法设置csv文件的路径
+        reader.setResource(new ClassPathResource("people.csv"));
+        //3在此处对cvs文件的数据和领域模型类做对应映射
+        reader.setLineMapper(new DefaultLineMapper<Person>() {{
             setLineTokenizer(new DelimitedLineTokenizer() {{
-                setNames(new String[]{"name", "age", "nation", "address"});
+                setNames("name", "age", "nation", "address");
             }});
             setFieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
                 setTargetType(Person.class);
@@ -56,64 +145,38 @@ public class BatchConfig {
         return reader;
     }
 
+    /**
+     * ItemProcessor：用来处理数据的接口
+     *
+     * @return
+     */
     @Bean
     public ItemProcessor<Person, Person> processor() {
-        CsvItemProcessor processor = new CsvItemProcessor();//1使用我们自己定义的ItemProcessor的实现CsvItemProcessor。
-        processor.setValidator(csvBeanValidator());//2为processor指定校验器为CsvBeanValidator；
+        // 使用我们自己定义的ItemProcessor的实现CsvItemProcessor。
+        CsvItemProcessor processor = new CsvItemProcessor();
+        // 为processor指定校验器为CsvBeanValidator；
+        processor.setValidator(csvBeanValidator());
         return processor;
     }
 
+    /**
+     * ItemWriter：用来输出数据的接口
+     *
+     * @param dataSource
+     * @return
+     */
     @Bean
-    public ItemWriter<Person> writer(DataSource dataSource) {//1Spring能让容器中已有的Bean以参数的形式注入，Spring Boot已为我们定义了dataSource。
-        JdbcBatchItemWriter<Person> writer = new JdbcBatchItemWriter<Person>();//2我们使用JDBC批处理的JdbcBatchItemWriter来写数据到数据库。
-        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Person>());
-        String sql = "insert into person " + " (name,age,nation,address) "
-                + "values( :name, :age, :nation,:address)";
-        writer.setSql(sql);//3在此设置要执行批处理的SQL语句。
+    public ItemWriter<Person> writer(DataSource dataSource) {
+        // 我们使用JDBC批处理的JdbcBatchItemWriter来写数据到数据库。
+        JdbcBatchItemWriter<Person> writer = new JdbcBatchItemWriter<Person>();
+        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+        String sql = "insert into person (name,age,nation,address) values ( :name, :age, :nation,:address)";
+        // 在此设置要执行批处理的SQL语句。
+        writer.setSql(sql);
         writer.setDataSource(dataSource);
         return writer;
     }
 
-    @Bean
-    public JobRepository jobRepository(DataSource dataSource, PlatformTransactionManager transactionManager)
-            throws Exception {
-        //jobRepository的定义需要dataSource和transactioManager，Spring Boot已为我们自动配置了这两个类，Spring可通过方法注入已有的Bean。
-        JobRepositoryFactoryBean jobRepositoryFactoryBean = new JobRepositoryFactoryBean();
-        jobRepositoryFactoryBean.setDataSource(dataSource);
-        jobRepositoryFactoryBean.setTransactionManager(transactionManager);
-        jobRepositoryFactoryBean.setDatabaseType("mysql");
-        return jobRepositoryFactoryBean.getObject();
-    }
-
-    @Bean
-    public SimpleJobLauncher jobLauncher(DataSource dataSource, PlatformTransactionManager transactionManager)
-            throws Exception {
-        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
-        jobLauncher.setJobRepository(jobRepository(dataSource, transactionManager));
-        return jobLauncher;
-    }
-
-    @Bean
-    public Job importJob(JobBuilderFactory jobs, Step s1) {
-        return jobs.get("importJob")
-                .incrementer(new RunIdIncrementer())
-                .flow(s1) //1为Job指定Step。
-                .end()
-                .listener(csvJobListener()) //2绑定监听器csvJobListener。
-                .build();
-    }
-
-    @Bean
-    public Step step1(StepBuilderFactory stepBuilderFactory, ItemReader<Person> reader, ItemWriter<Person> writer,
-                      ItemProcessor<Person, Person> processor) {
-        return stepBuilderFactory
-                .get("step1")
-                .<Person, Person>chunk(65000)//1批处理每次提交65000条数据。
-                .reader(reader)//2给step绑定reader。
-                .processor(processor)//3给step绑定processor。
-                .writer(writer)//4给step绑定writer。
-                .build();
-    }
 
     @Bean
     public CsvJobListener csvJobListener() {
