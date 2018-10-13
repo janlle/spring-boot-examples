278a217a91f6857928c9017e74daab2095a7c0f3
leone <exklin@gmail.com>
2018-10-13 18:26:40 +0800

    feat(all):update

--- spring-boot-cache/src/main/java/com/andy/cache/controller/BookController.java
+++ spring-boot-cache/src/main/java/com/andy/cache/controller/BookController.java
@@ -3,6 +3,9 @@ package com.andy.cache.controller;
 import com.andy.cache.anno.CacheLock;
 import com.andy.cache.anno.CacheParam;
 import com.andy.cache.anno.LocalLock;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -21,6 +24,7 @@ public class BookController {
         return "success - " + token;
     }
 
+    @Transactional(propagation = Propagation.SUPPORTS,readOnly = true,isolation = Isolation.DEFAULT)
     @PostMapping
     @LocalLock(key = "book:arg[0]")
     public String save(@RequestParam String token) {
