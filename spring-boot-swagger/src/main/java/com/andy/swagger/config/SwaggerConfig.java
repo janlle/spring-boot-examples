50eb0a83ca6cbd5d85a077342cc4eeed78e20b5a
leone <exklin@gmail.com>
2018-10-17 11:12:11 +0800

    feat(all):update on swagger

--- spring-boot-swagger/src/main/java/com/andy/swagger/config/SwaggerConfig.java
+++ spring-boot-swagger/src/main/java/com/andy/swagger/config/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package com.andy.swagger.config;
 
 import com.google.common.base.Predicates;
-import org.apache.el.parser.BooleanNode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -13,7 +12,6 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
