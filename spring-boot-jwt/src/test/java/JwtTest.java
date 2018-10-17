1ed16a792b243f31418e53c0e926e5018b554346
leone <janlle@126.com>
2018-10-17 11:35:55 +0800

    feat(all):update on jwt

--- spring-boot-jwt/src/test/java/JwtTest.java
+++ spring-boot-jwt/src/test/java/JwtTest.java
@@ -5,23 +5,19 @@ import java.util.Map;
 
 /**
  * @author Leone
- * @since 2018-04-16 09:43
+ * @since 2018-04-16
  **/
 public class JwtTest {
 
     public static void main(String[] args) throws Exception {
+
         String token = JwtToken.createToken();
-        System.out.println("Token="+ token);
-
-        Map<String, Claim> claims = JwtToken.verifToken(token);
-        System.out.println(claims.get("name").asString());
-        System.out.println(claims.get("age").asInt());
-        System.out.println(claims.get("org") == null ? null : claims.get("org").asString());
-
         String tokenExpire = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmciOiLku4rml6XlpLTmnaEiLCJuYW1lIjoiamFtZXMiLCJleHAiOjE1MjM4NDM1NDksImFnZSI6MjN9.CS-PXcfpmjLErjNVbfIYlmEDcYhz4pyZ7QimTD2Xcg0";
 
-        Map<String, Claim> result = JwtToken.verifToken(tokenExpire);
+        System.out.println("Token="+ token);
+        Map<String, String> result = JwtToken.verifyToken(token);
         System.out.println(result);
+
     }
 
 
