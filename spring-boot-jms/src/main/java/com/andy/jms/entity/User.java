cc992febc468247527af3199dfc09a16e0c31bc4
leone <exklin@gmail.com>
2018-10-24 17:41:22 +0800

    feat(all):update user entity

--- spring-boot-jms/src/main/java/com/andy/jms/entity/User.java
+++ spring-boot-jms/src/main/java/com/andy/jms/entity/User.java
@@ -2,10 +2,10 @@ package com.andy.jms.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
-@Data
-public class User {
+public class User implements Serializable {
 
     private Long userId;
 
@@ -13,24 +13,89 @@ public class User {
 
     private String password;
 
-    private Date birthday;
+    private String description;
 
-    private Double salary;
+    private Integer age;
 
     private Date createTime;
 
-    private Boolean deleted;
+    private boolean deleted;
 
     public User() {
     }
 
-    public User(Long userId, String account, String password, Date birthday, Double salary, Date createTime, Boolean deleted) {
+    public User(String account, String password, String description, Integer age, Date createTime, Boolean deleted) {
+        this.account = account;
+        this.password = password;
+        this.description = description;
+        this.age = age;
+        this.createTime = createTime;
+        this.deleted = deleted;
+    }
+
+    public User(Long userId, String account, String password, String description, Integer age, Date createTime, Boolean deleted) {
         this.userId = userId;
         this.account = account;
         this.password = password;
-        this.birthday = birthday;
-        this.salary = salary;
+        this.description = description;
+        this.age = age;
         this.createTime = createTime;
+        this.deleted = deleted;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
         this.deleted = deleted;
     }
 }
