4ce389c0dd5280d4069fbcb55290650213422f36
leone <exklin@gmail.com>
2018-10-25 17:03:59 +0800

    feat(all):update data module

--- spring-boot-data/src/main/java/com/andy/data/util/EntityFactory.java
+++ spring-boot-data/src/main/java/com/andy/data/util/EntityFactory.java
@@ -1,11 +1,13 @@
 package com.andy.data.util;
 
 import com.andy.data.entity.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 /**
  * <p>
@@ -18,6 +20,8 @@ public class EntityFactory {
 
     private static final String jsonUserData = "{\"username\":\"james\",\"password\":\"admin\",\"email\":\"andy@163.com\",\"token\":\"token:12idkail8lld\",\"salary\":10004,\"birthday\":\"2018-04-02\"}";
 
+    private static Random random = new Random();
+
     /**
      * 获取object数据格式数据
      *
@@ -27,14 +31,18 @@ public class EntityFactory {
     public static List<User> getUsers(Integer count) {
         List<User> userList = new ArrayList<>();
         for (long i = 0; i < count; i++) {
-            User user = new User(i, "jack", RandomUtil.getStr(12), "hello my friend i am a boy " + RandomUtil.getStr(6), 18, new Date(), false);
+            User user = new User(RandomUtil.getName(), RandomUtil.getStr(12), RandomUtil.getUserMap().toString(), 1 + random.nextInt(90), new Date(), false);
             userList.add(user);
         }
         return userList;
     }
 
+    /**
+     * 获取一个object
+     * @return
+     */
     public static User getUser() {
-        return new User(Long.parseLong(1 + RandomUtil.getNum(7)), "andy", RandomUtil.getStr(12), "hello my friend i am a boy " + RandomUtil.getStr(6), 18, new Date(), false);
+        return new User(RandomUtil.getName(), RandomUtil.getStr(12), RandomUtil.getUserMap().toString(), 1 + random.nextInt(90), new Date(), false);
     }
 
     /**
@@ -44,6 +52,11 @@ public class EntityFactory {
      */
     public static String getJsonUser() {
         return jsonUserData;
+    }
+
+    public static void main(String[] args) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        System.out.println(objectMapper.writeValueAsString(getUser()));
     }
 
 }
--- spring-boot-log/src/main/java/com/andy/log/util/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.andy.log.util;
-
-/**
- * @author Leone
- * @since 2018-06-12 23:13
- **/
-public class Utils {
-}
