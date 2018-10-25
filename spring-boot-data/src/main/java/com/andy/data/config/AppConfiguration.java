dfde9d1c1a8a37e6864330470399b27cf6267b6c
leone <janlle@126.com>
2018-10-25 11:49:36 +0800

    feat(all):update data

--- spring-boot-data/src/main/java/com/andy/data/config/AppConfiguration.java
+++ spring-boot-data/src/main/java/com/andy/data/config/AppConfiguration.java
@@ -1,14 +1,18 @@
 package com.andy.data.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.core.*;
 
 import java.lang.reflect.Method;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Leone
@@ -47,20 +51,20 @@ public class AppConfiguration {
         return keyGenerator;
     }
 
-   /* @Bean
-    public CacheManager cacheManager(RedisTemplate redisTemplate) {
-        RedisCacheManager manager = new RedisCacheManager(redisTemplate);
-        manager.setUsePrefix(true);
-        RedisCachePrefix cachePrefix = new RedisPrefix("prefix");
-        manager.setCachePrefix(cachePrefix);
-        // 整体缓存过期时间
-        manager.setDefaultExpiration(3600L);
-        // 设置缓存过期时间。key和缓存过期时间，单位秒
-        Map<String, Long> expiresMap = new HashMap<>();
-        expiresMap.put("user", 1000L);
-        manager.setExpires(expiresMap);
-        return manager;
-    }*/
+//    @Bean
+//    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+//        RedisCacheManager manager = new RedisCacheManager(redisTemplate);
+//        manager.setUsePrefix(true);
+//        RedisCachePrefix cachePrefix = new RedisPrefix("prefix");
+//        manager.setCachePrefix(cachePrefix);
+//        // 整体缓存过期时间
+//        manager.setDefaultExpiration(3600L);
+//        // 设置缓存过期时间。key和缓存过期时间，单位秒
+//        Map<String, Long> expiresMap = new HashMap<>();
+//        expiresMap.put("user", 1000L);
+//        manager.setExpires(expiresMap);
+//        return manager;
+//    }
 
 //    @Bean
 //    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
--- spring-boot-data/src/main/java/com/andy/data/controller/RedisController.java
+++ spring-boot-data/src/main/java/com/andy/data/controller/RedisController.java
@@ -1,7 +1,7 @@
 package com.andy.data.controller;
 
-import com.andy.data.redis.RedisCacheService;
-import com.andy.data.redis.RedisService;
+import com.andy.data.service.RedisCacheService;
+import com.andy.data.service.RedisService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
