e99a4282462fd3efcc71f1b78293f652077e8aa0
leone <exklin@gmail.com>
2018-10-27 16:29:24 +0800

    feat(all):add some class

--- spring-boot-pay/src/main/java/com/andy/pay/common/property/WxProperties.java
+++ spring-boot-pay/src/main/java/com/andy/pay/common/property/WxProperties.java
@@ -27,6 +27,9 @@ public class WxProperties {
 
     private String create_order;
 
+    /*退款url*/
+    private String refund_url;
+
     /*授权url*/
     private String authUrl;
 
@@ -36,101 +39,7 @@ public class WxProperties {
     /*获取openid的url*/
     private String sessionKeyUrl;
 
-
-    public String getApp_id() {
-        return app_id;
-    }
-
-    public void setApp_id(String app_id) {
-        this.app_id = app_id;
-    }
-
-    public String getMch_id() {
-        return mch_id;
-    }
-
-    public void setMch_id(String mch_id) {
-        this.mch_id = mch_id;
-    }
-
-    public String getApp_secret() {
-        return app_secret;
-    }
-
-    public void setApp_secret(String app_secret) {
-        this.app_secret = app_secret;
-    }
-
-    public String getApp_key() {
-        return app_key;
-    }
-
-    public void setApp_key(String app_key) {
-        this.app_key = app_key;
-    }
-
-    public String getApi_key() {
-        return api_key;
-    }
-
-    public void setApi_key(String api_key) {
-        this.api_key = api_key;
-    }
-
-    public String getTrade_type() {
-        return trade_type;
-    }
-
-    public void setTrade_type(String trade_type) {
-        this.trade_type = trade_type;
-    }
-
-    public String getPartner_key() {
-        return partner_key;
-    }
-
-    public void setPartner_key(String partner_key) {
-        this.partner_key = partner_key;
-    }
-
-    public String getNotify_url() {
-        return notify_url;
-    }
-
-    public void setNotify_url(String notify_url) {
-        this.notify_url = notify_url;
-    }
-
-    public String getCreate_order() {
-        return create_order;
-    }
-
-    public void setCreate_order(String create_order) {
-        this.create_order = create_order;
-    }
-
-    public String getAuthUrl() {
-        return authUrl;
-    }
-
-    public void setAuthUrl(String authUrl) {
-        this.authUrl = authUrl;
-    }
-
-    public String getTokenUrl() {
-        return tokenUrl;
-    }
-
-    public void setTokenUrl(String tokenUrl) {
-        this.tokenUrl = tokenUrl;
-    }
-
-    public String getSessionKeyUrl() {
-        return sessionKeyUrl;
-    }
-
-    public void setSessionKeyUrl(String sessionKeyUrl) {
-        this.sessionKeyUrl = sessionKeyUrl;
-    }
+    /*证书路径*/
+    private String certificate;
 
 }
--- spring-boot-pay/src/main/java/com/andy/pay/service/UserService.java
+++ spring-boot-pay/src/main/java/com/andy/pay/service/UserService.java
@@ -1,13 +1,29 @@
 package com.andy.pay.service;
 
+import com.andy.pay.common.exception.ExceptionMessage;
+import com.andy.pay.mapper.UserMapper;
+import com.andy.pay.pojos.entity.Order;
+import com.andy.pay.pojos.entity.User;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.util.Assert;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * @author Leone
- * @since 2018-06-03 19:53
+ * @since 2018-06-03
  **/
 @Slf4j
 @Service
 public class UserService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    public User findOne(Long userId) {
+        User user = userMapper.findByUserId(userId);
+        Assert.notNull(user, ExceptionMessage.USER_NOT_EXIST.getMessage());
+        return user;
+    }
 }
