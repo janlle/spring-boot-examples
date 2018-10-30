021def31b295a9e4ec1e87028363a585218e7ef7
leone <janlle@126.com>
2018-10-30 18:55:24 +0800

    feat(all):update jms

--- spring-boot-jms/src/main/java/com/andy/jms/rabbitmq/config/RabbitMQConstant.java
+++ spring-boot-jms/src/main/java/com/andy/jms/rabbitmq/config/RabbitMQConstant.java
@@ -2,33 +2,33 @@ package com.andy.jms.rabbitmq.config;
 
 /**
  * @author Leone
- * @since 2018-05-01
+ * @since 2018_05_01
  **/
 public interface RabbitMQConstant {
 
     // 消息队列配置
-    String QUEUE_A = "queue-a";
-    String QUEUE_B = "queue-b";
-    String QUEUE_C = "queue-c";
-    String QUEUE_D = "queue-d";
-    String QUEUE_E = "queue-e";
-    String QUEUE_F = "queue-f";
-    String QUEUE_G = "queue-g";
-    String QUEUE_H = "queue-h";
-    String QUEUE_I = "queue-i";
+    String QUEUE_A = "queue_a";
+    String QUEUE_B = "queue_b";
+    String QUEUE_C = "queue_c";
+    String QUEUE_D = "queue_d";
+    String QUEUE_E = "queue_e";
+    String QUEUE_F = "queue_f";
+    String QUEUE_G = "queue_g";
+    String QUEUE_H = "queue_h";
+    String QUEUE_I = "queue_i";
 
 
     // rabbitMQ有四种类型的交换机fanout、direct、topic、headers
-    String HEADERS_EXCHANGE = "headers-exchange";
-    String FANOUT_EXCHANGE = "fanout-exchange";
-    String DIRECT_EXCHANGE = "direct-exchange";
-    String TOPIC_EXCHANGE = "topic-exchange";
+    String HEADERS_EXCHANGE = "headers_exchange";
+    String FANOUT_EXCHANGE = "fanout_exchange";
+    String DIRECT_EXCHANGE = "direct_exchange";
+    String TOPIC_EXCHANGE = "topic_exchange";
 
 
     // 绑定匹配规则[*]表示一个单词,[#]表示任意数量（零个或多个）单词。
     String KEY_A = "topic.#";
-    String KEY_B = "topic.msg";
-    String KEY_C = "direct-a";
-    String KEY_D = "direct-b";
+    String KEY_B = "topic.*";
+    String KEY_C = "direct_a";
+    String KEY_D = "direct_b";
 
 }
