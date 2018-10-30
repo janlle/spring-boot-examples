1d4d247f61a1656372ede7ddfe12d0034431b3b0
leone <exklin@gmail.com>
2018-10-30 17:18:34 +0800

    feat(all):update jms

--- spring-boot-jms/src/main/java/com/andy/jms/rabbitmq/sender/MessageSender.java
+++ spring-boot-jms/src/main/java/com/andy/jms/rabbitmq/sender/MessageSender.java
@@ -5,8 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * @author Leone
@@ -16,7 +17,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageSender {
 
-    @Autowired
+    @Resource
     private AmqpTemplate template;
 
     //-------------------------普通队列模式-------------------------------
@@ -43,7 +44,7 @@ public class MessageSender {
 
     /**
      * topic类型的交换机(主题模式)
-     *
+     * <p>
      * 直连交换机的routing_key方案非常简单，如果我们希望一条消息发送给多个队列，那么这个交换机需要绑定上非常多的routing_key，假设每个交换机上都绑定一堆的routing_key连接到各个队列上。
      * 那么消息的管理就会异常地困难。所以RabbitMQ提供了一种主题交换机，发送到主题交换机上的消息需要携带指定规则的routing_key，主题交换机会根据这个规则将数据发送到对应的(多个)队列上。
      * 主题交换机的routing_key需要有一定的规则，交换机和队列的binding_key需要采用*.#.*.....的格式，每个部分用.分开，其中：
@@ -62,7 +63,7 @@ public class MessageSender {
 
     /**
      * fanout类型的交换机(广播模式)
-     *
+     * <p>
      * 扇形交换机是最基本的交换机类型，它所能做的事情非常简单———广播消息。扇形交换机会把能接收到的消息全部发送给绑定在自己身上的队列。因为广播不需要“思考”，所以扇形交换机处理消息的速度也是所有的交换机类型里面最快的。
      *
      * @param exchange
@@ -76,7 +77,7 @@ public class MessageSender {
 
     /**
      * headers类型的交换机(首部模式)
-     *
+     * <p>
      * 首部交换机是忽略routing_key的一种路由方式。路由器和交换机路由的规则是通过Headers信息来交换的，这个有点像HTTP的Headers。将一个交换机声明成首部交换机，绑定一个队列的时候，定义一个Hash的数据结构，消息发送的时候，会携带一组hash数据结构的信息，当Hash的内容匹配上的时候，消息就会被写入队列。
      * 绑定交换机和队列的时候，Hash结构中要求携带一个键“x-match”，这个键的Value可以是any或者all，这代表消息携带的Hash是需要全部匹配(all)，还是仅匹配一个键(any)就可以了。相比直连交换机，首部交换机的优势是匹配的规则不被限定为字符串(string)。
      *
