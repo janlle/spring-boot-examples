b34c02844c09c4b3d9f107cad36f12da110f413b
leone <janlle@126.com>
2018-11-02 22:20:40 +0800

    feat(all):update qiNiu module

--- spring-boot-concurrency/src/main/java/com/andy/concurrency/test/ConcurrencyTest.java
+++ spring-boot-concurrency/src/main/java/com/andy/concurrency/test/ConcurrencyTest.java
@@ -2,24 +2,35 @@ package com.andy.concurrency.test;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 /**
+ * <p>模拟并发</p>
+ *
  * @author Leone
- * @since 2018-04-22 16:28
+ * @since 2018-04-22
  **/
 @Slf4j
 public class ConcurrencyTest {
 
-    public static int clientTotal = 10000;
-    public static int threadTotal = 50;
+    public static int clientTotal = 5000;
+
+    public static int threadTotal = 200;
 
     public static int count = 0;
 
     public static void main(String[] args) throws Exception {
+        // 初始化线程池
         ExecutorService executorService = Executors.newCachedThreadPool();
+
+
         final Semaphore semaphore = new Semaphore(threadTotal);
+
         final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
 
         for (int i = 0; i < clientTotal; i++) {
             executorService.execute(() -> {
@@ -38,9 +49,11 @@ public class ConcurrencyTest {
         log.info("count={}", count);
     }
 
+    /**
+     * 执行原子操作
+     */
     private static void add() {
-
-        count ++;
+        count++;
     }
 
 
