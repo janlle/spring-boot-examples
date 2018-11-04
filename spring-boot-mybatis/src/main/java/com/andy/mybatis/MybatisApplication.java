68b6bb9b0973c2416642c8f1085c24eb1349d088
leone <exklin@gmail.com>
2018-11-04 12:07:47 +0800

    feat(all):add new module

---
+++ spring-boot-mybatis/src/main/java/com/andy/mybatis/MybatisApplication.java
@@ -0,0 +1,10 @@
+package com.andy.mybatis;
+
+/**
+ * <p>
+ *
+ * @author Leone
+ * @since 2018-11-04
+ **/
+public class MybatisApplication {
+}
---
+++ spring-boot-mybatis/src/main/java/com/andy/mybatis/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.andy.mybatis.mapper;
+
+import com.andy.mybatis.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *
+ * @author Leone
+ * @since 2018-11-04
+ **/
+
+public interface UserMapper extends BaseMapper<User> {
+
+
+
+
+}
