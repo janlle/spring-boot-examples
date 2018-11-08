fb5a186cec653823dad894cfe4a7f537edb9588d
leone <janlle@126.com>
2018-11-08 22:06:36 +0800

    feat(all):update

--- spring-boot-shiro/src/main/java/com/andy/shiro/ShiroApplication.java
+++ spring-boot-shiro/src/main/java/com/andy/shiro/ShiroApplication.java
@@ -1,6 +1,5 @@
 package com.andy.shiro;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Leone
  * @since 2018-04-11
  **/
-@MapperScan(basePackages = {"com.andy.shiro.mapper"})
 @SpringBootApplication
 public class ShiroApplication {
     public static void main(String[] args) {
