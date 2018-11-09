3e45a896a7a45e84a3a1bfa42f51814cbd2873b3
leone <janlle@126.com>
2018-11-09 10:21:07 +0800

    feat(all):update shiro config

--- spring-boot-shiro/src/main/java/com/andy/shiro/config/CredentialMatcher.java
+++ spring-boot-shiro/src/main/java/com/andy/shiro/config/CredentialMatcher.java
@@ -5,8 +5,10 @@ import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authc.credential.SimpleCredentialsMatcher;
 
+import java.util.Objects;
+
 /**
- * <p>匹配器</p>
+ * <p>匹配器
  *
  * @author Leone
  * @since 2018-04-21
@@ -16,9 +18,10 @@ public class CredentialMatcher extends SimpleCredentialsMatcher {
     @Override
     public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
         UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) token;
-        String password = new String(usernamePasswordToken.getPassword());
-        String dbPassword = (String) info.getCredentials();
-        return this.equals(password, dbPassword);
+        String password = usernamePasswordToken.getPassword().toString();
+        String dbPassword = info.getCredentials().toString();
+        return Objects.equals(password, dbPassword);
     }
+
 
 }
