f70435b2de3c8d2cae373b114e7ca44e86d5185a
leone <janlle@126.com>
2018-11-17 20:02:37 +0800

    feat(all):update

--- spring-boot-pay/src/main/java/com/andy/pay/pojos/dto/OrderDto.java
+++
@@ -1,8 +0,0 @@
-package com.andy.pay.pojos.dto;
-
-/**
- * @author Leone
- * @since 2018-06-03 21:08
- **/
-public class OrderDto {
-}
--- spring-boot-pay/src/main/java/com/andy/pay/pojos/form/OrderForm.java
+++
@@ -1,8 +0,0 @@
-package com.andy.pay.pojos.form;
-
-/**
- * @author Leone
- * @since 2018-06-03
- **/
-public class OrderForm {
-}
