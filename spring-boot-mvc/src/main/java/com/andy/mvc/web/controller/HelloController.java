ee558e35a3f7413849740ee49d92be715090bbf8
leone <exklin@gmail.com>
2018-11-29 17:11:53 +0800

    update

--- spring-boot-mvc/src/main/java/com/andy/mvc/web/controller/HelloController.java
+++ spring-boot-mvc/src/main/java/com/andy/mvc/web/controller/HelloController.java
@@ -35,47 +35,6 @@ public class HelloController {
 //        ));
 //    }
 
-    @GetMapping("/hello/{latency}")
-    public String hello(@PathVariable int latency) {
-        return restTemplate.getForObject(TARGET_HOST + "/hello/" + latency, String.class);
-    }
-
-    @GetMapping("insertMybatis/{num}")
-    public String insert(@PathVariable("num") Integer num) {
-        long start = System.currentTimeMillis();
-        userService.insertUserMybatis(num);
-        long end = System.currentTimeMillis();
-        double time = (double) (end - start) / 1000;
-        return "使用mybaits插入" + num + "条数据到mysql一共用了" + time + "秒！";
-    }
-
-
-    @GetMapping("insertJpa/{num}")
-    public String insertJpa(@PathVariable("num") Integer num) {
-        long start = System.currentTimeMillis();
-        userService.insertUserJpa(num);
-        long end = System.currentTimeMillis();
-        double time = (double) (end - start) / 1000;
-        return "使用jpa插入" + num + "条数据到mysql一共用了" + time + "秒！";
-    }
-
-//    @GetMapping("/customerStarter")
-//    public String customerStarter() {
-//        return helloService.sayHello();
-//    }
-
-
-    @GetMapping("/new/{num}")
-    public String newUser(@PathVariable("num") Integer num) {
-        userService.newUser(num);
-        return "jvm测试，新建了" + num + "个user对象！";
-    }
-
-    @GetMapping("/mvc/hello")
-    public String hello() {
-        return "hello world";
-    }
-
 
     @GetMapping("/login")
     public String login() {
