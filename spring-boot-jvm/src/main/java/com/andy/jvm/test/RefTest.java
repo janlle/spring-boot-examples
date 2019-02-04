b2468a4a1903e427f82f900128cf5a20f9f45026
leone <exklin@gmail.com>
2019-02-04 12:03:03 +0800

    :pencil2: update jvm

---
+++ spring-boot-jvm/src/main/java/com/andy/jvm/test/RefTest.java
@@ -0,0 +1,31 @@
+package com.andy.jvm.test;
+
+/**
+ * <p>
+ *
+ * @author leone
+ * @since 2019-02-03
+ **/
+public class RefTest {
+
+    private Object ins;
+
+    public static void main(String[] args) {
+
+        RefTest re1 = new RefTest();
+
+        RefTest re2 = new RefTest();
+
+        re1.ins = re2;
+
+        re2.ins = re1;
+
+        re1 = null;
+
+        re2 = null;
+
+        System.gc();
+
+    }
+
+}
