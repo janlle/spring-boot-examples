67d8b79eca239380772a186613a02e9e526de352
leone <exklin@gmail.com>
2019-02-18 15:24:54 +0800

    :sparkles:[log]:update log module

--- spring-boot-log/src/main/java/com/andy/log/task/LogTask.java
+++ spring-boot-log/src/main/java/com/andy/log/task/LogTask.java
@@ -13,6 +13,7 @@ import java.util.Random;
 
 /**
  * 模拟用户产生日志
+ *
  * @author Leone
  * @since 2018-06-12
  **/
@@ -32,7 +33,7 @@ public class LogTask {
      * 用户购物产生的日志()
      */
     @Async
-    @Scheduled(fixedRate = 100)
+    //@Scheduled(fixedRate = 100)
     public void orderLogTask() {
         logger.info(System.currentTimeMillis() + "\tip\t" + RandomValue.getIp() + "\tusername\t" + RandomValue.getName() + "\tgoodsName\t" + RandomValue.getGoods() + "\torderId\t" + RandomValue.getNum(24));
     }
@@ -41,36 +42,38 @@ public class LogTask {
      * 用户聊天产生的日志()
      */
     @Async
-    @Scheduled(fixedDelay = 50 * 2)
+    //@Scheduled(fixedDelay = 50 * 2)
     public void userChatLogTask() {
-        logger.info(System.currentTimeMillis() + "\tfromUser\t" + RandomValue.getName() + "\tip\t" + RandomValue.getIp() + "\ttoUser\t" + RandomValue.getName() + "\tip\t" + RandomValue.getIp() + "\tmessage\t" + RandomValue.getMessage() + "\ttime\t" + LocalDateTime.now().toLocalTime());
+        logger.info(System.currentTimeMillis() + "\t" + RandomValue.randomMac() + "\t" + RandomValue.getName() + "\t" + RandomValue.getIp() + "\t" + RandomValue.getName() + "\t" + RandomValue.getIp() + "\t" + RandomValue.getMessage() + "\t" + LocalDateTime.now().toLocalTime());
     }
 
     /**
      * 用户访问ip产生的日志(每3秒执行一次)
      */
     @Async
-    @Scheduled(cron = "0/1 * * * * ?")
+    @Scheduled(cron = "* * * * * ?")
     public void userVisitLogTask() {
-        logger.info(System.currentTimeMillis() + "\tphone\t" + RandomValue.getTel() + "\turl\t" + RandomValue.getUrl() + "\tip\t" + RandomValue.getIp() + "\ttime\t" + RANDOM.nextInt(20));
+        for (int i = 0; i < 200; i++) {
+            logger.info(System.currentTimeMillis() + "\t" + RandomValue.randomMac() + "\t" + RandomValue.randomTel() + "\t" + RandomValue.getUrl() + "\t" + RandomValue.randomDriver() + "\t" + RandomValue.getIp() + "\t" + RANDOM.nextInt(100) + "\t" + RANDOM.nextInt(5000));
+        }
     }
 
     /**
      * 用户发送邮件产生的日志(每分钟执行一次)
      */
     @Async
-    @Scheduled(cron = "0/1 * * * * ?")
+    //@Scheduled(cron = "0/1 * * * * ?")
     public void userSendMailLogTask() {
-        logger.info(System.currentTimeMillis() + "\tfromMail\t" + RandomValue.getEmail() + "\ttoMail\t" + RandomValue.getEmail() + "\tcontent\t" + RandomValue.getMessage() + "\ttime\t" + LocalDateTime.now().toLocalTime());
+        logger.info(System.currentTimeMillis() + "\t" + RandomValue.randomMac() + "\t" + RandomValue.getEmail() + "\t" + RandomValue.getEmail() + "\t" + RandomValue.getMessage() + "\t" + LocalDateTime.now().toLocalTime());
     }
 
     /**
      * 用户秒杀商品产生的日志(每2分钟执行一次)
      */
-    @Scheduled(cron = "0 0/2 * * * ?")
+    //@Scheduled(cron = "0 0/2 * * * ?")
     public void secondKillLogTask() {
         for (int i = 0; i < RandomValue.random.nextInt(156) + 100; i++) {
-            logger.info(System.currentTimeMillis() + "\t" + RandomValue.getName() + "\tsecKill\t" + RandomValue.getGoods() + "\tphone\t" + RandomValue.getTel() + "\ttime\t" + LocalDateTime.now().toLocalTime());
+            logger.info(System.currentTimeMillis() + "\t" + RandomValue.getName() + "\tsecKill\t" + RandomValue.getGoods() + "\tphone\t" + RandomValue.randomTel() + "\ttime\t" + LocalDateTime.now().toLocalTime());
         }
     }
 
