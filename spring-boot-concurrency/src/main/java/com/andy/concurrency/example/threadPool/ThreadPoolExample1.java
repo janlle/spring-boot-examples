85dfdbc867a04b165755dc81a1aaef67f4307f55
leone <exklin@gmail.com>
2019-04-09 15:12:02 +0800

    :pencil2:[thread]: update

--- spring-boot-concurrency/src/main/java/com/andy/concurrency/example/threadPool/ThreadPoolExample1.java
+++ spring-boot-concurrency/src/main/java/com/andy/concurrency/example/threadPool/ThreadPoolExample1.java
@@ -6,6 +6,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
+ * 创建一个可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程。
+ * <p>
+ * 这种类型的线程池特点是：
+ * <p>
+ * 工作线程的创建数量几乎没有限制(其实也有限制的,数目为Integer.MAX_VALUE), 这样可灵活的往线程池中添加线程。
+ * 如果长时间没有往线程池中提交任务，即如果工作线程空闲了指定的时间(默认为1分钟)，则该工作线程将自动终止。终止后，如果你又提交了新的任务，则线程池重新创建一个工作线程。
+ * 在使用CachedThreadPool时，一定要注意控制任务的数量，否则，由于大量线程同时运行，很有会造成系统瘫痪。
+ *
  * @author Leone
  * @since 2018-05-08
  **/
@@ -14,12 +22,13 @@ public class ThreadPoolExample1 {
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newCachedThreadPool();
-        for (int i = 0; i < 10; i++) {
-            final int a = i;
-            executorService.execute(new Runnable() {
-                @Override
-                public void run() {
-                    log.info("thread:{}", a);
+        for (int i = 0; i < 100; i++) {
+            executorService.execute(() -> {
+                log.info("thread: {}", Thread.currentThread().getName());
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             });
         }
--- spring-boot-concurrency/src/main/java/com/andy/concurrency/example/threadPool/ThreadPoolExample3.java
+++ spring-boot-concurrency/src/main/java/com/andy/concurrency/example/threadPool/ThreadPoolExample3.java
@@ -4,8 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
+ * 创建一个单线程化的Executor，即只创建唯一的工作者线程来执行任务，它只会用唯一的工作线程来执行任务，
+ * 保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行。
+ * 如果这个线程异常结束，会有另一个取代它，保证顺序执行。
+ * 单工作线程最大的特点是可保证顺序地执行各个任务，并且在任意给定的时间不会有多个线程是活动的。
+ *
  * @author Leone
  * @since 2018-05-08
  **/
@@ -13,15 +19,20 @@ import java.util.concurrent.Executors;
 public class ThreadPoolExample3 {
 
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        for (int i = 0; i < 10; i++) {
-            final int a = i;
-            executorService.execute(() -> {
-                    log.info("thread:{}", a);
-                }
+        ExecutorService threadPool = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 20; i++) {
+            threadPool.execute(() -> {
+                        log.info("thread: {}", Thread.currentThread().getName());
+                        try {
+                            // 模拟业务逻辑
+                            TimeUnit.SECONDS.sleep(1);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
             );
         }
-        executorService.shutdown();
+        threadPool.shutdown();
     }
 
 }
