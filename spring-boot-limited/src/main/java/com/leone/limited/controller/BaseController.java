396e1c9409342a664868a4db9592619554682ae0
leone <exklin@gmail.com>
2019-04-19 12:25:41 +0800

    :sparkles:[all]: update

---
+++ spring-boot-limited/src/main/java/com/leone/limited/controller/BaseController.java
@@ -0,0 +1,31 @@
+package com.leone.limited.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <p>
+ *
+ * @author leone
+ * @since 2019-04-19
+ **/
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class BaseController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/test")
+    public void test(HttpServletRequest request, HttpServletResponse response) {
+        log.info("port: {} address: {}", port, request.getRemoteAddr());
+    }
+
+}
