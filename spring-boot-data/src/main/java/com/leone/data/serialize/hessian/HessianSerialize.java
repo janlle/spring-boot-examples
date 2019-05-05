9a927b3e19e43b7e5ddaee4ceef9f3d7814cb75f
leone <exklin@gmail.com>
2019-05-05 11:13:38 +0800

    :pencil2:[data]: update spring boot data

---
+++ spring-boot-data/src/main/java/com/leone/data/serialize/hessian/HessianSerialize.java
@@ -0,0 +1,69 @@
+package com.leone.data.serialize.hessian;
+
+import com.caucho.hessian.io.HessianInput;
+import com.caucho.hessian.io.HessianOutput;
+import com.leone.common.entity.User;
+import com.leone.common.utils.EntityFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * <p>
+ *
+ * @author leone
+ * @since 2019-05-05
+ **/
+public class HessianSerialize {
+
+    public static void main(String[] args) {
+        byte[] data = serialize(EntityFactory.getUsers(1000000));
+        List<User> user = deserialize(data);
+    }
+
+    public static <T> byte[] serialize(T obj) {
+        long start = System.currentTimeMillis();
+        HessianOutput hessianOutput = null;
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+            // Hessian的序列化输出
+            hessianOutput = new HessianOutput(bos);
+            hessianOutput.writeObject(obj);
+            return bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("hessian serialize time: " + (System.currentTimeMillis() - start));
+            if (Objects.nonNull(hessianOutput)) {
+                try {
+                    hessianOutput.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static <T> T deserialize(byte[] data) {
+        long start = System.currentTimeMillis();
+        HessianInput hessianInput = null;
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(data)) {
+            // Hessian的反序列化读取对象
+            hessianInput = new HessianInput(bis);
+            return (T) hessianInput.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("hessian deserialize time: " + (System.currentTimeMillis() - start));
+            if (Objects.nonNull(hessianInput)) {
+                hessianInput.close();
+            }
+        }
+        return null;
+    }
+
+
+}
