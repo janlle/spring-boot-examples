2520d426f06cb5a9f67d17d0f52de0a8c28e1c47
leone <exklin@gmail.com>
2019-07-05 15:40:54 +0800

    :sparkles: delete code

---
+++ spring-boot-limited/src/main/java/com/leone/boot/limited/synccontainer/VectorExample2.java
@@ -0,0 +1,43 @@
+package com.leone.boot.limited.synccontainer;
+
+
+
+import java.util.Vector;
+
+/**
+ * @author leone
+ * @since 2018-05-06 16:31
+ **/
+
+public class VectorExample2 {
+
+    private static Vector<Integer> vector = new Vector<>();
+
+    public static void main(String[] args) {
+
+        while (true) {
+            for (int i = 0; i < 10; i++) {
+                vector.add(i);
+            }
+            Thread thread1 = new Thread() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        vector.remove(i);
+                    }
+                }
+            };
+            Thread thread2 = new Thread() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        vector.get(i);
+                    }
+                }
+            };
+            thread1.start();
+            thread2.start();
+        }
+    }
+
+}
