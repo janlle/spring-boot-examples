3286c0db94e89259567339b5eb09fbb7d9c98db5
leone <exklin@gmail.com>
2020-04-29 21:15:30 +0800

    Update

--- spring-boot-layui/src/test/java/com/leone/layui/SpringBootLayuiApplicationTests.java
+++ spring-boot-layui/src/test/java/com/leone/layui/SpringBootLayuiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.leone.layui;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootLayuiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
