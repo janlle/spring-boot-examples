360a4313477602f5728ac940ab89a769b9b7e34b
leone <exklin@gmail.com>
2020-04-29 20:56:34 +0800

    add spring boot layui

---
+++ spring-boot-layui/src/main/java/com/leone/layui/SpringBootLayuiApplication.java
@@ -0,0 +1,11 @@
+package com.leone.layui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLayuiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLayuiApplication.class, args);
+    }
+}
---
+++ spring-boot-layui/src/test/java/com/leone/layui/SpringBootLayuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leone.layui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLayuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
