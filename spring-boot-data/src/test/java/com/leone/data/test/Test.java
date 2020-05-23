9636bd958eee6b5ee018213521964e473513d668
leone <exklin@gmail.com>
2020-05-23 11:27:26 +0800

    update

---
+++ spring-boot-data/src/test/java/com/leone/data/test/Test.java
@@ -0,0 +1,21 @@
+package com.leone.data.test;
+
+import sun.java2d.BackBufferCapsProvider;
+import sun.java2d.pipe.AATileGenerator;
+
+
+/**
+ * <p>
+ *
+ * @author leone
+ * @since 2020-04-06
+ **/
+public interface Test extends BackBufferCapsProvider, AATileGenerator {
+
+
+
+}
+
+
+
+
